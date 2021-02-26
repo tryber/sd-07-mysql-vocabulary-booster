deda67ef2a779ca19949fa2cb7c3d5583b84fc66
Vanessa Oliveira <vanessanaara@gmail.com>
2021-02-26 20:10:32 -0300

    Desafios 1 a 8

---
+++ desafio8.sql
@@ -0,0 +1,8 @@
+SELECT customers.ContactName AS 'Nome de contato',
+	shippers.ShipperName AS 'Empresa que fez o envio',
+  orders.OrderDate AS 'Data do pedido'
+FROM w3schools.orders AS orders
+INNER JOIN w3schools.customers AS customers ON orders.CustomerID = customers.CustomerID
+INNER JOIN w3schools.shippers AS shippers ON orders.ShipperID = shippers.ShipperID
+WHERE orders.ShipperID = 1 OR orders.ShipperID = 2
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
