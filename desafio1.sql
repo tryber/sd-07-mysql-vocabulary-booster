c4f3b1224120335ebb5ba1791ca12eca5b0f2204
Kramer <kramerscs@gmail.com>
2021-02-27 15:22:41 -0300

    feat: challenge 1 to 3

---
+++ desafio1.sql
@@ -0,0 +1,5 @@
+SELECT COUNTRY_NAME AS País, IF(R.REGION_NAME = 'Europe', 'incluído', 'não incluído') AS 'Status Inclusão'
+FROM hr.countries AS C
+INNER JOIN hr.regions AS R
+ON C.REGION_ID = R.REGION_ID
+ORDER BY País;
