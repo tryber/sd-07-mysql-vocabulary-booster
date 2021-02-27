f3bff0d808b1b2cd7d10de064fb8c2d7acbcbb4c
Danwhat <daanwhant@gmail.com>
2021-02-27 09:32:00 -0300

    hate lint

--- desafio18.sql
+++ desafio18.sql
@@ -2,11 +2,11 @@ DELIMITER $$
 CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mes INT, ano INT)
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE retorno INT;
-    SELECT
-	COUNT(EMPLOYEE_ID)
-	FROM hr.employees
+DECLARE retorno INT;
+  SELECT
+COUNT(EMPLOYEE_ID)
+FROM hr.employees
 	WHERE MONTH(HIRE_DATE) = mes AND YEAR(HIRE_DATE)= ano
-    INTO retorno;
-    RETURN retorno;
+  INTO retorno;
+  RETURN retorno;
 END $$
