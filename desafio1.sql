c3a603057e3fa2ed5acc2e8a2addecb9a73f8372
Guilherme Almeida <almeidaguilherme204@gmail.com>
2021-02-27 16:52:32 -0300

    Desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,5 @@
-SELECT country.COUNTRY_NAME AS País, if(region.REGION_NAME = 'Europe' , 'Europe' , 'não incluído') as `Status Inclusão`  FROM hr.countries AS country 
+SELECT country.COUNTRY_NAME AS País, if(region.REGION_NAME = 'Europe' , 'incluído' , 'não incluído') as `Status Inclusão`  FROM hr.countries AS country 
 INNER JOIN hr.regions AS region
 ON country.REGION_ID = region.REGION_ID
 ORDER BY country.COUNTRY_NAME;
+
