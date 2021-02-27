479945f740c06276995ed4159d72cf5f93be2059
Vanessa Oliveira <vanessanaara@gmail.com>
2021-02-27 11:24:47 -0300

    code climate

--- desafio8.sql
+++ desafio8.sql
@@ -1,6 +1,6 @@
 SELECT customers.ContactName AS 'Nome de contato',
-	shippers.ShipperName AS 'Empresa que fez o envio',
-  orders.OrderDate AS 'Data do pedido'
+shippers.ShipperName AS 'Empresa que fez o envio',
+orders.OrderDate AS 'Data do pedido'
 FROM w3schools.orders AS orders
 INNER JOIN w3schools.customers AS customers ON orders.CustomerID = customers.CustomerID
 INNER JOIN w3schools.shippers AS shippers ON orders.ShipperID = shippers.ShipperID
