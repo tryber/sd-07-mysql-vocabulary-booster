5761389a59b4cf5f91b3f842254cea09438687df
Kramer <kramerscs@gmail.com>
2021-02-28 09:57:13 -0300

    feat: challenge 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,5 @@
 SELECT
-CONCAT(FIRST_NAME, ' ', LAST_NAME) AS 'Nome completo', 
+UCASE(CONCAT(FIRST_NAME, ' ', LAST_NAME)) AS 'Nome completo', 
 JH.START_DATE AS 'Data de início',
 SALARY AS Salário FROM hr.employees AS E
 INNER JOIN hr.job_history AS JH
