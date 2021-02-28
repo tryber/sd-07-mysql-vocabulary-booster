e5270940ab99c60d7d1f9e0fa1031680f90bc8e5
Francisco <franciscoliratorres@gmail.com>
2021-02-28 16:13:47 -0300

    6, 7, 8 corrigidos e  9

--- desafio9.sql
+++ desafio9.sql
@@ -1,10 +1,7 @@
-SELECT CUST.ContactName "Nome de contato",
-SHI.ShipperName "Empresa que fez o envio",
-ORD.OrderDate "Data do pedido"
-FROM w3schools.orders ORD
-INNER JOIN w3schools.customers CUST
-ON ORD.CustomerID = CUST.CustomerID
-INNER JOIN w3schools.shippers SHI
-ON ORD.ShipperID = SHI.ShipperID
-WHERE SHI.ShipperName = "Speedy Express" OR SHI.ShipperName = "United Package"
-ORDER BY CUST.ContactName, SHI.ShipperName, ORD.OrderDate;
+SELECT CONCAT(EMP.FirstName, ' ', EMP.LastName) "Nome completo",
+COUNT(ORD.EmployeeID) "Total de pedidos"
+FROM w3schools.employees EMP
+INNER JOIN w3schools.orders ORD
+ON ORD.EmployeeID = EMP.EmployeeID
+GROUP BY ORD.EmployeeID
+ORDER BY COUNT(ORD.EmployeeID);
