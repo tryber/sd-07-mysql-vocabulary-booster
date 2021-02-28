f059a653899b4eef8015218d13dadd456015a07e
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2021-02-28 09:10:26 -0300

    Desafio 11 - Adicinando condição para remover resultados com 0

--- desafio11.sql
+++ desafio11.sql
@@ -5,5 +5,5 @@ FROM w3schools.customers AS c1,
 w3schools.customers AS c2
 WHERE c1.Country = c2.Country
 GROUP BY c1.CustomerID
-HAVING COUNT(c1.CustomerID)
+HAVING (COUNT(c1.CustomerID) - 1) > 0
 ORDER BY c1.ContactName;
