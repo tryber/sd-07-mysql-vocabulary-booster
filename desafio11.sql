f8962d3308444b72bcb7c1e5c42ce0c23be4f8e6
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2021-02-27 22:31:44 -0300

    Corrigindo Req 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,12 @@
 SELECT
 t1.ContactName AS 'Nome',
 t1.Country AS 'País',
-(SELECT (COUNT(*) - 1) FROM w3schools.customers WHERE t1.Country = Country) AS 'Número de compatriotas'
+(SELECT (COUNT(*))
+	FROM w3schools.customers
+    WHERE t1.Country = Country AND t1.ContactName <> ContactName) AS 'Número de compatriotas'
 FROM w3schools.customers AS t1
+WHERE (SELECT (COUNT(*))
+	FROM w3schools.customers
+    WHERE t1.Country = Country AND t1.ContactName <> ContactName) <> 0
 GROUP BY t1.ContactName
 ORDER BY t1.ContactName;
