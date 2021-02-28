f756a18bf32441d9a0afa1dad2ca1ae34521d9c7
vitorAmorims <vtamorims@gmail.com>
2021-02-27 21:27:53 -0300

    Inserindo alias na coluna do desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,3 +1,3 @@
-SELECT countries.country_name, (IF (countries.region_id = 1, 'incluído', 'não incluído')) AS "Status Inclusão"
+SELECT countries.country_name AS "País", (IF (countries.region_id = 1, 'incluído', 'não incluído')) AS "Status Inclusão"
 FROM countries
 ORDER BY countries.country_name;
