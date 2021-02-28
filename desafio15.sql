039cea4ca21f754f347ee92663ff2a28a8e2e740
Kramer <kramerscs@gmail.com>
2021-02-28 12:03:25 -0300

    feat: challenge 15

---
+++ desafio15.sql
@@ -0,0 +1,10 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `buscar_media_por_cargo`(IN jobTitle VARCHAR(50))
+BEGIN
+	SELECT ROUND(AVG(SALARY), 2) AS 'Média salarial' FROM(
+	SELECT E.SALARY, J.JOB_TITLE
+	FROM hr.employees AS E
+	INNER JOIN hr.jobs AS J
+	ON E.JOB_ID = J.JOB_ID
+	WHERE J.JOB_TITLE = jobTitle) AS Consulta
+	GROUP BY JOB_TITLE;
+END
