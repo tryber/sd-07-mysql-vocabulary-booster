367e3008375d7ade08f99556b302dfa71e18d464
spawnred <bruno.spawnhell@gmail.com>
2021-02-28 16:54:00 -0300

    corrigindo item 6

--- desafio6.sql
+++ desafio6.sql
@@ -2,7 +2,7 @@ SELECT
 CONCAT(FIRST_NAME, " ", LAST_NAME) AS "Nome completo",
 jobs.JOB_TITLE AS Cargo,
 HIRE_DATE AS "Data de início do cargo",
-departments.DEPARTMENT_NAME
+departments.DEPARTMENT_NAME AS Departamento
 FROM hr.employees
 INNER JOIN jobs ON employees.JOB_ID = jobs.JOB_ID
 INNER JOIN departments ON employees.DEPARTMENT_ID = departments.DEPARTMENT_ID
