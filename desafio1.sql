d243a30a2f557d17ec352bf419f02a21a70c1c62
renatomak <renato.mark.silva@gmail.com>
2021-02-27 22:22:58 -0300

    Desafio 1-modificado

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,4 @@
 SELECT country_name País, if(REGION_NAME='Europe', 'incluído', 'não incluído') `Status Inclusão`
-FROM countries c, regions r
+FROM hr.countries c, hr.regions r
 WHERE c.REGION_ID=r.REGION_ID
 ORDER BY REGION_NAME;
