347c9cbb7510ae09bee202ac279841f8660d4402
Kramer <kramerscs@gmail.com>
2021-02-28 17:51:07 -0300

    refactor: challenge 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,10 @@
-SELECT t1.ContactName AS 'Nome', t1.Country AS País, COUNT(*) AS 'Número de compatriotas'
-FROM w3schools.customers AS t1, w3schools.customers AS t2
-WHERE t1.Country = t2.Country AND (t1.CustomerID <> t2.CustomerID)
-GROUP BY t1.ContactName
-ORDER BY t1.ContactName;
+SELECT Nome, t0.Country AS País, Qt AS 'Número de compatriotas'
+FROM w3schools.customers AS t0
+INNER JOIN(
+  SELECT t1.ContactName AS 'Nome', COUNT(*) AS Qt
+  FROM w3schools.customers AS t1
+  INNER JOIN( SELECT * FROM w3schools.customers) AS t2
+  ON t1.Country = t2.Country AND (t1.CustomerID <> t2.CustomerID)
+  GROUP BY t1.ContactName) AS Consulta
+  ON Nome = t0.ContactName
+  ORDER BY Nome;
