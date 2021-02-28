9536264492939f3704213ff2195d6c67993b5785
Kramer <kramerscs@gmail.com>
2021-02-28 12:14:12 -0300

    feat: challenge 15

--- desafio15.sql
+++ desafio15.sql
@@ -3,13 +3,13 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN jobTitle VARCHAR(50))
 BEGIN
-	SELECT AVG(SALARY) AS 'Média salarial' FROM(
-	SELECT E.SALARY, J.JOB_TITLE
-	FROM hr.employees AS E
-	INNER JOIN hr.jobs AS J
-	ON E.JOB_ID = J.JOB_ID
-	WHERE J.JOB_TITLE = jobTitle) AS Consulta
-	GROUP BY JOB_TITLE;
+SELECT AVG(SALARY) AS 'Média salarial' FROM(
+SELECT E.SALARY, J.JOB_TITLE
+FROM hr.employees AS E
+INNER JOIN hr.jobs AS J
+ON E.JOB_ID = J.JOB_ID
+WHERE J.JOB_TITLE = jobTitle) AS Consulta
+GROUP BY JOB_TITLE;
 END $$
 
 DELIMITER ;
