73cc94706b8cb3ac8efeeb944ea44f2f724214ba
spawnred <bruno.spawnhell@gmail.com>
2021-02-28 17:09:44 -0300

    corrigindo item 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,9 +1,10 @@
 SELECT
-CONCAT(FIRST_NAME, " ", LAST_NAME) AS "Nome completo",
+CONCAT(employees.FIRST_NAME, " ", employees.LAST_NAME) AS "Nome completo",
 jobs.JOB_TITLE AS Cargo,
-HIRE_DATE AS "Data de início do cargo",
+START_DATE AS "Data de início do cargo",
 departments.DEPARTMENT_NAME AS Departamento
-FROM hr.employees
-INNER JOIN jobs ON employees.JOB_ID = jobs.JOB_ID
-INNER JOIN departments ON employees.DEPARTMENT_ID = departments.DEPARTMENT_ID
-ORDER BY CONCAT(FIRST_NAME, " ", LAST_NAME) DESC, Cargo ASC;
+FROM hr.job_history
+INNER JOIN employees ON job_history.EMPLOYEE_ID = employees.EMPLOYEE_ID
+INNER JOIN jobs ON job_history.JOB_ID = jobs.JOB_ID
+INNER JOIN departments ON job_history.DEPARTMENT_ID = departments.DEPARTMENT_ID
+ORDER BY CONCAT(employees.FIRST_NAME, " ", employees.LAST_NAME) DESC, Cargo ASC;
