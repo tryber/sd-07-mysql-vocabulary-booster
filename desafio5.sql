c3787113ebced53c208297e2c7097e906ec6e68a
renatomak <renato.mark.silva@gmail.com>
2021-02-28 17:25:10 -0300

    Desafio 5 - modificado

--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,7 @@
 SELECT J.JOB_TITLE AS Cargo,
 (J.MAX_SALARY - J.MIN_SALARY) AS 'Variação Salarial',
-ROUND(AVG(J.MIN_SALARY), 2) AS 'Média mínima mensal',
-ROUND(AVG(J.MAX_SALARY), 2) AS 'Média máxima mensal'
+ROUND(J.MIN_SALARY/12, 2) AS 'Média mínima mensal',
+ROUND(J.MAX_SALARY/12, 2) AS 'Média máxima mensal'
 FROM hr.jobs AS J, hr.employees AS E
 WHERE J.JOB_ID=E.JOB_ID
 GROUP BY E.JOB_ID
