257fd8c88ed6f6ac4fbb7f1775c614723a595348
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2021-02-28 11:14:02 -0300

    issues v1

--- desafio17.sql
+++ desafio17.sql
@@ -4,8 +4,8 @@ CREATE TRIGGER inserindoCarrosv2
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
-   INSERT INTO orders(OrderDate)
-   VALUES (CURRENT_DATE());
+INSERT INTO orders(OrderDate)
+VALUES (CURRENT_DATE());
 END; 
 $$
 DELIMITER ;
