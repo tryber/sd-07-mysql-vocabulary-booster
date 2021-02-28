8f825e069f38661dddf410e52f78eda6ee296aec
renatomak <renato.mark.silva@gmail.com>
2021-02-28 17:21:01 -0300

    Desafio 4 - modificado

--- desafio4.sql
+++ desafio4.sql
@@ -1,12 +1,12 @@
-SELECT j.JOB_TITLE AS 'Cargo',
-ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
+SELECT j.JOB_TITLE Cargo,
+ROUND(AVG(e.SALARY), 2) `Média salarial`,
 CASE
-WHEN ROUND(AVG(e.SALARY), 2) < 5801 THEN 'Júnior'
-WHEN ROUND(AVG(e.SALARY), 2) < 7501 THEN 'Pleno'
-WHEN ROUND(AVG(e.SALARY), 2) < 10501 THEN 'Sênior'
+WHEN ROUND(AVG(e.SALARY), 2)<5801 THEN 'Júnior'
+WHEN ROUND(AVG(e.SALARY), 2)<7501 THEN 'Pleno'
+WHEN ROUND(AVG(e.SALARY), 2)<10501 THEN 'Sênior'
 ELSE 'CEO'
 END AS `Senioridade`
-FROM hr.employees AS e INNER JOIN hr.jobs AS j
-ON j.JOB_ID=e.JOB_ID
+FROM hr.employees e, hr.jobs j
+WHERE j.JOB_ID=e.JOB_ID
 GROUP BY e.JOB_ID
-ORDER BY `Média salarial`;
+ORDER BY `Média salarial`, Cargo;
