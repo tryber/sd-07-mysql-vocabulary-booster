fb788e2c762c9a8b60d01c4d8b413dd38a4531d1
renatomak <renato.mark.silva@gmail.com>
2021-02-28 17:43:51 -0300

    Desafio 6 - modificado a clausula where na ligaçao de job_history e Departament

--- desafio6.sql
+++ desafio6.sql
@@ -4,6 +4,6 @@ J.JOB_TITLE Cargo,
 JH.START_DATE 'Data de início do cargo',
 D.DEPARTMENT_NAME Departamento
 FROM hr.jobs J, hr.employees E, hr.departments D, hr.job_history JH
-WHERE D.DEPARTMENT_ID=E.DEPARTMENT_ID
+WHERE D.DEPARTMENT_ID=JH.DEPARTMENT_ID
 AND JH.EMPLOYEE_ID=E.EMPLOYEE_ID AND J.JOB_ID=JH.JOB_ID
 ORDER BY `Nome completo` DESC, Cargo;
