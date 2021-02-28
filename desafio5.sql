e7dda0cffb45f2ccc4a58d0db620b91b87c0e733
marcelo ivan <sagacello@hotmail.com>
2021-02-28 15:37:19 -0300

    1 ao 6

--- desafio5.sql
+++ desafio5.sql
@@ -1,6 +1,6 @@
 SELECT JOB_TITLE AS "Cargo" ,ROUND((MAX_SALARY - MIN_SALARY), 2) AS "Variação Salarial",
-ROUND(AVG(MIN_SALARY), 2) AS "Média mínima mensal",
-ROUND(AVG(MAX_SALARY), 2) AS "Média máxima mensal" 
+ROUND(AVG(MIN_SALARY / 12), 2) AS "Média mínima mensal",
+ROUND(AVG(MAX_SALARY / 12), 2) AS "Média máxima mensal" 
 FROM hr.jobs 
 GROUP BY JOB_TITLE
 ORDER BY ROUND((MAX_SALARY - MIN_SALARY), 2) ,JOB_TITLE;
