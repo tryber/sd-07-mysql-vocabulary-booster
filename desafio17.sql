6f66e96bd80dab299bd15852935dfd753e2ea2d9
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2021-02-28 11:11:46 -0300

    Requisito 17

---
+++ desafio17.sql
@@ -0,0 +1,11 @@
+USE w3schools;
+DELIMITER $$
+CREATE TRIGGER inserindoCarrosv2
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+   INSERT INTO orders(OrderDate)
+   VALUES (CURRENT_DATE());
+END; 
+$$
+DELIMITER ;
