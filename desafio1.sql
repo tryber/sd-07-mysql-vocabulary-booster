dbe137b64d5fdc6cbd2e7d4c4f436a90050ee1dc
Macarena Rodriguez <macarenar697@gmail.com>
2021-03-01 18:57:56 -0300

    Req1

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,3 @@
-SELECT COUNTRY_NAME AS Pais, IF(REGION_ID = 1, 'incluído', 'não incluído') AS Status_Inclusão FROM hr.countries;
+SELECT COUNTRY_NAME AS Pais, IF(REGION_ID = 1, 'incluído', 'não incluído') AS Status_Inclusão
+FROM hr.countries
+ORDER BY COUNTRY_NAME;
