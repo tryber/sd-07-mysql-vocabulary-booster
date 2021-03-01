96d56c8bd00b7f59ef86494070b50a7262a7c5fe
luc-zago <luc.zago@hotmail.com>
2021-03-01 16:59:27 -0300

    Alterado desafio 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,12 +1,5 @@
-SELECT UPPER(CONCAT(e.first_name," ", e.last_name)) AS `Nome completo`,
-CASE
-WHEN e.job_id = h.job_id THEN h.start_date
-WHEN e.job_id != h.job_id THEN MAX(DATE_ADD(h.end_date, INTERVAL 1 DAY))
-ELSE e.hire_date
-END AS `Data de início`,
-e.salary AS "Salário"
-FROM hr.employees AS e
-LEFT JOIN hr.job_history AS h ON e.employee_id = h.employee_id
-GROUP BY e.employee_id
-HAVING MONTH(`Data de início`) IN (1, 2, 3)
-ORDER BY `Nome completo`, `Data de início`;
+SELECT UPPER(CONCAT(e.first_name, " ", e.last_name)) AS "Nome completo",
+h.start_date AS "Data de início", e.salary AS "Salário" FROM hr.job_history AS h
+JOIN hr.employees AS e ON e.employee_id = h.employee_id
+WHERE MONTH(start_date) IN (1, 2, 3)
+ORDER BY UPPER(CONCAT(e.first_name, " ", e.last_name)), h.start_date;
