b6c6ad1bf3a95944fd1b8a8174bb02123bee7436
Guilherme Almeida <almeidaguilherme204@gmail.com>
2021-03-01 15:08:23 -0300

    Desafio 6

---
+++ desafio7.sql
@@ -0,0 +1,7 @@
+SELECT CONCAT(employe.FIRST_NAME,' ',employe.LAST_NAME) AS "Nome completo",
+history_job.START_DATE AS "Data de início",
+employe.SALARY AS Salário
+FROM hr.job_history AS history_job
+INNER JOIN hr.employees AS employe
+ON employe.EMPLOYEE_ID = history_job.EMPLOYEE_ID
+WHERE MONTH(history_job.START_DATE) >= 1 AND MONTH(history_job.START_DATE)  <= 3;
