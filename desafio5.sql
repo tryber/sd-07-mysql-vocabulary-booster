1a7fdb5568b04235f42cc0ee803f3a56f668f619
Lenodeoliveira <lenodeoliveira@gmail.com>
2021-03-01 14:59:33 -0300

    Correção requisito 5

--- desafio5.sql
+++ desafio5.sql
@@ -3,5 +3,4 @@ ROUND(MAX_SALARY - MIN_SALARY, 2) AS `Variação Salarial`,
 ROUND(MIN_SALARY / 12, 2) AS `Média mínima mensal`,
 ROUND(MAX_SALARY / 12, 2) AS `Média máxima mensal`
 FROM hr.jobs
-GROUP BY JOB_TITLE
 ORDER BY `Variação Salarial`, Cargo;
