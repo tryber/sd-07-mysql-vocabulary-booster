7e8f70be0faa0729b1e2ec2334830fe413d0d069
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2021-03-01 14:57:10 -0300

    r11 v3

--- desafio11.sql
+++ desafio11.sql
@@ -3,6 +3,5 @@ t1.ContactName AS 'Nome',
 t1.Country AS 'País',
 (SELECT (COUNT(*)) FROM w3schools.customers WHERE t1.Country = Country AND t1.ContactName <> ContactName;) AS 'Número de compatriotas'
 FROM w3schools.customers AS t1
-WHERE (SELECT (COUNT(*)) FROM w3schools.customers WHERE t1.Country = Country AND t1.ContactName <> ContactName;) <> 0
-GROUP BY t1.ContactName
+GROUP BY t1.ContactName HAVING (SELECT (COUNT(*)) FROM w3schools.customers WHERE t1.Country = Country AND t1.ContactName <> ContactName;) <> 0
 ORDER BY t1.ContactName;
