54fba08d6c6622084545d28fa121f48e6eac4f73
Melissa Gomes <mel.ge171201@gmail.com>
2021-03-01 18:41:51 -0300

    req 6 aaaaa

--- desafio6.sql
+++ desafio6.sql
@@ -8,4 +8,4 @@ INNER JOIN hr.jobs AS j
 INNER JOIN hr.departments AS d
 INNER JOIN hr.job_history AS h
 ON e.JOB_ID = j.JOB_ID AND e.DEPARTMENT_ID = d.DEPARTMENT_ID AND e.JOB_ID = h.JOB_ID
-ORDER BY `Nome completo`, Cargo; 
+ORDER BY `Nome completo` DESC, Cargo; 
