ccffe954de7e58839d31db363adbd4a652e1a8ad
Francisco <franciscoliratorres@gmail.com>
2021-03-01 10:33:08 -0300

    Ajuste desafio 14

--- desafio14.sql
+++ desafio14.sql
@@ -1,6 +1,7 @@
 INSERT INTO w3schools.customers (CustomerName, ContactName, Address, City, PostalCode, country) VALUES
 ("Torres Enterprise", "Francisco Torres", "Rua Zodiaco, 525", "Belo Horizonte", "30360430", "Australia");
 
-SELECT DISTINCT Country País FROM w3schools.customers
+SELECT DISTINCT Country "País"
+FROM w3schools.customers
 ORDER BY Country
 LIMIT 5;
