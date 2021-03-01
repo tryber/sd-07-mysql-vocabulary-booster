8575f415e444c9fa1baaf0da4c6575b0c5ba77cf
Matheus Beaumord <matheusbeaumord@gmail.com>
2021-03-01 17:14:01 -0300

    Envio quatro do requisito 1 para testes

--- desafio1.sql
+++ desafio1.sql
@@ -1,8 +1,8 @@
 SELECT c.COUNTRY_NAME 'País',
 CASE 
-	WHEN r.REGION_NAME like 'Europe' THEN "incluído"
-	ELSE "não incluído"
-	END AS "Status Inclusão"
+WHEN r.REGION_NAME like 'Europe' THEN "incluído"
+ELSE "não incluído"
+END AS "Status Inclusão"
 FROM hr.countries c
 LEFT JOIN hr.regions r
 ON c.REGION_ID = r.REGION_ID
