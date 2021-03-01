28455c8acba93c1da158b4ac7c7093df9f2c7d56
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2021-03-01 14:34:09 -0300

    tentando consertar o r11

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,7 @@
 SELECT
 t1.ContactName AS 'Nome',
 t1.Country AS 'País',
-(SELECT (COUNT(*)) FROM w3schools.customers WHERE t1.Country = Country AND t1.ContactName <> ContactName) AS 'Número de compatriotas'
+(SELECT (COUNT(*)) FROM w3schools.customers WHERE t1.Country = Country AND t1.ContactName <> ContactName;) AS 'Número de compatriotas'
 FROM w3schools.customers AS t1
 WHERE (SELECT (COUNT(*)) FROM w3schools.customers WHERE t1.Country = Country AND t1.ContactName <> ContactName) <> 0
 GROUP BY t1.ContactName
