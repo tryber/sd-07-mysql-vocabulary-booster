09d10837f51f80003eeefa05edcfe24df820e450
danielmlduarte <danielduarte.tryber@gmail.com>
2021-03-01 18:16:47 -0300

    Desafio 13

---
+++ desafio13.sql
@@ -0,0 +1,7 @@
+SELECT
+p.ProductName AS Produto,
+p.Price AS Preço
+FROM w3schools.products p
+LEFT JOIN w3schools.order_details od ON p.ProductID = od.ProductID
+WHERE od.Quantity > 80
+ORDER BY Produto;
