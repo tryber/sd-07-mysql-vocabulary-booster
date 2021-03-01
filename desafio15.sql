4df6bd73b9f3c5f5a7361ebf60cca1e9039a992a
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2021-03-01 17:55:28 -0300

    codeclimate

--- desafio15.sql
+++ desafio15.sql
@@ -6,7 +6,7 @@ BEGIN
 SELECT
     ROUND(AVG(t1.salary), 2) AS 'Média salarial'
 FROM employees AS t1
-         JOIN jobs AS t2 ON t1.job_id = t2.job_id
+    JOIN jobs AS t2 ON t1.job_id = t2.job_id
 WHERE Cargo = t2.job_title;
 END $$
 DELIMITER ;
