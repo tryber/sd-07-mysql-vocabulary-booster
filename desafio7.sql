0462afb7482e035acf1fa11cc1f15ce978543f60
Melissa Gomes <mel.ge171201@gmail.com>
2021-03-01 20:37:21 -0300

    7 d

--- desafio7.sql
+++ desafio7.sql
@@ -5,4 +5,5 @@ e.SALARY AS `Salário`
 FROM hr.employees AS `e`
 INNER JOIN hr.job_history AS `h`
 ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
+WHERE MONTH(h.START_DATE) IN (1,2,3)
 ORDER BY `Nome completo`, `Data de início`;
