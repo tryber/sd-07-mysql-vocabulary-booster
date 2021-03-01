8ea23ef34a0a93c7468500f21e242ec5b0a643b9
mbjunqueiraweb <mbjunqueira@gmail.com>
2021-03-01 16:49:16 -0300

    req14 fix

--- desafio14.sql
+++ desafio14.sql
@@ -1,7 +1,7 @@
 SELECT País FROM
 (SELECT Country AS País FROM w3schools.customers AS C
 UNNION ALL
-SELECT Country AS País FROM w3schools.suppliers AS S) AS Dado
+SELECT Country AS País FROM w3schools.suppliers AS S) AS T
 GROUP BY País
 ORDER BY País
 LIMIT 5;
