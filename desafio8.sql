53c5c83ca3cdfaee01ee9beb063aec18f918c2d5
ygorvmdf <ygorvitor.fonseca@gmail.com>
2021-03-01 18:57:10 -0300

    ajustando desafio 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,10 +1,11 @@
 SELECT
-ContactName AS 'Nome de contato',
-s.ShipperName AS 'Nome de contato',
+c.ContactName AS 'Nome de contato',
+s.ShipperName AS 'Empresa que fez o envio',
 o.OrderDate AS 'Data do pedido'
 FROM w3schools.customers c
 JOIN w3schools.orders o
 ON c.CustomerID = o.CustomerID
 JOIN w3schools.shippers s
 ON o.ShipperID = s.ShipperID
+WHERE s.ShipperName IN ('Speedy Express', 'United Package')
 ORDER BY ContactName, s.ShipperName, o.OrderDate;
