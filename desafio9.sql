4a852ada0b93b7bdf3622f2133b9697296105d18
wberilo <wberilo@gmail.com>
2021-03-01 14:20:16 -0300

    f

--- desafio9.sql
+++ desafio9.sql
@@ -1,5 +1,5 @@
 SELECT CONCAT(t2.FirstName,' ',t2.LastName) as `Nome completo`,
-count(t2.EmployeeID)
+count(t2.EmployeeID) as `Total de pedidos`
 FROM orders as t1
 INNER JOIN employees as t2
 on t1.EmployeeID = t2.EmployeeID
