0e87873b69b33751225b7cba9f6dc6e443e19bfb
Emerson Moonlit <emoonlit@pop-os.localdomain>
2021-03-01 17:41:05 -0300

    Desafio5 agora vai

--- desafio5.sql
+++ desafio5.sql
@@ -4,5 +4,5 @@ SELECT
   ROUND((AVG(MIN_SALARY) /12 ), 2) AS 'Média mínima mensal',
   ROUND((AVG(MAX_SALARY) /12 ), 2) AS 'Média máxima mensal'
 FROM hr.jobs
-GROUP BY JOB_TITLE
+GROUP BY JOB_TITLE, 2
 ORDER BY 2, 1;
