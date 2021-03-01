78a5acefe1f8626798977fd2a6e488568a3da18b
wberilo <wberilo@gmail.com>
2021-03-01 17:43:35 -0300

    16

---
+++ desafio16.sql
@@ -0,0 +1,19 @@
+use hr
+DELIMITER $$
+create FUNCTION buscar_quantidade_de_empregos_por_funcionario(email varchar(30))
+RETURNS NUMERIC READS SQL DATA
+BEGIN
+DECLARE total INT;
+select 
+t1.EMPLOYEE_ID, 
+count(t1.EMPLOYEE_ID),
+t2.EMAIL
+from job_history as t1 
+inner join employees as t2
+on t1.EMPLOYEE_ID = t2.EMPLOYEE_ID
+group by EMPLOYEE_ID 
+HAVING t2.email = email
+into total;
+RETURN TOTAL;
+END $$
+DELIMITER ;
