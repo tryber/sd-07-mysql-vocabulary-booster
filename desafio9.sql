647e3337de77427058b9a91a0a2cbf33f5e38c73
MoisesSantana <moisaant@gmail.com>
2021-03-01 13:45:11 -0300

    mais algumas correcoes

--- desafio9.sql
+++ desafio9.sql
@@ -1,9 +1,10 @@
 SELECT
-CONCAT(e.FirstName, ' ', e.LastName),
-COUNT(o.EmployeeID)
+CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo',
+COUNT(o.EmployeeID) AS 'Total de pedidos'
 FROM w3schools.orders AS o
 INNER JOIN
 w3schools.employees AS e
 ON
 e.EmployeeID = o.EmployeeID
-GROUP BY 1;
+GROUP BY 1
+ORDER BY 2;
