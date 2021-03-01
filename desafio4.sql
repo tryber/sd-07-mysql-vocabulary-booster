a82a57d691ccbb1a6d204c6584eb8903504dc8e9
CleberFontinele <cleberfontinele2387@gmail.com>
2021-02-28 22:59:35 -0300

    desafio 3 & 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,9 +1,9 @@
 SELECT J.JOB_TITLE AS Cargo, ROUND(AVG(M.SALARY),2) AS 'Média salarial',
   CASE
-      WHEN AVG(M.SALARY) >= '2000' AND AVG(M.SALARY) <= '5800' THEN 'Júnior'
-      WHEN AVG(M.SALARY) >= '5801' AND AVG(M.SALARY) <= '7500' THEN 'Pleno'
-      WHEN AVG(M.SALARY) >= '7501' AND AVG(M.SALARY) <= '10500' THEN 'Sênior'
-      WHEN AVG(M.SALARY) > '10500' THEN 'CEO'
+      WHEN ROUND(AVG(M.SALARY),2) >= '2000' AND ROUND(AVG(M.SALARY),2) <= '5800' THEN 'Júnior'
+      WHEN ROUND(AVG(M.SALARY),2) >= '5801' AND ROUND(AVG(M.SALARY),2) <= '7500' THEN 'Pleno'
+      WHEN ROUND(AVG(M.SALARY),2) >= '7501' AND ROUND(AVG(M.SALARY),2) <= '10500' THEN 'Sênior'
+      WHEN ROUND(AVG(M.SALARY),2) > '10500' THEN 'CEO'
       END AS "Senioridade"
       FROM hr.jobs AS J
       INNER JOIN hr.employees AS M
