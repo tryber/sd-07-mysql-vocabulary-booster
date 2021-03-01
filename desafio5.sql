4456a8d5228485dcd23636f0dc52c59cd569406b
danielmlduarte <danielduarte.tryber@gmail.com>
2021-03-01 01:56:12 -0300

    Desafio 5

--- desafio5.sql
+++ desafio5.sql
@@ -3,7 +3,7 @@ j.job_title AS Cargo,
 (j.max_salary - j.min_salary) AS 'Variação Salarial',
 ROUND(MIN(e.salary), 2) AS 'Média mínima mensal',
 ROUND(MAX(e.salary), 2) AS 'Média máxima mensal'
-FROM hr.employees e
-LEFT JOIN hr.jobs j ON j.job_id = e.job_id
+FROM hr.jobs j
+LEFT JOIN hr.employees e ON j.job_id = e.job_id
 GROUP BY Cargo
 ORDER BY (j.max_salary - j.min_salary), Cargo;
