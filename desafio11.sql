39e322726c690dc9577fc9a23c7a6afba01c6d5a
Guilherme Almeida <almeidaguilherme204@gmail.com>
2021-03-01 17:08:45 -0300

    Desafio 10

--- desafio11.sql
+++ desafio11.sql
@@ -2,5 +2,6 @@ SELECT C1.ContactName AS "Nome",C1.Country AS "País",COUNT(C1.Country) AS "Núm
 FROM w3schools.customers AS C1
 INNER JOIN w3schools.customers AS C2
 ON C1.Country = C2.Country
+WHERE C1.CustomerID <> C2.CustomerID
 GROUP BY C1.CustomerID
 ORDER BY Nome;
