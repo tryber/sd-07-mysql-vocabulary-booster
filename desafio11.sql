4e7aa779141a26b80888d9257cd1d4d5061aabb3
luc-zago <luc.zago@hotmail.com>
2021-03-01 17:59:27 -0300

    Alterado desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,7 @@
-SELECT c1.ContactName AS "Nome", c1.Country AS "País", COUNT(c2.Country)
-FROM w3schools.customers AS c1, w3schools.customers AS c2
-WHERE c1.Country = c2.Country
-GROUP BY c1.ContactName
+SELECT c1.ContactName AS "Nome", c1.Country AS "País",
+COUNT(c2.Country) AS "Número de compatriotas"
+FROM w3schools.customers AS c1
+JOIN w3schools.customers AS c2
+ON c1.Country = c2.Country AND c1.CustomerID != c2.CustomerID
+GROUP BY c1.ContactName, c1.Country
 ORDER BY c1.ContactName;
