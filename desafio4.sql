d7e47f400b0bd6d3664ab9808ef22993fcafe012
Bruno <bruno_grb@hotmail.com>
2021-03-01 13:04:00 -0300

    corrigindo CEO desafio 4

--- desafio4.sql
+++ desafio4.sql
@@ -5,7 +5,7 @@ CASE
 WHEN ROUND(AVG(employees.SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
 WHEN ROUND(AVG(employees.SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
 WHEN ROUND(AVG(employees.SALARY), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
-WHEN ROUND(AVG(employees.SALARY), 2) > 10500 THEN 'Sênior'
+WHEN ROUND(AVG(employees.SALARY), 2) > 10500 THEN 'CEO'
 END AS 'Senioridade'
 FROM hr.employees AS employees
 INNER JOIN hr.jobs AS jobs
