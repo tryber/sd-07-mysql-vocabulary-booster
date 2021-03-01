34bc710e51888e0c925afff12d046069dacbb709
Lfocosi <luccafocosi@gmail.com>
2021-03-01 19:45:02 -0300

    desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,10 @@
+SELECT p.ProductName as 'Produto',
+min(o.Quantity) as 'Mínima',
+max(o.Quantity) as 'Máxima',
+round(avg(Quantity), 2) as 'Média'
+FROM w3schools.order_details as o
+inner join w3schools.products as p
+on o.ProductID = p.ProductID
+group by o.ProductID
+having `Média` > 20.00
+order by `Média`, 'Produto';
