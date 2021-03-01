62bdd2b48e77ed3c17adcd5b618fb8f81b7b0e88
Anderson Santana <anderslash@gmail.com>
2021-03-01 17:23:49 -0300

    Desafio 10 adicao da media acima de 20

--- desafio10.sql
+++ desafio10.sql
@@ -1,8 +1,9 @@
-SELECT P.ProductName AS "Produto",
-MIN(OD.Quantity) AS "Mínima",
-MAX(OD.Quantity) AS "Máxima",
-ROUND(AVG(OD.Quantity), 2) AS "Média"
+SELECT P.ProductName AS `Produto`,
+MIN(OD.Quantity) AS `Mínima`,
+MAX(OD.Quantity) AS `Máxima`,
+ROUND(AVG(OD.Quantity), 2) AS `Média`
 FROM w3schools.order_details AS OD
 INNER JOIN w3schools.products AS P ON P.ProductID = OD.ProductID
 GROUP BY P.ProductName
-ORDER BY 4, 1;
+HAVING `Média` > 20
+ORDER BY `Média`, `Produto`;
