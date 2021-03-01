afa0aa92e27d3fb3547e8d97e666833189476a00
Bruno <bruno_grb@hotmail.com>
2021-03-01 13:38:34 -0300

    corrigindo desafio 6

--- desafio7.sql
+++ desafio7.sql
@@ -3,20 +3,20 @@ SELECT
 SELECT
 CONCAT(FIRST_NAME, ' ' ,LAST_NAME)
   FROM hr.employees
-  WHERE hr.employees.EMPLOYEE_ID = hr.job_history.EMPLOYEE_ID
+  WHERE employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
 ) AS `Nome completo`,
 (
 SELECT
   JOB_TITLE
   FROM hr.jobs
-  WHERE hr.jobs.JOB_ID = hr.job_history.JOB_ID
+  WHERE jobs.JOB_ID = job_history.JOB_ID
 ) AS `Cargo`,
 START_DATE AS 'Data de início do cargo',
 (
 SELECT
   DEPARTMENT_NAME
   FROM hr.departments
-  WHERE hr.departments.DEPARTMENT_ID = hr.job_history.DEPARTMENT_ID
+  WHERE departments.DEPARTMENT_ID = job_history.DEPARTMENT_ID
 ) AS 'Departamento'
 FROM hr.job_history
 ORDER BY `Nome completo` DESC, `Cargo` ASC;
