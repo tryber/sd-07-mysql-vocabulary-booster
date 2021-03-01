a2288f90e67760747e5e0f97740915bb87d8140d
vitorAmorims <vtamorims@gmail.com>
2021-03-01 18:03:54 -0300

    desafio10.sql

---
+++ desafio10.sql
@@ -0,0 +1,12 @@
+
+SELECT 
+  products.ProductName AS "Produto",
+  MIN(order_details.Quantity) AS "Mínima",
+  MAX(order_details.Quantity) AS "Máxima",
+  ROUND(AVG(order_details.Quantity), 2) AS "Média"
+FROM order_details
+JOIN products
+ON order_details.ProductID = products.ProductID
+GROUP BY products.ProductID
+HAVING `Média` > 20
+ORDER BY Média;
