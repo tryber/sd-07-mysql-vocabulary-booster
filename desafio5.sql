6b6620f422e0a0b6215128dad580bbd035c99d87
berchon <berchon@gmail.com>
2021-03-01 17:54:21 -0300

    Desafios 1 ao 5

---
+++ desafio5.sql
@@ -0,0 +1,23 @@
+-- Considere os salários máximo e mínimo de um cargo como salários anuais.
+-- Use o banco hr como referência
+-- Crie uma query que exiba 04 colunas:
+-- 		1. A primeira deve possuir o alias "Cargo" e exibir o nome do cargo.
+-- 		2. A segunda deve possuir o alias "Variação Salarial" e exibir a diferença entre os salários máximo e mínimo daquele cargo.
+-- 		3. A terceira deve possuir o alias "Média mínima mensal" e exibir o salário mínimo mensal daquele cargo. Arredonde a média
+-- 			com uma precisão de duas casas decimais.
+-- 		4. A quarta deve possuir o alias "Média máxima mensal" e exibir o salário máximo mensal daquele cargo. Arredonde a média com
+-- 			uma precisão de duas casas decimais.
+-- Os resultados devem estar ordenados pela variação salarial em ordem crescente.
+-- Em caso de empate na variação, os resultados devem ser ordenados pelo nome do cargo em ordem alfabética.
+
+SELECT
+    jobs.JOB_TITLE AS 'Cargo',
+    ROUND(jobs.MAX_SALARY - jobs.MIN_SALARY, 2) AS `Variação Salarial`,
+    ROUND(jobs.MIN_SALARY / 12, 2) AS `Média mínima mensal`,
+    ROUND(jobs.MAX_SALARY / 12, 2) AS `Média máxima mensal`
+FROM hr.jobs AS jobs
+ORDER BY `Variação Salarial` ASC, Cargo ASC;
+
+-- GROUP BY employees.JOB_ID
+-- INNER JOIN hr.jobs AS jobs
+-- ON employees.JOB_ID = jobs.JOB_ID
--- tests/hrChallenges.spec.js
+++ tests/hrChallenges.spec.js
@@ -31,7 +31,7 @@ describe('Desafios iniciais', () => {
     sequelize.close();
   });
 
-  describe('Exiba os países e indicando se cada um deles se encontra ou não na região formada pela Europa', () => {
+  describe.only('Exiba os países e indicando se cada um deles se encontra ou não na região formada pela Europa', () => {
     it('Verifica o desafio 1', async () => {
       const challengeQuery = readFileSync('desafio1.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult1');
@@ -40,7 +40,7 @@ describe('Desafios iniciais', () => {
     });
   });
 
-  describe('Exiba os cargos com seu nível de renumeração associado, com base no salário máximo do cargo', () => {
+  describe.only('Exiba os cargos com seu nível de renumeração associado, com base no salário máximo do cargo', () => {
     it('Verifica o desafio 2', async () => {
       const challengeQuery = readFileSync('desafio2.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult2');
@@ -49,7 +49,7 @@ describe('Desafios iniciais', () => {
     });
   });
 
-  describe('Exiba os cargos com a diferença entre seus salários máximo e mínimo', () => {
+  describe.only('Exiba os cargos com a diferença entre seus salários máximo e mínimo', () => {
     it('Verifica o desafio 3', async () => {
       const challengeQuery = readFileSync('desafio3.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult3');
@@ -58,7 +58,7 @@ describe('Desafios iniciais', () => {
     });
   });
 
-  describe('Exiba a média salarial e o nível de senioridade de todas as pessoas empregadas, agrupadas pelo cargo', () => {
+  describe.only('Exiba a média salarial e o nível de senioridade de todas as pessoas empregadas, agrupadas pelo cargo', () => {
     it('Verifica o desafio 4', async () => {
       const challengeQuery = readFileSync('desafio4.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult4');
@@ -67,7 +67,7 @@ describe('Desafios iniciais', () => {
     });
   });
 
-  describe('Exiba os cargos com sua variação salarial e suas médias máxima e mínima mensal, considerando salários máximo e minímo como anuais', () => {
+  describe.only('Exiba os cargos com sua variação salarial e suas médias máxima e mínima mensal, considerando salários máximo e minímo como anuais', () => {
     it('Verifica o desafio 5', async () => {
       const challengeQuery = readFileSync('desafio5.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult5');
