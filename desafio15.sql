eed1e97394a19a189523573465fcafbd8879f7fb
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-03-01 19:03:39 -0300

    Desafio 15: Arrumando Letra maiscula

--- desafio15.sql
+++ desafio15.sql
@@ -3,7 +3,7 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo_param VARCHAR(50))
 BEGIN
-SELECT ROUND(AVG(e.SALARY), 2) AS `Média Salarial`
+SELECT ROUND(AVG(e.SALARY), 2) AS `Média salarial`
 FROM employees AS `e`
 INNER JOIN jobs AS `j`
 ON e.JOB_ID = j.JOB_ID
