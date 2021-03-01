af478ec1c39da2ba4d5ed1f199bf046620ca20e4
danielmlduarte <danielduarte.tryber@gmail.com>
2021-03-01 20:28:07 -0300

    Desafio 16 - Corrigido para Function

--- desafio16.sql
+++ desafio16.sql
@@ -1,19 +1,19 @@
 USE hr;
 DELIMITER $$
 
-CREATE PROCEDURE buscar_quantidade_de_empregos_por_funcionario(
-IN email_funcionario VARCHAR(50),
-OUT quantidade_empregos DOUBLE)
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(
+email_funcionario VARCHAR(50))
+RETURNS INT READS SQL DATA
 BEGIN
+DECLARE quantidade_empregos INT;
 SELECT
 COUNT(j.employee_id) INTO quantidade_empregos
 FROM hr.employees e
 LEFT JOIN hr.job_history j ON e.employee_id = j.employee_id
 WHERE e.email = email_funcionario;
+RETURN quantidade_empregos;
 END $$
 
 DELIMITER ;
 
-
-CALL buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR', @quantidade_empregos);
-SELECT @quantidade_empregos;
+SELECT buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR');
