e7ab152ecf75860166703e414fab649144111c7c
rodrigoderkian <derkianpromo@gmail.com>
2021-03-01 18:51:48 -0300

    identaçao do wb

--- desafio6.sql
+++ desafio6.sql
@@ -1,5 +1,5 @@
 SELECT 
-    CONCAT(employ.FIRST_NAME, ' ', employ.LAST_NAME) AS 'Nome completo',
+    CONCAT(employ.FIRST_NAME, ' ', employ.LAST_NAME) AS `Nome completo`,
     job.JOB_TITLE AS 'Cargo',
     history.START_DATE AS 'Data de início do cargo',
     department.DEPARTMENT_NAME AS 'Departamento'
@@ -11,4 +11,4 @@ FROM
     hr.jobs AS job ON job.JOB_ID = history.JOB_ID
         INNER JOIN
     hr.departments AS department ON department.DEPARTMENT_ID = history.DEPARTMENT_ID
-ORDER BY 'Nome completo' DESC , 'Cargo' ASC;
+ORDER BY `Nome completo` DESC , `Cargo` ASC;
