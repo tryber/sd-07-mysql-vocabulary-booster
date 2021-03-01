0f5eafdba300a882f624c82e4f70b586e7a0b6b1
hvenses <hvenses@gmail.com>
2021-03-01 16:27:30 -0300

    Retirando DESC da ordenacao no req8

--- desafio8.sql
+++ desafio8.sql
@@ -4,4 +4,4 @@ o.OrderDate as 'Data do pedido' FROM w3schools.customers AS c
 INNER JOIN w3schools.orders AS o ON c.CustomerID = o.CustomerID
 INNER JOIN w3schools.shippers AS s ON o.ShipperID = s.ShipperID
 WHERE s.ShipperName IN('Speedy Express', 'United Package')
-ORDER BY c.ContactName, s.ShipperName, o.OrderDate DESC;
+ORDER BY c.ContactName, s.ShipperName, o.OrderDate;
