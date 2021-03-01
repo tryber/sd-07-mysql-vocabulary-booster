bf125c6b6ac6da42ffed6154b3a155df41e1ad06
Vitor Rodrigues <vitor_rc@outlook.com>
2021-03-01 16:12:05 -0300

    Requisito 5

---
+++ desafio5.sql
@@ -0,0 +1,7 @@
+SELECT 
+JOB_TITLE AS Cargo,
+MAX_SALARY - MIN_SALARY AS `Variação Salarial`,
+MIN_SALARY / 12 AS `Média mínima mensal`,
+MAX_SALARY / 12 AS `Média máxima mensal`
+FROM hr.jobs
+ORDER BY `Variação Salarial`, Cargo;
