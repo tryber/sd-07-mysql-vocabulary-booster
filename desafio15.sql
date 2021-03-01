b7319e536a96d704ebf4b32eea716f4cbb07b1fb
wberilo <wberilo@gmail.com>
2021-03-01 16:59:43 -0300

    15

--- desafio15.sql
+++ desafio15.sql
@@ -2,12 +2,13 @@ use hr
 DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(50))
 BEGIN
-  SELECT
-  cast(AVG(t1.SALARY) as decimal(10,2)) as `Média salarial`
+  SELECT `Média salarial` FROM (SELECT
+  cast(AVG(t1.SALARY) as decimal(10,2)) as `Média salarial`,
+  t2.job_title
   from employees as t1
   INNER JOIN jobs as t2 
   on t1.job_id = t2.job_id
   group by t1.job_id
-  HAVING t2.JOB_TITLE = cargo;
+  HAVING t2.JOB_TITLE = 'Programmer') as T;
 END $$
 DELIMITER ;
