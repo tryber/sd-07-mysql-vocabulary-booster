ed81e5c167ebb1e93253523821c0637470b6b087
ygorvmdf <ygorvitor.fonseca@gmail.com>
2021-03-01 19:57:47 -0300

    ajustando desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -2,4 +2,4 @@ SELECT
 c1.CustomerName AS 'Nome',
 c2.Country AS 'País',
 COUNT(*) - 1 AS 'Número de compatriotas' FROM w3schools.customers c1
-JOIN w3schools.customers c2 ON c1.Country = c2.Country GROUP BY c1.CustomerName;
+JOIN w3schools.customers c2 ON c1.Country = c2.Country GROUP BY c1.CustomerName HAVING `Número de compatriotas` > 0;
