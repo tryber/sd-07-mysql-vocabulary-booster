524b44420301e3d8905f811183cbf9733f96d6b3
Matheus Beaumord <matheusbeaumord@gmail.com>
2021-03-01 17:07:15 -0300

    Envio do requisito 1 para teste

---
+++ desafio1.sql
@@ -0,0 +1,9 @@
+SELECT c.COUNTRY_NAME 'País',
+CASE 
+ WHEN r.REGION_NAME like 'Europe' THEN 'incluído'
+ ELSE 'não incluído'
+ END AS 'Status Inclusão'
+FROM hr.countries c
+LEFT JOIN hr.regions r
+ON c.REGION_ID = r.REGION_ID
+ORDER BY c.COUNTRY_NAME;
