cb462f76a4f3e8150b64175de077222c0964b15d
MoisesSantana <moisaant@gmail.com>
2021-03-01 14:06:22 -0300

    chega de correcoes por favor

--- desafio5.sql
+++ desafio5.sql
@@ -4,6 +4,4 @@ ROUND(MAX_SALARY - MIN_SALARY, 2) AS 'Variação Salarial',
 ROUND(MIN_SALARY / 12, 2) AS 'Média mínima mensal',
 ROUND(MAX_SALARY / 12, 2) AS 'Média máxima mensal'
 FROM hr.jobs
-GROUP BY
-Cargo
 ORDER BY 2, 1;
