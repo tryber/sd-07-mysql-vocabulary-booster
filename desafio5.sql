e0b0eb7be810b0def5a5159b50f6de1344b7cfb6
danielmlduarte <danielduarte.tryber@gmail.com>
2021-03-01 14:46:06 -0300

    Desafio 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,9 +1,7 @@
 SELECT
 j.job_title AS Cargo,
-(j.max_salary - j.min_salary) AS 'Variação Salarial',
-ROUND(MIN(e.salary), 2) AS 'Média mínima mensal',
-ROUND(MAX(e.salary), 2) AS 'Média máxima mensal'
+j.max_salary - j.min_salary AS 'Variação Salarial',
+ROUND(j.min_salary / 12, 2) AS 'Média mínima mensal',
+ROUND(j.max_salary / 12, 2) AS 'Média máxima mensal'
 FROM hr.jobs j
-LEFT JOIN hr.employees e ON e.job_id = j.job_id
-GROUP BY Cargo
 ORDER BY 2, Cargo;
