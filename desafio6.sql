6dbdeb6f1d66fb79400edd26dc67c8fe4102db04
ygorvmdf <ygorvitor.fonseca@gmail.com>
2021-03-01 17:05:59 -0300

    desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,11 @@
+SELECT
+CONCAT(first_name, ' ', last_name) AS 'Nome completo',
+j.job_title AS 'Cargo',
+e.hire_date AS 'Data de início do cargo',
+d.department_name AS 'Departamento'
+FROM hr.employees e
+JOIN hr.jobs j
+ON e.job_id = j.job_id
+JOIN hr.departments d
+ON e.department_id = d.department_id
+ORDER BY CONCAT(first_name, ' ', last_name) DESC, j.job_title;
