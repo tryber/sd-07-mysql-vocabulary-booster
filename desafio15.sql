8497035a1c202412a6845f77484271e83622455c
wberilo <wberilo@gmail.com>
2021-03-01 16:42:44 -0300

    f

--- desafio15.sql
+++ desafio15.sql
@@ -3,8 +3,7 @@ DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(50))
 BEGIN
   SELECT
-  t2.job_title,
-  cast(AVG(t1.SALARY) as decimal(10,2))
+  cast(AVG(t1.SALARY) as decimal(10,2)) as `Média Salarial`
   from employees as t1
   INNER JOIN jobs as t2 
   on t1.job_id = t2.job_id
