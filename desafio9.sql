803bf8786823245fbf9163cb3de2491859bf86d7
mbjunqueiraweb <mbjunqueira@gmail.com>
2021-03-01 15:26:36 -0300

    req9 fix3

--- desafio9.sql
+++ desafio9.sql
@@ -1,5 +1,5 @@
-SELECT CONCAT(FirstName, ' ', LastName) AS 'Nome Completo', Count(O.EmployeeID) AS 'Total de pedidos'
+SELECT CONCAT(FirstName,' ',LastName) AS 'Nome completo', Count(O.EmployeeID) AS 'Total de pedidos'
 FROM w3schools.employees AS E
 INNER JOIN w3schools.orders AS O ON E.EmployeeID = O.EmployeeID
-GROUP BY CONCAT(FirstName, ' ', LastName)
-ORDER BY COUNT(O.EmployeeID);
+GROUP BY CONCAT(FirstName,' ',LastName)
+ORDER BY Count(O.EmployeeID);
