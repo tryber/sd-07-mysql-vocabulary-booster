237ac059521fdc942abedfbdceab2e5aa275b911
Muniz <vinicius.muniz.marin@gmail.com>
2021-03-01 19:33:10 -0300

    desafio4 fix

--- desafio4.sql
+++ desafio4.sql
@@ -1,19 +1,20 @@
 SELECT j.job_title AS 'Cargo',
-ROUND(e.salary, 2) AS 'Média salarial',
+ROUND(AVG(e.salary), 2) AS 'Média salarial',
 CASE
-WHEN e.salary  >= 2000
-AND e.salary  <= 5800
+WHEN AVG(e.salary)  >= 2000
+AND AVG(e.salary)  <= 5800
 THEN 'Júnior'
-WHEN e.salary  >= 5801
-AND e.salary  <= 7500
+WHEN AVG(e.salary)  >= 5801
+AND AVG(e.salary)  <= 7500
 THEN 'Pleno'
-WHEN e.salary  >= 7501
-AND e.salary  <= 10500
+WHEN AVG(e.salary)  >= 7501
+AND AVG(e.salary)  <= 10500
 THEN 'Sênior'
-WHEN e.salary  > 10500
+WHEN AVG(e.salary)  > 10500
 THEN 'CEO'
 END AS 'Senioridade'
 FROM employees e
 INNER JOIN jobs j
 ON e.job_id = j.job_id
-ORDER BY e.salary, j.job_title;
+GROUP BY job_title
+ORDER BY AVG(e.salary), j.job_title;
