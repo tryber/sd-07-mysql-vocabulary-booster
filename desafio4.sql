571d82f1bf7f743e042f0e04aaa98cdaa9c94fa0
Guilherme Almeida <almeidaguilherme204@gmail.com>
2021-03-01 13:40:03 -0300

    Desafio 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,10 +1,10 @@
 SELECT cargo.JOB_TITLE AS Cargo,ROUND(AVG(employe.SALARY), 2) AS "Média salarial",
 CASE
-   WHEN AVG(employe.SALARY) >=2000 AND AVG(employe.SALARY) <= 5800 THEN 'Júnior'
-   WHEN AVG(employe.SALARY) > 5800 AND AVG(employe.SALARY) <= 7500 THEN 'Pleno'
-   WHEN AVG(employe.SALARY) > 7500 AND AVG(employe.SALARY) <= 10500 THEN 'Sênior'
-   WHEN AVG(employe.SALARY) >= 10500 THEN 'CEO'
-   END AS Senioridade
+WHEN AVG(employe.SALARY) >=2000 AND AVG(employe.SALARY) <= 5800 THEN 'Júnior'
+WHEN AVG(employe.SALARY) > 5800 AND AVG(employe.SALARY) <= 7500 THEN 'Pleno'
+WHEN AVG(employe.SALARY) > 7500 AND AVG(employe.SALARY) <= 10500 THEN 'Sênior'
+WHEN AVG(employe.SALARY) >= 10500 THEN 'CEO'
+END AS Senioridade
 FROM hr.jobs AS cargo
 INNER JOIN hr.employees AS employe
 ON cargo.JOB_ID = employe.JOB_ID
