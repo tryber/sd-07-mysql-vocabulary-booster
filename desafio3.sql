6fcb4593bb37db02d580a2823092acc30576333c
ygorvmdf <ygorvitor.fonseca@gmail.com>
2021-03-01 16:12:29 -0300

    consertando desafio 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,2 +1,2 @@
-SELECT job_title, (max_salary - min_salary) AS 'Diferença entre salários máximo e mínimo' FROM hr.jobs
+SELECT job_title AS 'Cargo', (max_salary - min_salary) AS 'Diferença entre salários máximo e mínimo' FROM hr.jobs
 ORDER BY (max_salary - min_salary), job_title;
