e8003d561c05a043e9ac87545bad901dd054dbf2
luc-zago <luc.zago@hotmail.com>
2021-03-01 16:33:02 -0300

    Alteração no desafio 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,7 +1,7 @@
 SELECT job_title AS "Cargo",
 CASE
 WHEN max_salary >= 5000 AND max_salary <= 10000 THEN "Baixo"
-WHEN max_salary > 10000 AND max_salary <= 20000 THEN "Medio"
+WHEN max_salary > 10000 AND max_salary <= 20000 THEN "Médio"
 WHEN max_salary > 20000 AND max_salary <= 30000  THEN "Alto"
 WHEN max_salary > 30000 THEN "Altíssimo"
 END AS "Nível"
