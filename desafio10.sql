6371084752303201129635b6b1b622cb9d894064
Guilherme Almeida <almeidaguilherme204@gmail.com>
2021-03-01 16:29:46 -0300

    Desafio 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,6 +1,6 @@
 SELECT products.ProductName AS Produto ,MIN(order_data.Quantity) AS "Mínima",
 MAX(order_data.Quantity) AS "Máxima",
-ROUND(AVG(order_data.Quantity),2) AS "Média"
+ROUND(AVG(order_data.Quantity)) AS "Média"
 FROM w3schools.order_details AS order_data
 INNER JOIN w3schools.products AS products
 ON products.ProductID = order_data.ProductID
