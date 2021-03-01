2b27e9d0064679cc3413a364dbe78c26010dd625
mbjunqueiraweb <mbjunqueira@gmail.com>
2021-03-01 15:09:49 -0300

    reqs8,9 fix

--- desafio8.sql
+++ desafio8.sql
@@ -1,11 +1,10 @@
-SELECT 
-C.CustomerName as `Nome de contato`,
-S.ShipperName as `Empresa que fez o envio`,
-O.OrderDate as `Data do pedido`
-FROM orders as O
-INNER JOIN customers as C
-on O.CustomerID = C.CustomerID
-INNER JOIN shippers as S
-on O.ShipperID = S.ShipperID
-WHERE S.ShipperName IN ('Speedy Express', 'United Package')
-ORDER BY C.CustomerName, S.ShipperName, O.OrderDate;
+SELECT C.ContactName AS 'Nome de contato',
+S.ShipperName AS 'Empresa que fez o envio',
+O.OrderDate AS 'Data do pedido'
+FROM w3schools.customers AS C
+INNER JOIN w3schools.orders AS O ON C.CustomerID = O.CustomerID
+INNER JOIN w3schools.shippers S ON O.ShipperID = S.ShipperID
+WHERE S.ShipperName LIKE 'Speedy Express' OR S.ShipperName LIKE 'United Package'
+ORDER BY C.ContactName ASC,
+S.ShipperName ASC, 
+O.OrderDate ASC;
--- desafio9.sql
+++ desafio9.sql
@@ -1,5 +1,5 @@
-SELECT CONCAT(First_Name, ' ', Last_Name) AS 'Nome Completo', Count(O.EmployeeID) AS 'Total de pedidos'
+SELECT CONCAT(FirstName, ' ', LastName) AS 'Nome Completo', Count(O.EmployeeID) AS 'Total de pedidos'
 FROM w3schools.employees AS E
 INNER JOIN w3schools.orders AS O ON E.Employeed_ID = O.Employeed_ID
-GROUP BY CONCAT(First_Name, ' ', Last_Name)
+GROUP BY CONCAT(FirstName, ' ', LastName)
 ORDER BY COUNT(O.Employeed_ID);
