270aa96598f6246a9a8b8b37eda1cb76e4e9753f
Melissa Gomes <mel.ge171201@gmail.com>
2021-03-01 20:28:23 -0300

    aaa

--- desafio7.sql
+++ desafio7.sql
@@ -2,7 +2,7 @@ SELECT
 UCASE(CONCAT (e.FIRST_NAME, " ", e.LAST_NAME)) AS `Nome completo`,
 h.START_DATE AS `Data de início`,
 e.SALARY AS `Salário`
-FROM hr.employees AS e
-INNER JOIN hr.job_history AS h
+FROM hr.employees AS `e`
+INNER JOIN hr.job_history AS `h`
 ON e.JOB_ID = h.JOB_ID
-ORDER BY `Nome completo`, `Data de início` DESC;
+ORDER BY `Nome completo`, `Data de início`;
