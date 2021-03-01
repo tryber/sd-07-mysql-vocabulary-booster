f7e468d262d41c82675d6ae9156d8c0d60264768
nyxnil <nsmnilsonn@gmail.com>
2021-03-01 20:10:26 -0300

    projeto vocabulary booster 0.2

--- desafio4.sql
+++ desafio4.sql
@@ -1,1 +1,6 @@
-Select JOB_TITLE as 'Cargo', round((MAX_SALARY + MIN_SALARY) / 2) as 'Diferença entre salários máximo e mínimo' from hr.jobs order by JOB_TITLE;
+Select JOB_TITLE as 'Cargo', round((MAX_SALARY + MIN_SALARY) / 2) as 'Média salarial', case
+when round((MAX_SALARY + MIN_SALARY) / 2) between 2000 and 5800 then 'Júnior'
+when round((MAX_SALARY + MIN_SALARY) / 2) between 5801 and 7500 then 'Pleno'
+when round((MAX_SALARY + MIN_SALARY) / 2) between 7501 and 10500 then 'Sênior'
+when round((MAX_SALARY + MIN_SALARY) / 2) > 10500 then 'CEO'
+end as 'Senioridade' from hr.jobs order by JOB_TITLE;
