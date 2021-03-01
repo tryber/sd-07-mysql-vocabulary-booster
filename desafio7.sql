14b12f7cdca9358c58e63e272f38bc21a5f4e103
alvarovascon <alvarovascon2@gmail.com>
2021-03-01 17:07:26 -0300

    desafio 7 - CORREÇÃO

--- desafio7.sql
+++ desafio7.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS 'Nome completo',
+SELECT UCASE(CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME)) AS 'Nome completo',
 JH.START_DATE AS 'Data de início do cargo',
 E.SALARY AS 'Salário'
 FROM hr.employees AS E
