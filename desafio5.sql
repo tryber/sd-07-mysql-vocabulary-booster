8f4f64bdafafbb00b0b89c992a3d4a77edd5e4ab
Bruno <bruno_grb@hotmail.com>
2021-03-01 19:31:22 -0300

    adicionando desafios prontos

--- desafio5.sql
+++ desafio5.sql
@@ -1,6 +1,6 @@
 SELECT
 JOB_TITLE AS 'Cargo',
-MAX_SALARY - MIN_SALARY AS 'Variação Salarial',
+MAX_SALARY - MIN_SALARY AS `Variação Salarial`,
 ROUND(MIN_SALARY / 12, 2) AS 'Média mínima mensal',
 ROUND(MAX_SALARY / 12, 2) AS 'Média máxima mensal'
-FROM hr.jobs ORDER BY (MAX_SALARY - MIN_SALARY) ASC;
+FROM hr.jobs ORDER BY `Variação Salarial`, JOB_TITLE;
--- desafio6.sql
+++ desafio6.sql
@@ -1,22 +1,12 @@
-SELECT
-(
-SELECT
-CONCAT(FIRST_NAME, ' ' ,LAST_NAME)
-  FROM hr.employees
-  WHERE hr.employees.EMPLOYEE_ID = hr.job_history.EMPLOYEE_ID
-) AS `Nome completo`,
-(
-SELECT
-  JOB_TITLE
-  FROM hr.jobs
-  WHERE hr.jobs.JOB_ID = hr.job_history.JOB_ID
-) AS `Cargo`,
-START_DATE AS 'Data de início do cargo',
-(
-SELECT
-  DEPARTMENT_NAME
-  FROM hr.departments
-  WHERE hr.departments.DEPARTMENT_ID = hr.job_history.DEPARTMENT_ID
-) AS 'Departamento'
-FROM hr.job_history
+SELECT CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME) AS `Nome completo`,
+jobs.JOB_TITLE AS `Cargo`,
+job_history.START_DATE AS 'Data de início do cargo',
+departments.DEPARTMENT_NAME AS 'Departamento'
+FROM hr.job_history AS job_history
+INNER JOIN hr.employees
+ON job_history.EMPLOYEE_ID = employees.EMPLOYEE_ID
+INNER JOIN hr.jobs AS jobs
+ON jobs.JOB_ID = job_history.JOB_ID
+INNER JOIN hr.departments AS departments
+ON departments.DEPARTMENT_ID = job_history.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC, `Cargo` ASC;
