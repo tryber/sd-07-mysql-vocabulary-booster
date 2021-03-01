891deb8182ddceb22c67f97873c688cd03b3ae14
luc-zago <luc.zago@hotmail.com>
2021-03-01 16:49:39 -0300

    Corrigido desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,15 +1,8 @@
-
-SELECT CONCAT(e.first_name," ", e.last_name) AS "Nome completo",
-j.job_title AS "Cargo",
-CASE
-WHEN e.job_id = h.job_id THEN h.start_date
-WHEN e.job_id != h.job_id THEN MAX(DATE_ADD(h.end_date, INTERVAL 1 DAY))
-ELSE e.hire_date
-END AS "Data de início do cargo",
+SELECT CONCAT(e.first_name, " ", e.last_name) AS "Nome completo",
+j.job_title AS "Cargo", jh.start_date AS "Data de início do cargo",
 d.department_name AS "Departamento"
-FROM hr.employees AS e
-LEFT JOIN hr.jobs AS j ON e.job_id = j.job_id
-LEFT JOIN hr.job_history AS h ON e.employee_id = h.employee_id
-LEFT JOIN hr.departments AS d ON e.department_id = d.department_id
-GROUP BY e.employee_id
-ORDER BY CONCAT(e.first_name," ", e.last_name) DESC, j.job_title;
+FROM hr.job_history AS jh
+JOIN hr.employees AS e ON e.employee_id = jh.employee_id
+JOIN hr.jobs AS j ON j.job_id = jh.job_id
+JOIN hr.departments AS d ON d.department_id = jh.department_id
+ORDER BY CONCAT(e.first_name, " ", e.last_name) DESC, j.job_title;
