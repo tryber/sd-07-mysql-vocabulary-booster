a461fdf98ce783afb35b97409ee90c08b075873a
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-03-01 18:33:28 -0300

    ajustes no desafios 02 para aprovação no LINT

--- desafio4.sql
+++ desafio4.sql
@@ -1,8 +1,8 @@
 SELECT job.job_title AS "Cargo", ROUND(AVG(employees.salary), 2) AS "Média salarial",
 	CASE
-		WHEN AVG(salary) >= 2000 AND AVG(salary)<= 5800 THEN "Júnior"
-		WHEN AVG(salary) >= 5801  AND AVG(salary)<= 7500 THEN "Pleno"
-		WHEN AVG(salary) >= 7501 AND AVG(salary)<= 10500 THEN "Sênior"
+		WHEN AVG(salary) BETWEEN 2000 AND 5800 THEN "Júnior"
+		WHEN AVG(salary) BETWEEN 5801  AND 7500 THEN "Pleno"
+		WHEN AVG(salary) BETWEEN 7501 AND 10500 THEN "Sênior"
 		WHEN AVG(salary) > 10500 THEN "CEO"
 	END AS "Senioridade"
 FROM hr.jobs AS job
