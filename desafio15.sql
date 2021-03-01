202bfbf3be4c45bcc79647ae99cc010e63292925
luc-zago <luc.zago@hotmail.com>
2021-03-01 17:26:25 -0300

    Alterado desafio 15

--- desafio15.sql
+++ desafio15.sql
@@ -2,7 +2,7 @@ USE hr;
 DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(IN job VARCHAR(50))
 BEGIN
-SELECT AVG(e.salary)
+SELECT ROUND(AVG(e.salary), 2)
 FROM hr.employees AS e
 JOIN hr.jobs as j ON e.job_id = j.job_id AND j.job_title = job; 
 END $$
