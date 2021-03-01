e722ef49a7de86fffe7b9c7181b628fc39f273e6
wberilo <wberilo@gmail.com>
2021-03-01 15:34:44 -0300

    f

--- desafio11.sql
+++ desafio11.sql
@@ -2,7 +2,7 @@ SELECT
 t1.ContactName as Nome,
 t1.Country as País,
 COUNT(t2.Country) as `Número de compatriotas`
-from customers t1, customers t2
+from w3schools.customers t1, w3schools.customers t2
 where t1.Country = t2.Country AND t1.CustomerID != t2.CustomerID
 group by t1.ContactName
 order by t1.ContactName
