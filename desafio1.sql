8a99bfcd6e5f9cfb109acc971aa06fb3ba6d0686
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2021-03-01 19:28:31 -0300

    Requirement 1 correction

--- desafio1.sql
+++ desafio1.sql
@@ -1,6 +1,6 @@
-SELECT co.COUNTRY_NAME AS 'País',
+SELECT co.COUNTRY_NAME AS País,
 IF (re.REGION_NAME = 'Europe', 'incluído', 'não incluído') AS 'Status Inclusão'
 FROM hr.countries AS co
 INNER JOIN hr.regions AS re
 ON re.REGION_ID = co.REGION_ID
-ORDER BY 'País';
+ORDER BY 1;
