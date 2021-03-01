6e7c9a582b45af8423bdfd6f47ecb7a0abd42385
nevesfreire <nando.neves@gmail.com>
2021-03-01 19:57:28 -0300

    teste

--- desafio5.sql
+++ desafio5.sql
@@ -3,5 +3,5 @@ JOB_TITLE AS Cargo,
 MAX_SALARY - MIN_SALARY AS 'Variação Salarial',
 round(MIN_SALARY/12,2) AS 'Média mínima mensal',
 round(MAX_SALARY/12,2) AS 'Média máxima mensal'
- from hr.jobs 
- ORDER BY MAX_SALARY - MIN_SALARY, JOB_TITLE;
+from hr.jobs 
+ORDER BY MAX_SALARY - MIN_SALARY, JOB_TITLE;
