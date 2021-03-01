7d948ae20d7d6ef2b7c5c435f7454d825d4b9e62
luc-zago <luc.zago@hotmail.com>
2021-03-01 18:40:44 -0300

    Alterado desafio 14

--- desafio14.sql
+++ desafio14.sql
@@ -1,6 +1,5 @@
-SELECT c.Country AS "País"
-FROM w3schools.customers AS c
-INNER JOIN w3schools.suppliers AS s ON c.Country != s.Country
-GROUP BY c.Country
-ORDER BY c.Country
+SELECT c.Country AS "País" FROM w3schools.customers AS c
+UNION
+SELECT s.Country AS "País" FROM w3schools.suppliers AS s
+ORDER BY `País`
 LIMIT 5;
