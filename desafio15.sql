08b194abd9b5dee5d9270b250e0720ca093fbbe8
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-03-01 18:59:58 -0300

    Desafio 15: Code Climate

--- desafio15.sql
+++ desafio15.sql
@@ -3,11 +3,11 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo_param VARCHAR(50))
 BEGIN
-	SELECT ROUND(AVG(e.SALARY), 2) AS `Média Salarial`
-	FROM employees AS `e`
-	INNER JOIN jobs AS `j`
-	ON e.JOB_ID = j.JOB_ID
-	WHERE j.JOB_TITLE = cargo_param;
+SELECT ROUND(AVG(e.SALARY), 2) AS `Média Salarial`
+FROM employees AS `e`
+INNER JOIN jobs AS `j`
+ON e.JOB_ID = j.JOB_ID
+WHERE j.JOB_TITLE = cargo_param;
 END $$
 
 DELIMITER ;
