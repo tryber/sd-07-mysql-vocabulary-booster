05f55e5e4b21e70cce62d8a7967bc6f8add96222
wberilo <wberilo@gmail.com>
2021-03-01 15:47:28 -0300

    fix

--- desafio11.sql
+++ desafio11.sql
@@ -4,5 +4,5 @@ t1.Country as País,
 COUNT(t2.Country) as `Número de compatriotas`
 from w3schools.customers as t1, w3schools.customers as t2
 where t1.Country = t2.Country AND t1.CustomerID != t2.CustomerID
-group by t1.ContactName
-order by t1.ContactName;
+group by t1.CustomerID
+order by t1.ContactName
