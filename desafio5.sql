48f587b67483ce08e98dbe0c3f4e042518fa6cda
berchon <berchon@gmail.com>
2021-03-01 18:53:09 -0300

    Desafios 1 ao 5 - Arrumando erro no 5

--- desafio5.sql
+++ desafio5.sql
@@ -12,8 +12,11 @@
 
 SELECT
 jobs.JOB_TITLE AS 'Cargo',
-ROUND(jobs.MAX_SALARY - jobs.MIN_SALARY, 2) AS `Variação Salarial`,
+jobs.MAX_SALARY - jobs.MIN_SALARY AS `Variação Salarial`,
 ROUND(jobs.MIN_SALARY / 12, 2) AS `Média mínima mensal`,
 ROUND(jobs.MAX_SALARY / 12, 2) AS `Média máxima mensal`
 FROM hr.jobs AS jobs
+INNER JOIN hr.employees AS employees
+ON jobs.JOB_ID = employees.JOB_ID
+GROUP BY jobs.JOB_ID
 ORDER BY `Variação Salarial` ASC, Cargo ASC;
