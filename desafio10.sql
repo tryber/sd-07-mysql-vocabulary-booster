0051721b8725c0ea8f98ea1ca17202e00cd5c6fb
Guilherme Almeida <almeidaguilherme204@gmail.com>
2021-03-01 16:19:14 -0300

    Desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,9 @@
+SELECT products.ProductName AS Produto ,MIN(order_data.Quantity) AS "Mínima",
+MAX(order_data.Quantity) AS "Máxima",
+AVG(order_data.Quantity) AS "Média"
+FROM w3schools.order_details AS order_data
+INNER JOIN w3schools.products AS products
+ON products.ProductID = order_data.ProductID
+WHERE order_data.Quantity > 20.00
+GROUP BY order_data.ProductID
+ORDER BY `Média`,Produto;
