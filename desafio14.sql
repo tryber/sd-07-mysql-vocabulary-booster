1d63215b8ee7a01158a9c53c0a1a3e074114c68e
FelipeAVieiraTurma7 <veiculozeroemissao@gmail.com>
2021-03-01 17:01:05 -0300

    desafios 7, 11, 14, 15

--- desafio14.sql
+++ desafio14.sql
@@ -1,4 +1,4 @@
-SELECT Country AS 'País' FROM w3schools.customers
+SELECT Country AS País FROM w3schools.customers
 UNION
 SELECT Country FROM w3schools.suppliers
-ORDER BY 'País' LIMIT 5;
+ORDER BY País LIMIT 5;
--- desafio15.sql
+++ desafio15.sql
@@ -1,9 +1,9 @@
-USE hr
+USE hr;
 DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN Cargo VARCHAR(100))
 BEGIN
-SELECT ROUND(AVG(e.SALARY), 2)
+SELECT ROUND(AVG(e.SALARY), 2) AS 'Média salarial'
 FROM hr.employees AS e
 INNER JOIN hr.jobs AS j ON j.JOB_ID = e.JOB_ID
 WHERE j.JOB_TITLE = Cargo;
