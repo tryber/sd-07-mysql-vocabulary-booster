682907a038dfeb41e05b1b8bcbb088ca931a1daf
Guilherme Almeida <almeidaguilherme204@gmail.com>
2021-03-01 17:38:40 -0300

    Desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -3,10 +3,11 @@ job.JOB_TITLE AS Cargo,
 history_job.START_DATE AS "Data de início do cargo",
 department.DEPARTMENT_NAME AS "Departamento"
 FROM hr.employees AS employe
-RIGHT JOIN hr.job_history AS history_job
+INNER JOIN hr.job_history AS history_job
 ON history_job.EMPLOYEE_ID = employe.EMPLOYEE_ID
 LEFT JOIN hr.jobs AS job
 ON job.JOB_ID = history_job.JOB_ID
 LEFT JOIN hr.departments AS department
 ON department.DEPARTMENT_ID = employe.DEPARTMENT_ID
+GROUP BY employe.EMPLOYEE_ID
 ORDER BY `Nome completo` DESC,Cargo;
