6fc7f602072222dc15deeb9f68dc6e511177bd4b
MoisesSantana <moisaant@gmail.com>
2021-03-01 13:54:27 -0300

    nao aguento mais correcoes

--- desafio6.sql
+++ desafio6.sql
@@ -4,10 +4,10 @@ j.JOB_TITLE AS Cargo,
 jh.START_DATE AS 'Data de início do cargo',
 d.DEPARTMENT_NAME AS Departamento
 FROM hr.employees AS e
-INNER JOIN hr.jobs AS j
-ON e.JOB_ID = j.JOB_ID
-INNER JOIN hr.departments AS d
-ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
 INNER JOIN hr.job_history AS jh
 ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+INNER JOIN hr.jobs AS j
+ON jh.JOB_ID = j.JOB_ID
+INNER JOIN hr.departments AS d
+ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
 ORDER BY 1 DESC, 2;
