2399ef28da04c5b1ecc2329c31a84cdb7a059eeb
Lenodeoliveira <lenodeoliveira@gmail.com>
2021-03-01 17:33:33 -0300

    Correção exercício 8

--- desafio8.sql
+++ desafio8.sql
@@ -3,6 +3,6 @@ SHIPPER.ShipperName AS `Empresa que fez o envio`,
 O.OrderDate AS `Data do pedido`
 FROM w3schools.customers AS C
 INNER JOIN w3schools.orders AS O ON O.CustomerID = C.CustomerID
-INNER JOIN w3schools.shippers AS SHIPPER ON SHIPPER.ShipperID = O.ShipperID
-WHERE SHIPPER.ShipperName = 'Speedy Express' OR SHIPPER.ShipperName = 'United Package'
+INNER JOIN w3schools.shippers AS SHIPPER ON O.ShipperID = SHIPPER.ShipperID
+WHERE SHIPPER.ShipperName IN ('Speedy Express', 'United Package')
 ORDER BY `Nome do contato`,`Empresa que fez o envio`, `Data do pedido`;
