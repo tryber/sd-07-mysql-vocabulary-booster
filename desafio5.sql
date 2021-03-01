44ff3a489a98c12b5cc7b348a85daa05550f32f7
Guilherme Almeida <almeidaguilherme204@gmail.com>
2021-03-01 18:17:03 -0300

    Desafio 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,5 +1,5 @@
 SELECT cargo.JOB_TITLE AS Cargo,cargo.MAX_SALARY  - cargo.MIN_SALARY AS "Variação Salarial",
-ROUND(AVG(cargo.MIN_SALARY / 2),2) AS "Média mínima mensal", ROUND(AVG(cargo.MAX_SALARY / 2),2) AS "Média máxima mensal"
+ROUND(AVG(cargo.MIN_SALARY / 12),2) AS "Média mínima mensal", ROUND(AVG(cargo.MAX_SALARY / 12),2) AS "Média máxima mensal"
 FROM hr.jobs AS cargo
 RIGHT JOIN hr.employees AS employe
 ON cargo.JOB_ID = employe.JOB_ID
