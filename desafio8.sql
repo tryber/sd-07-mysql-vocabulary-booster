267e71acbc81a308c66f6fe88020d49f88249b87
hvenses <hvenses@gmail.com>
2021-03-01 16:19:07 -0300

    Corrigindo o req8 - trocando customerName por contactName

--- desafio8.sql
+++ desafio8.sql
@@ -1,7 +1,7 @@
-SELECT c.CustomerName as 'Nome de contato',
+SELECT c.ContactName as 'Nome de contato',
 s.ShipperName as 'Empresa que fez o envio',
 o.OrderDate as 'Data do pedido' FROM w3schools.customers AS c
 INNER JOIN w3schools.orders AS o ON c.CustomerID = o.CustomerID
 INNER JOIN w3schools.shippers AS s ON o.ShipperID = s.ShipperID
 WHERE s.ShipperName IN('Speedy Express', 'United Package')
-ORDER BY c.CustomerName, s.ShipperName, o.OrderDate DESC;
+ORDER BY c.ContactName, s.ShipperName, o.OrderDate DESC;
