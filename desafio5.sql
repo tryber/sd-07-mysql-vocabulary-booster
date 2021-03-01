18e484fa34e0304526e902d3ef04805cbb6abfd3
Guilherme Almeida <almeidaguilherme204@gmail.com>
2021-03-01 14:18:17 -0300

    Desafio 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,4 +1,4 @@
-SELECT cargo.JOB_TITLE AS Cargo, MAX(employe.SALARY) - MIN(employe.SALARY)   AS "Variação Salarial",
+SELECT cargo.JOB_TITLE AS Cargo, MAX(employe.SALARY * 12) - MIN(employe.SALARY * 12)   AS "Variação Salarial",
 MIN(employe.SALARY) AS "Média mínima mensal", MAX(employe.SALARY) AS "Média máxima mensal"
 FROM hr.jobs AS cargo
 INNER JOIN hr.employees AS employe
