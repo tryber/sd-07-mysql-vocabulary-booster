a1471eac65af3fd20f038d7de8bde4d0b32e6480
julioKauer <juliokauer@gmail.com>
2021-03-01 12:04:31 -0300

    Desafio 4

--- desafio4.sql
+++ desafio4.sql
@@ -5,8 +5,8 @@ SELECT j.job_title AS Cargo, ROUND(AVG(e.salary),2) AS "Média salarial",
         WHEN ROUND(AVG(e.salary),2) >= 5800 THEN 'Pleno'
         ELSE 'Júnior'
     END AS "Senioridade" 
-    FROM jobs AS j
-    JOIN employees AS e
+    FROM hr.jobs AS j
+    INNER JOIN hr.employees AS e
     ON j.job_id =  e.job_id
     GROUP BY e.job_id
     ORDER BY ROUND(AVG(e.salary),2), Cargo;
