734c10106a36c4114d49bb097721eb31f14f2a0c
marcelo ivan <sagacello@hotmail.com>
2021-03-01 18:08:55 -0300

    16 TESTE2

--- desafio16.sql
+++ desafio16.sql
@@ -3,10 +3,10 @@ DELIMITER $$
 CREATE FUNCTION  buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(200))
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE empregos INT;
-    SELECT COUNT(E.EMPLOYEE_ID) FROM hr.job_history AS J
-    INNER JOIN hr.employees AS E ON E.EMPLOYEE_ID = J.EMPLOYEE_ID
-    WHERE E.EMAIL = email INTO empregos;
-	RETURN empregos;
+DECLARE empregos INT;
+SELECT COUNT(E.EMPLOYEE_ID) FROM hr.job_history AS J
+INNER JOIN hr.employees AS E ON E.EMPLOYEE_ID = J.EMPLOYEE_ID
+WHERE E.EMAIL = email INTO empregos;
+RETURN empregos;
 END $$
 DELIMITER ;
