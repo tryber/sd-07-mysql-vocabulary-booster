b8a979ac54b8ac605b6fe4dab238aa0959d844bc
dennerpl <denner.souzapl@gmail.com>
2021-03-01 18:40:26 -0300

    requisitos 1 e 4

--- desafio1.sql
+++ desafio1.sql
@@ -1,6 +1,6 @@
 SELECT COUNTRY_NAME AS País,
   CASE
-    WHEN REGION_ID = 1 THEN 'Europe'
+    WHEN REGION_ID = 1 THEN 'incluído'
     ELSE 'não incluído'
   END AS "Status Inclusão"
 FROM hr.countries
--- desafio4.sql
+++ desafio4.sql
@@ -1,20 +1,22 @@
-SELECT JOB_TITLE AS "Cargo",
-  ROUND(((MAX_SALARY + MIN_SALARY) / 2), 2) AS "Média salarial",
+SELECT tableJobs.JOB_TITLE AS `Cargo`,
+  ROUND(AVG(tableEmployees.SALARY), 2) AS `Média salarial`,
   CASE
     WHEN (
-      ((MAX_SALARY + MIN_SALARY) / 2) >= 2000
-      AND ((MAX_SALARY + MIN_SALARY) / 2) <= 5800
+      ROUND(AVG(tableEmployees.SALARY), 2) >= 2000
+      AND ROUND(AVG(tableEmployees.SALARY), 2) <= 5800
     ) THEN 'Júnior'
     WHEN (
-      ((MAX_SALARY + MIN_SALARY) / 2) >= 5801
-      AND ((MAX_SALARY + MIN_SALARY) / 2) <= 7500
+      ROUND(AVG(tableEmployees.SALARY), 2) >= 5801
+      AND ROUND(AVG(tableEmployees.SALARY), 2) <= 7500
     ) THEN 'Pleno'
     WHEN (
-      ((MAX_SALARY + MIN_SALARY) / 2) >= 7501
-      AND ((MAX_SALARY + MIN_SALARY) / 2) <= 10500
+      ROUND(AVG(tableEmployees.SALARY), 2) >= 7501
+      AND ROUND(AVG(tableEmployees.SALARY), 2) <= 10500
     ) THEN 'Sênior'
-    WHEN (((MAX_SALARY + MIN_SALARY) / 2) > 10500) THEN 'CEO'
+    WHEN ROUND(AVG(tableEmployees.SALARY), 2) > 10500 THEN 'CEO'
   END AS "Senioridade"
-FROM hr.jobs
-ORDER BY ((MAX_SALARY + MIN_SALARY) / 2),
-  JOB_TITLE;
+FROM hr.jobs AS tableJobs
+  INNER JOIN hr.employees AS tableEmployees ON tableJobs.JOB_ID = tableEmployees.JOB_ID
+GROUP BY tableJobs.JOB_TITLE
+ORDER BY `Média salarial`,
+  `Cargo`;
