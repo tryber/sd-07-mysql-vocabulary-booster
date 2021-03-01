15dd696483dfc92fd92027f808cc1caaf08c51b4
Renato Silva <renatosdev@gmail.com>
2021-03-01 12:45:13 -0300

    desafio6

---
+++ desafio6.sql
@@ -0,0 +1,12 @@
+SELECT CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS `Nome completo`,
+J.JOB_TITLE AS Cargo,
+JH.START_DATE AS `Data de início do cargo`,
+D.DEPARTMENT_NAME AS Departamento
+FROM employees AS E
+JOIN jobs as J
+ON E.JOB_ID = J.JOB_ID
+JOIN job_history AS JH
+ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
+JOIN departments AS D
+ON JH.DEPARTMENT_ID = D.DEPARTMENT_ID
+ORDER BY 1 DESC, 2;
