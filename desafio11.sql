74307d53268ff78ae56f3e572dc44f2c707113ed
wberilo <wberilo@gmail.com>
2021-03-01 15:04:46 -0300

    11

---
+++ desafio11.sql
@@ -0,0 +1,9 @@
+SELECT
+t1.ContactName as Nome,
+t1.Country as País,
+COUNT(t1.Country)
+from customers as t1
+INNER JOIN customers as t2
+on t1.Country = t2.Country where t1.CustomerID != t2.CustomerID
+group by t1.ContactName
+order by t1.ContactName
