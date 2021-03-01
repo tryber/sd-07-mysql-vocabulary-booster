d65f45d7968329729c4f459dc14b43ee34cc455d
Lfocosi <luccafocosi@gmail.com>
2021-03-01 17:01:01 -0300

    coreção 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,5 +1,5 @@
 SELECT job.JOB_TITLE as Cargo,
-round(avg(employee.SALARY), 2) as "Média salarial",
+round(avg(employee.SALARY), 2) as 'Média salarial',
 if(round(avg(employee.SALARY), 2) > 10500, 'CEO',
 if(round(avg(employee.SALARY), 2) > 7500, 'Sênior',
 if(round(avg(employee.SALARY), 2) > 5800, 'Pleno', 'Júnior'))) as Senioridade
@@ -7,4 +7,4 @@ FROM hr.jobs as job
 inner join hr.employees as employee
 on job.JOB_ID = employee.JOB_ID
 group by employee.JOB_ID
-order by `Média salarial`;
+order by round(avg(employee.SALARY), 2);
