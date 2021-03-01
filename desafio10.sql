07c0ed7e3941b843169d4bdbbbeb2f168a6c5748
wberilo <wberilo@gmail.com>
2021-03-01 15:11:25 -0300

    fixes

--- desafio10.sql
+++ desafio10.sql
@@ -2,7 +2,7 @@ SELECT
 t2.ProductName as Produto,
 MIN(t1.Quantity) as Mínima,
 MAX(t1.Quantity) as Máxima,
-AVG(t1.Quantity) as decimal(10,2) as Média
+cast(AVG(t1.Quantity) as decimal(10,2)) as Média
 FROM w3schools.order_details as t1
 INNER JOIN products as t2
 on t1.ProductID = t2.ProductID
--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,7 @@
 SELECT
 t1.ContactName as Nome,
 t1.Country as País,
-COUNT(t1.Country)
+COUNT(t1.Country) as `Número de compatriotas`
 from customers as t1
 INNER JOIN customers as t2
 on t1.Country = t2.Country where t1.CustomerID != t2.CustomerID
