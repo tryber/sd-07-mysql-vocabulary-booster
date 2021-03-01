d09a276ea8234bec5b1df3c605084aa2cd672b6f
marcelo ivan <sagacello@hotmail.com>
2021-03-01 18:06:59 -0300

    16 TESTE

--- desafio16.sql
+++ desafio16.sql
@@ -4,7 +4,7 @@ CREATE FUNCTION  buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(200
 RETURNS INT READS SQL DATA
 BEGIN
 	DECLARE empregos INT;
-    SELECT COUNT(*) FROM hr.job_history AS J
+    SELECT COUNT(E.EMPLOYEE_ID) FROM hr.job_history AS J
     INNER JOIN hr.employees AS E ON E.EMPLOYEE_ID = J.EMPLOYEE_ID
     WHERE E.EMAIL = email INTO empregos;
 	RETURN empregos;
