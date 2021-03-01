d1a5a662e3dd2d25813aaa7447152aec9904f656
Renato Silva <renatosdev@gmail.com>
2021-03-01 18:51:55 -0300

    desafio13

---
+++ desafio13.sql
@@ -0,0 +1,7 @@
+SELECT p.ProductName AS Produto,
+p.Price AS Preço
+FROM w3schools.products AS p
+JOIN w3schools.order_details AS od
+ON p.ProductID = od.ProductID
+WHERE  od.Quantity > 80
+ORDER BY 1;
