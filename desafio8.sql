0e29dea4eb1fa6c044fa715511fe9472299152b5
wberilo <wberilo@gmail.com>
2021-03-01 14:05:18 -0300

    8

--- desafio8.sql
+++ desafio8.sql
@@ -1,5 +1,5 @@
 SELECT 
-t2.CustomerName as `Nome de contato`,
+t2.ContactName as `Nome de contato`,
 t3.ShipperName as `Empresa que fez o envio`,
 t1.OrderDate as `Data do pedido`
 FROM orders as t1
@@ -8,4 +8,4 @@ on t1.CustomerID = t2.CustomerID
 INNER JOIN shippers as t3
 on t1.ShipperID = t3.ShipperID
 WHERE t3.ShipperName IN ('Speedy Express', 'United Package')
-ORDER BY t2.CustomerName, t3.ShipperName, t1.OrderDate;
+ORDER BY t2.ContactName, t3.ShipperName, t1.OrderDate
