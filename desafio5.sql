7a05d954fa4fcec6953e12b42d600057f6ca42e1
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-03-01 15:06:21 -0300

    Desafio 05: Refatoraçao

--- desafio5.sql
+++ desafio5.sql
@@ -1,9 +1,7 @@
-SELECT j.JOB_TITLE AS `Cargo`,
-(j.MAX_SALARY - j.MIN_SALARY) AS `Variação Salarial`,
-ROUND(MIN(e.SALARY), 2) AS `Média mínima mensal`,
-ROUND(MAX(e.SALARY), 2) AS `Média máxima mensal`
-FROM hr.jobs AS `j`
-INNER JOIN hr.employees AS `e`
-ON j.JOB_ID = e.JOB_ID
+SELECT JOB_TITLE AS `Cargo`,
+(MAX_SALARY - MIN_SALARY) AS `Variação Salarial`,
+ROUND(MIN_SALARY / 12, 2) AS `Média mínima mensal`,
+ROUND(MAX_SALARY / 12, 2) AS `Média máxima mensal`
+FROM hr.jobs
 GROUP BY `Cargo`
 ORDER BY `Variação Salarial`, `Cargo`;
