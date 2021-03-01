00d2320edf227005f6ad1ba201a79e3aef49fa28
ygorvmdf <ygorvitor.fonseca@gmail.com>
2021-03-01 18:45:51 -0300

    ajustando desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,11 +1,13 @@
 SELECT
 CONCAT(first_name, ' ', last_name) AS 'Nome completo',
 j.job_title AS 'Cargo',
-e.hire_date AS 'Data de início do cargo',
+start_date AS 'Data de início do cargo',
 d.department_name AS 'Departamento'
-FROM hr.employees e
+FROM hr.job_history jh
 JOIN hr.jobs j
-ON e.job_id = j.job_id
+ON jh.job_id = j.job_id
+JOIN hr.employees e
+ON jh.employee_id = e.employee_id
 JOIN hr.departments d
-ON e.department_id = d.department_id
-ORDER BY CONCAT(first_name, ' ', last_name) DESC, job_title;
+ON jh.department_id = d.department_id
+ORDER BY `Nome completo` DESC, `Cargo`;
