cb93e8e6361048eff0ca69177109bb665c8738ea
renatomak <renato.mark.silva@gmail.com>
2021-03-01 15:03:13 -0300

    Desafio 8-modificada sequencia do where

--- desafio8.sql
+++ desafio8.sql
@@ -2,6 +2,6 @@ SELECT C.ContactName `Nome de contato`,
 S.ShipperName `Empresa que fez o envio`,
 O.OrderDate `Data do pedido`
 FROM w3schools.shippers S, w3schools.customers C, w3schools.orders O
-WHERE C.CustomerID=O.CustomerID AND S.ShipperID=O.ShipperID
+WHERE  S.ShipperID=O.ShipperID AND C.CustomerID=O.CustomerID
 AND S.ShipperName IN ('Speedy Express', 'United Package')
 ORDER BY `Nome de contato`, `Empresa que fez o envio`;
