c743837a1412527cc8925ab99a2957fd7ef88c6b
danielmlduarte <danielduarte.tryber@gmail.com>
2021-03-01 12:49:52 -0300

    Desafio 5 - Refatorado

--- desafio5.sql
+++ desafio5.sql
@@ -4,6 +4,6 @@ j.job_title AS Cargo,
 ROUND(MIN(e.salary), 2) AS 'Média mínima mensal',
 ROUND(MAX(e.salary), 2) AS 'Média máxima mensal'
 FROM hr.jobs j
-LEFT JOIN hr.employees e ON j.job_id = e.job_id
-GROUP BY Cargo
-ORDER BY (j.max_salary - j.min_salary), Cargo;
+LEFT JOIN hr.employees e ON e.job_id = j.job_id
+GROUP BY e.job_id
+ORDER BY 2, Cargo;
