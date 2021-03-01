0dbab3f1dffd074319788d207a73aa1ef8ea6102
Guilherme Almeida <almeidaguilherme204@gmail.com>
2021-03-01 14:21:59 -0300

    Desafio 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,5 +1,5 @@
 SELECT cargo.JOB_TITLE AS Cargo, MAX(employe.SALARY * 12) - MIN(employe.SALARY * 12)   AS "Variação Salarial",
-MIN(employe.SALARY) AS "Média mínima mensal", MAX(employe.SALARY) AS "Média máxima mensal"
+MIN(employe.SALARY * 12) AS "Média mínima mensal", MAX(employe.SALARY * 12) AS "Média máxima mensal"
 FROM hr.jobs AS cargo
 INNER JOIN hr.employees AS employe
 ON cargo.JOB_ID = employe.JOB_ID
