a6f6e50764b3d02a7406bd2fa8c06e23f9453184
vitorAmorims <vtamorims@gmail.com>
2021-03-02 14:09:51 -0300

    desafio13.sql corrigido

--- desafio13.sql
+++ desafio13.sql
@@ -20,5 +20,5 @@ SELECT
   products.ProductName AS "Produto",
   products.Price AS "Preço"
 FROM products
-WHERE products.ProductID IN (SELECT order_details.ProductID FROM order_details GROUP BY order_details.ProductID HAVING SUM(order_details.Quantity) > 80)
+WHERE products.ProductID IN (SELECT order_details.ProductID FROM order_details WHERE order_details.Quantity > 80)
 ORDER BY Produto;
