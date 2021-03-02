9d9e7e6e23021ccf68eb0024d8e43f16c9856068
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2021-03-02 00:40:10 -0300

    Exercise 4 - done-2

--- desafio4.sql
+++ desafio4.sql
@@ -1,15 +1,14 @@
 SELECT jobs.job_title AS "Cargo",
 ROUND(AVG(salary), 2) AS "Média salarial",
 CASE
- WHEN ROUND(AVG(salary), 2) BETWEEN 2000 AND 5800 THEN "Júnior"
- WHEN ROUND(AVG(salary), 2) BETWEEN 5801 AND 7500 THEN "Pleno"
- WHEN ROUND(AVG(salary), 2) BETWEEN 7501 AND 10500 THEN "Sênior"
- WHEN ROUND(AVG(salary), 2) > 10500 THEN "CEO"
- END
- AS "Senioridade"
- FROM hr.employees
- INNER JOIN jobs
- ON employees.job_id = jobs.job_id
- GROUP BY Cargo
- ORDER BY ROUND(AVG(salary), 2), Cargo;
- 
+WHEN ROUND(AVG(salary), 2) BETWEEN 2000 AND 5800 THEN "Júnior"
+WHEN ROUND(AVG(salary), 2) BETWEEN 5801 AND 7500 THEN "Pleno"
+WHEN ROUND(AVG(salary), 2) BETWEEN 7501 AND 10500 THEN "Sênior"
+WHEN ROUND(AVG(salary), 2) > 10500 THEN "CEO"
+END
+AS "Senioridade"
+FROM hr.employees
+INNER JOIN jobs
+ON employees.job_id = jobs.job_id
+GROUP BY Cargo
+ORDER BY ROUND(AVG(salary), 2), Cargo;
