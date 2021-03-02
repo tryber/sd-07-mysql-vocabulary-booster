68132f3006d3059f39fcce44f2d3f477224234eb
Carol Bezerra <carol.arbe@gmail.com>
2021-03-01 21:38:08 -0300

    Ajuste requisito 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,2 +1,2 @@
-SELECT j.job_title AS Cargo, j.max_salary - j.min_salary AS 'Variação Salarial', ROUND(MIN(j.min_salary) / 12, 2) AS 'Média mínima mensal', ROUND(MAX(j.max_salary) / 12, 2) AS 'Média máxima mensal' FROM hr.jobs AS j INNER JOIN hr.employees AS `e` ON j.job_id = e.job_id GROUP BY j.job_title ORDER BY `Variação Salarial`, Cargo;
--- referência: Arthur Massaini e Luiz Fernandes Oliveira
+SELECT job_title AS Cargo, max_salary - min_salary AS 'Variação Salarial', ROUND(min_salary / 12, 2) AS 'Média mínima mensal', ROUND(max_salary / 12, 2) AS 'Média máxima mensal' FROM hr.jobs ORDER BY `Variação Salarial`, Cargo;
+-- referência: Luiz Fernandes Oliveira
