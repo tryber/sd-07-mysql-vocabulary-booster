ddf2be22486bd0c1b0cc18bcddf8142525be700c
rodrigoderkian <derkianpromo@gmail.com>
2021-03-02 09:21:30 -0300

    refatoraçao e padronizaçao

--- desafio4.sql
+++ desafio4.sql
@@ -2,9 +2,9 @@ SELECT
     j.JOB_TITLE AS 'Cargo',
     ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
     CASE
-        WHEN (AVG(e.SALARY) BETWEEN 2000 AND 5799) THEN 'Júnior'
-        WHEN (AVG(e.SALARY) BETWEEN 5800 AND 7499) THEN 'Pleno'
-        WHEN (AVG(e.SALARY) BETWEEN 7500 AND 10499) THEN 'Sênior'
+        WHEN (AVG(e.SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+        WHEN (AVG(e.SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+        WHEN (AVG(e.SALARY), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
         ELSE 'CEO'
     END AS 'Senioridade'
 FROM
@@ -12,4 +12,4 @@ FROM
         JOIN
     hr.jobs AS j ON e.JOB_ID = j.JOB_ID
 GROUP BY e.JOB_ID
-ORDER BY AVG(e.SALARY);
+ORDER BY `Média salarial`;
