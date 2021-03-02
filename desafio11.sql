50b09ff4fddf758c3d6be7243c075f0eba85b16d
samantabelow <samanta.below@gmail.com>
2021-03-02 16:32:08 -0300

    Desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,9 @@
 SELECT t1.ContactName AS 'Nome',
 t1.Country AS 'País',
 COUNT(t1.Country) - 1 AS 'Número de compatriotas'
-FROM w3schools.customers AS t1, w3schools.customers AS t2
+FROM w3schools.customers AS t1
+INNER JOIN w3schools.customers AS t2
+ON t1.Country = t2.Country
 GROUP BY t1.CustomerID
 HAVING COUNT(t1.Country) - 1 > 0
 ORDER BY `Nome`;
