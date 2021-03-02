185b922c32022d3184821cb3e69825ceb4b63f60
EmanuelleBrasil <emanuelle.rbl@gmail.com>
2021-03-02 19:33:45 -0300

    desafio 7 CORREÇAO

--- desafio7.sql
+++ desafio7.sql
@@ -1,10 +1,8 @@
 SELECT UCASE(CONCAT(FIRST_NAME, ' ', LAST_NAME)) AS 'Nome completo',
-  JOB_TITLE AS 'Cargo',
   START_DATE AS 'Data de início',
   SALARY AS 'Salário'
 FROM hr.job_history AS h
-  INNER JOIN hr.jobs AS j ON h.JOB_ID = j.JOB_ID
-  JOIN hr.employees AS e ON h.EMPLOYEE_ID = e.EMPLOYEE_ID
+  INNER JOIN hr.employees AS e ON h.EMPLOYEE_ID = e.EMPLOYEE_ID
 WHERE MONTH(START_DATE) BETWEEN 1 AND 3
 ORDER BY `Nome completo`,
   `Data de início`;
