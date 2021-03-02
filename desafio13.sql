4f57c3cd0fd7befb59d16516e230ce79ab17e596
Lenodeoliveira <lenodeoliveira@gmail.com>
2021-03-01 22:34:16 -0300

    Adicionando requisito 13

---
+++ desafio13.sql
@@ -0,0 +1,6 @@
+SELECT pr.ProductName AS `Produto`,
+pr.Price AS `Price`
+FROM w3schools.products AS pr
+INNER JOIN w3schools.order_details AS od ON od.ProductID = pr.ProductID
+WHERE od.Quantity > 80
+ORDER BY `Produto`;
