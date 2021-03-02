b26dd68647da8ce76818a1db54be14e6ec380890
Danwhat <daanwhant@gmail.com>
2021-03-02 09:58:31 -0300

    mais 3

--- desafio18.sql
+++ desafio18.sql
@@ -1,12 +1,14 @@
 DELIMITER $$
-CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mes INT, ano INT)
-RETURNS INT READS SQL DATA
+CREATE PROCEDURE exibir_historico_completo_por_funcionario(IN email VARCHAR(25))
 BEGIN
-DECLARE retorno INT;
-  SELECT
-COUNT(EMPLOYEE_ID)
-FROM hr.employees
-WHERE MONTH(HIRE_DATE) = mes AND YEAR(HIRE_DATE)= ano
-  INTO retorno;
-  RETURN retorno;
+SELECT
+CONCAT(FIRST_NAME,' ',LAST_NAME) AS 'Nome completo',
+D.DEPARTMENT_NAME AS 'Departamento',
+J.JOB_TITLE AS 'Cargo'
+FROM hr.job_history H
+JOIN hr.employees E ON H.EMPLOYEE_ID = E.EMPLOYEE_ID
+JOIN hr.departments D ON H.DEPARTMENT_ID = D.DEPARTMENT_ID
+JOIN hr.jobs J ON H.JOB_ID = J.JOB_ID
+WHERE E.EMAIL = email
+ORDER BY CONCAT(FIRST_NAME,' ',LAST_NAME), J.JOB_TITLE;
 END $$
--- desafio5.sql
+++ desafio5.sql
@@ -1,6 +1,6 @@
 SELECT
 JOB_TITLE AS 'Cargo',
-ROUND(MAX_SALARY - MIN_SALARY, 2) AS 'Variação Salarial',
+ROUND(MAX_SALARY - MIN_SALARY, 0) AS 'Variação Salarial',
 ROUND(MIN_SALARY / 12,2 ) AS 'Média mínima mensal',
 ROUND(MAX_SALARY / 12,2 ) AS 'Média máxima mensal'
 FROM hr.jobs
