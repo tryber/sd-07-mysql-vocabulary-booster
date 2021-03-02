7b41d48b6a1b4513f5f44e555c7253c847a16a31
Luiz Filipe <lfsmariz@gmail.com>
2021-03-02 14:00:43 -0300

    resolve codeclimate

--- desafio15.sql
+++ desafio15.sql
@@ -4,13 +4,13 @@ DROP PROCEDURE IF EXISTS buscar_media_por_cargo;
 DELIMITER |
 CREATE PROCEDURE buscar_media_por_cargo (IN cargo VARCHAR(50))
 BEGIN
-   SELECT ROUND(AVG(emp.SALARY), 2) AS 'Média salarial'
-        FROM hr.employees AS emp
-        INNER JOIN hr.jobs AS job
-			ON emp.JOB_ID = job.JOB_ID
-            WHERE job.JOB_TITLE = cargo
-			GROUP BY emp.JOB_ID;
-    
+SELECT ROUND(AVG(emp.SALARY), 2) AS 'Média salarial'
+FROM hr.employees AS emp
+INNER JOIN hr.jobs AS job
+ON emp.JOB_ID = job.JOB_ID
+WHERE job.JOB_TITLE = cargo
+GROUP BY emp.JOB_ID;
+
 END
 |
 DELIMITER ;
