470fe6e5904f66417e42ad51171f2a76d9c26f1c
Elano Evaristo <elano3004@gmail.com>
2021-03-02 19:44:55 -0300

    desafio1 tetativa 03

--- 1.sql
+++ 1.sql
@@ -1,8 +1,3 @@
--- Desafio 01
-
-SELECT countries.COUNTRY_NAME AS 'País',
-IF (regions.REGION_NAME = 'Europe', 'incluído', 'não incluído') AS 'Status Inclusão'
-FROM hr.countries AS countries
-INNER JOIN hr.regions AS regions
-ON countries.REGION_ID = regions.REGION_ID
-ORDER BY countries.COUNTRY_NAME;
+SELECT COUNTRY_NAME AS País, IF(REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
+FROM hr.countries
+ORDER BY COUNTRY_NAME;
