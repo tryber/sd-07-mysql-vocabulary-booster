416bd880f9058e9797ffcf3b56839cb35506072f
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-03-01 21:08:43 -0300

    desafios 05 e 06

---
+++ desafio5.sql
@@ -0,0 +1,5 @@
+SELECT job_title AS "Cargo", max_salary - min_salary AS "Variação Salarial",
+ROUND(AVG(min_salary) / 12, 2) AS "Média mínima mensal", ROUND(AVG(max_salary) / 12, 2) AS "Média máxima mensal"
+FROM hr.jobs
+GROUP BY job_title
+ORDER BY 2, job_title;
