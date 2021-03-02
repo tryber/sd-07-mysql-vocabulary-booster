3f3fa9a984322f7531337e03859ed0210c68e3ef
nevesfreire <nando.neves@gmail.com>
2021-03-02 13:09:04 -0300

    6

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 select 
 concat(employees.FIRST_NAME, ' ', employees.LAST_NAME) AS 'Nome completo',
 jobs.JOB_TITLE AS Cargo,
-history.START_DATE AS 'Data de início',
+history.START_DATE AS 'Data de início do cargo',
 departments.DEPARTMENT_NAME AS Departamento
 
 from hr.job_history as history
