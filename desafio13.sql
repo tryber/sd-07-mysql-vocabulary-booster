90dcee18cb7588a156162fd9bdef369d33db5b2c
Melissa Gomes <mel.ge171201@gmail.com>
2021-03-02 19:48:47 -0300

    13 e 14

--- desafio13.sql
+++ desafio13.sql
@@ -1,1 +1,8 @@
-SELECT * FROM employees;
+SELECT
+p.ProductName AS `Produto`,
+p.Price AS `Preço`
+FROM w3schools.products AS `p`
+INNER JOIN w3schools.order_details AS `q`
+ON p.ProductID = q.ProductID
+WHERE q.Quantity > 80
+ORDER BY `Produto`;
