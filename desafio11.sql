ba9ef9856b04966cf6eb5d67be5576108827d6ae
Lenodeoliveira <lenodeoliveira@gmail.com>
2021-03-01 22:16:56 -0300

    Correção requisito 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,8 @@
 SELECT C1.ContactName AS `Nome`,
 C1.Country AS `País`,
-(SELECT COUNT(*) 
-FROM w3schools.customers AS C2
-WHERE C2.Country = C1.Country AND C1.CustomerID <> C2.CustomerID) AS `Número de compatriotas`
-FROM w3schools.customers AS C1
-WHERE Country = Country
+COUNT(C1.Country) AS `Número de compatriotas`
+FROM w3schools.customers AS C1, w3schools.customers AS C2
+WHERE C2.Country = C1.Country AND C1.CustomerID <> C2.CustomerID
+GROUP BY C1.CustomerID
+HAVING `Número de compatriotas` > 0
 ORDER BY `Nome`;
