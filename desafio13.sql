f124214ef782bfa5d0af04d67cfd089972df7a65
Lenodeoliveira <lenodeoliveira@gmail.com>
2021-03-01 22:38:13 -0300

    Correção requisito 13

--- desafio13.sql
+++ desafio13.sql
@@ -1,5 +1,5 @@
 SELECT pr.ProductName AS `Produto`,
-pr.Price AS `Price`
+pr.Price AS `Preço`
 FROM w3schools.products AS pr
 INNER JOIN w3schools.order_details AS od ON od.ProductID = pr.ProductID
 WHERE od.Quantity > 80
