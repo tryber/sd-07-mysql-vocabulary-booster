74cc5eb8abd2aa144626bf6fa4171c44b9bd40c3
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-03-02 00:57:50 -0300

    requisito 09 e ajustes no requisito 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,14 +1,12 @@
 SELECT concat(employees.first_name, " ", employees.last_name) AS "Nome completo",
 jobs.job_title AS "Cargo",
-start_date AS "Data de início do cargo",
+historys.start_date AS "Data de início do cargo",
 departments.department_name AS "Departamento"
 FROM hr.employees AS employees
 INNER JOIN hr.jobs AS jobs
 ON employees.job_id = jobs.job_id
-
 INNER JOIN hr.departments AS departments
 ON employees.department_id = departments.department_id
-
 INNER JOIN hr.job_history AS historys
-ON employees.department_id = historys.department_id
-ORDER BY 1, 2;
+ON employees.employee_id = historys.employee_id
+ORDER BY 1 DESC, 2;
