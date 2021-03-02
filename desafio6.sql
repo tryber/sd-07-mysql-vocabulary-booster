21a9cee19aa345d56ecb10af4594183f510129d7
rodrigoderkian <derkianpromo@gmail.com>
2021-03-02 09:12:20 -0300

    refatoraçao e padronizaçao

--- desafio6.sql
+++ desafio6.sql
@@ -1,12 +1,12 @@
 SELECT 
-    CONCAT(employ.FIRST_NAME, ' ', employ.LAST_NAME) AS 'Nome completo',
+    CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
     j.JOB_TITLE AS 'Cargo',
     h.START_DATE AS 'Data de início do cargo',
     d.DEPARTMENT_NAME AS 'Departamento'
 FROM
     hr.job_history AS h
         INNER JOIN
-    hr.employees AS employ ON employ.EMPLOYEE_ID = h.EMPLOYEE_ID
+    hr.employees AS e ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
         INNER JOIN
     hr.jobs AS j ON j.JOB_ID = h.JOB_ID
         INNER JOIN
