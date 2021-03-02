640d91401c612ef77bddd035a0b33f71fe91de38
murilopereira006 <muriloedu006@gmail.com>
2021-03-02 19:22:00 -0300

    no desafio 8 eu tava usando o nome do cliente errado..

--- desafio8.sql
+++ desafio8.sql
@@ -1,10 +1,10 @@
-SELECT CUST.CustomerName AS "Nome de contato",
+SELECT CUST.ContactName AS "Nome de contato",
 SHIP.ShipperName AS "Empresa que fez o envio",
-ORD.OrderDate AS "Data do pedido"
-FROM w3schools.customers AS CUST
-INNER JOIN w3schools.orders AS ORD
-ON CUST.CustomerID = ORD.CustomerID
+DATE(ORD.OrderDate) AS "Data do pedido"
+FROM w3schools.orders AS ORD
+INNER JOIN w3schools.customers AS CUST
+ON ORD.CustomerID = CUST.CustomerID
 INNER JOIN w3schools.shippers AS SHIP
-ON SHIP.ShipperID = ORD.ShipperID
-WHERE SHIP.ShipperName IN ('Speedy Express', 'United Package')
-ORDER BY `Nome de contato` ASC, `Empresa que fez o envio` ASC, `Data do pedido` ASC;
+ON ORD.ShipperID = SHIP.ShipperID
+HAVING `Empresa que fez o envio` = 'Speedy Express' OR `Empresa que fez o envio` = 'United Package'
+ORDER BY `Nome de contato` ASC, `Empresa que fez o envio` ASC, `Data do pedido`;
