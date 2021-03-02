670de0072e81709319b3fd36566dbe0ac35beba7
Filipi Firmino <contatoic3web@gmail.com>
2021-03-02 19:30:39 -0300

    chalenge 11 add where

--- desafio11.sql
+++ desafio11.sql
@@ -3,10 +3,12 @@ SELECT
   CUSTO.Country AS 'País',
   num_pat.num - 1	AS 'Número de compatriotas'
 FROM
-  (SELECT Country, COUNT(Country) AS num FROM w3schools.customers GROUP BY Country) AS num_pat
+ (SELECT Country, COUNT(Country) AS num FROM w3schools.customers GROUP BY Country) AS num_pat
 INNER JOIN
   w3schools.customers AS CUSTO
 ON
   CUSTO.Country = num_pat.Country
+WHERE
+  num_pat.num - 1 > 0
 ORDER BY
   ContactName;
