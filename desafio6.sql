aac44fe1ed80b106459590cdadb108e5203ffe7b
Andre Serudo <andreserudo@gmail.com>
2021-03-02 18:04:36 -0400

    desafio 6 - a batalha continua

--- desafio6.sql
+++ desafio6.sql
@@ -9,18 +9,15 @@
 -- A quarta deve possuir o alias "Departamento" e mostrar o nome do departamento em que a pessoa exerceu seu cargo.
 -- Os resultados devem estar ordenados pelo nome completo das pessoas empregadas em ordem decrescente.
 -- Em caso de empate no nome completo, ordene os resultados pelo nome do cargo em ordem alfabética.
-SELECT
+SELECT 
 CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS "Nome completo",
 J.JOB_TITLE AS "Cargo",
 H.START_DATE AS "Data de início do cargo",
 D.DEPARTMENT_NAME AS "Departamento"
-FROM	hr.employees E
-INNER JOIN
-hr.jobs AS J ON J.JOB_ID = E.JOB_ID
-INNER JOIN hr.job_history AS H
-ON H.EMPLOYEE_ID = E.EMPLOYEE_ID
-INNER JOIN
-hr.departments AS D on D.DEPARTMENT_ID = E.DEPARTMENT_ID
+FROM hr.employees E
+INNER JOIN hr.job_history AS H ON H.EMPLOYEE_ID = E.EMPLOYEE_ID
+INNER JOIN hr.jobs AS J ON J.JOB_ID = H.JOB_ID
+INNER JOIN hr.departments AS D on D.DEPARTMENT_ID = E.DEPARTMENT_ID
 ORDER BY
 1 DESC,
 2;
