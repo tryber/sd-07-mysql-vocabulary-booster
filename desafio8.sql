76f3836a9c888da8abb99fc4b3ac6d252594b317
Melissa Gomes <mel.ge171201@gmail.com>
2021-03-01 21:50:30 -0300

    8

--- desafio8.sql
+++ desafio8.sql
@@ -1,8 +1,9 @@
-SELECT c.ContactName AS `Nome de contato`,
+SELECT
+c.ContactName AS `Nome de contato`,
 s.ShipperName AS `Empresa que fez o envio`,
 o.OrderDate AS `Data do pedido`
 FROM w3schools.customers AS `c`
-INNER JOIN w3schools.order AS `o`
+INNER JOIN w3schools.orders AS `o`
 ON c.CustomerID = o.CustomerID
 INNER JOIN w3schools.shippers AS `s`
 ON s.ShipperID = o.ShipperID
