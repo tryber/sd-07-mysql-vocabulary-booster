863e9777b226dc34807885834d83d2e5b37e96c9
Luise Rios <luisemiranda@gmail.com>
2021-03-02 13:58:51 -0300

    desafio 6 correçao

--- desafio6.sql
+++ desafio6.sql
@@ -3,11 +3,16 @@ CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
 j.JOB_TITLE AS 'Cargo',
 h.START_DATE AS 'Data de início do cargo',
 d.DEPARTMENT_NAME AS 'Departamento'
-FROM hr.job_history h, hr.employees e, hr.departments d, hr.jobs j
 
-WHERE e.EMPLOYEE_ID = h.EMPLOYEE_ID
-AND e.DEPARTMENT_ID = d.DEPARTMENT_ID
-AND h.JOB_ID = j.JOB_ID
-AND j.JOB_TITLE IS NOT NULL
+FROM hr.employees e
+
+INNER JOIN hr.jobs j
+ON e.JOB_ID = j.JOB_ID
+
+INNER JOIN hr.job_history h
+ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
+
+INNER JOIN hr.departments d
+ON h.DEPARTMENT_ID = d.DEPARTMENT_ID
 
 ORDER BY `Nome completo` DESC, Cargo;
