40eeb56cbdda9cabbba3b98550a99e11f991a8f3
Lfocosi <luccafocosi@gmail.com>
2021-03-01 21:20:33 -0300

    correção 11-

--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,7 @@
 SELECT c1.ContactName as 'Nome',
 c1.Country as 'País',
-(count(c1.ContactName) - 1) as 'Número de compatriotas'
+count(c1.ContactName) as 'Número de compatriotas'
 FROM w3schools.customers as c1, w3schools.customers as c2
-where c1.Country = c2.Country
-group by `Nome`
-having `Número de compatriotas` > 0
+where c1.Country = c2.Country and c1.CustomerID <> c2.CustomerID
+group by c1.CustomerID
 order by c1.ContactName;
