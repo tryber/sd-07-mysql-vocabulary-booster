9e01ab8c8a793d612a7bf47357edc1eb55e8e64f
nevesfreire <nando.neves@gmail.com>
2021-03-02 12:34:22 -0300

    7

--- desafio7.sql
+++ desafio7.sql
@@ -1,14 +1,9 @@
-SELECT 
-UCASE(CONCAT(employees.FIRST_NAME , ' ', employees.LAST_NAME)) AS 'Nome completo',
-JOBS.JOB_TITLE AS Cargo,
-employees.HIRE_DATE AS 'Data de início',
-employees.SALARY AS 'Salário',
-Month(employees.HIRE_DATE)
-FROM hr.employees AS employees 
-
-INNER JOIN hr.jobs AS JOBS 
-ON JOBS.JOB_ID = employees.JOB_ID
-INNER JOIN hr.departments AS departments
-ON employees.DEPARTMENT_ID = departments.DEPARTMENT_ID
-WHERE MONTH(employees.HIRE_DATE) IN (1, 2, 3)
-ORDER BY CONCAT(employees.FIRST_NAME , ' ', employees.LAST_NAME), JOBS.JOB_TITLE ;
+select 
+UCASE(concat(employees.FIRST_NAME, ' ', employees.LAST_NAME)) AS 'Nome completo',
+history.START_DATE AS 'Data de início',
+employees.SALARY AS Salário
+from hr.job_history as history
+left join hr.employees as employees
+on history.EMPLOYEE_ID = employees.EMPLOYEE_ID
+where month(history.START_DATE) in (1,2,3)
+order by 1,2;
