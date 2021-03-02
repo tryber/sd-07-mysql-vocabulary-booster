e8e496afc1af51426d0c0a12b20ff8972d53bbca
josielvs <josielvs@gmail.com>
2021-03-02 19:12:52 -0300

    Desafio 3 - Refatorado

--- desafio3.sql
+++ desafio3.sql
@@ -1,4 +1,3 @@
-USE hr;
-SELECT JOB_TITLE AS 'Cargo', (MAX_SALARY - MIN_SALARY)
-AS 'Diferença entre salários máximo e mínimo' FROM jobs
-ORDER BY (MAX_SALARY - MIN_SALARY), JOB_TITLE;
+SELECT j.JOB_TITLE AS 'Cargo', (MAX_SALARY - MIN_SALARY)
+AS 'Diferença entre salários máximo e mínimo' FROM hr.jobs AS j
+ORDER BY (MAX_SALARY - MIN_SALARY), j.JOB_TITLE;
