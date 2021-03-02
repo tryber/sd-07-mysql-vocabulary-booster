afb7e56e4b652b833ccd7c5138dbafbf01d3fc39
PedroMarqDev <PedroMarqDev@gmail.com>
2021-03-02 16:53:26 -0300

    desafio6.sql line 6 identation

--- desafio6.sql
+++ desafio6.sql
@@ -3,7 +3,7 @@ CONCAT(FIRST_NAME, ' ', LAST_NAME) AS `Nome completo`,
 j.JOB_TITLE AS Cargo,
 jh.START_DATE AS `Data de início do cargo`,
 d.DEPARTMENT_NAME AS Departamento
- FROM hr.employees AS e
+FROM hr.employees AS e
 INNER JOIN hr.jobs AS j ON
 e.JOB_ID = j.JOB_ID
 INNER JOIN hr.job_history AS jh ON
