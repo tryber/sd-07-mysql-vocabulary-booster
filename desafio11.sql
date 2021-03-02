29db621f4a137469d837c983b4632d1f28783e65
Filipi Firmino <contatoic3web@gmail.com>
2021-03-02 19:34:17 -0300

    chalenge 11 add where

--- desafio11.sql
+++ desafio11.sql
@@ -3,7 +3,7 @@ SELECT
   CUSTO.Country AS 'País',
   num_pat.num - 1	AS 'Número de compatriotas'
 FROM
- (SELECT Country, COUNT(Country) AS num FROM w3schools.customers GROUP BY Country) AS num_pat
+  (SELECT Country, COUNT(Country) AS num FROM w3schools.customers GROUP BY Country) AS num_pat
 INNER JOIN
   w3schools.customers AS CUSTO
 ON
