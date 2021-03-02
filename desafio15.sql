2e0b70dc3a4ceecb5ef7b71245cfe08784fbd604
Vitor Rodrigues <vitor_rc@outlook.com>
2021-03-02 18:17:31 -0300

    Ajuste no Req 16

--- desafio15.sql
+++ desafio15.sql
@@ -1,6 +1,6 @@
 USE hr;
 DELIMITER $$
-CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(300))
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(50))
 BEGIN
 SELECT ROUND(AVG(emp.SALARY),2) AS `Média salarial`
 FROM hr.jobs AS jb
--- desafio16.sql
+++ desafio16.sql
@@ -1,6 +1,6 @@
 USE hr;
 DELIMITER $$
-CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario (email VARCHAR(300))
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario (email VARCHAR(50))
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE total INT;
@@ -8,7 +8,7 @@ SELECT COUNT(*) AS total_empregos
 FROM hr.employees AS emp
 INNER JOIN hr.job_history AS jh
 ON emp.EMPLOYEE_ID = jh.EMPLOYEE_ID
-WHERE emp.EMAIL = @email INTO total;
+WHERE emp.EMAIL = email INTO total;
 RETURN total;
 END $$
 DELIMITER ;
