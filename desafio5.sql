1ecd65195c8d41d906ba952f47b7dc81969af692
w27M <marcos.1998226@gmail.com>
2021-03-02 15:15:03 -0300

    exercicio 6

--- desafio5.sql
+++ desafio5.sql
@@ -1,4 +1,4 @@
-SELECT JOB_TITLE AS 'Cargo', (MAX_SALARY - MIN_SALARY) AS 'Variação Salarial',
+SELECT JOB_TITLE AS 'Cargo', (MAX_SALARY - MIN_SALARY) AS 'Variação Salarial', 
 ROUND(AVG(MIN_SALARY/12),2) AS 'Média mínima mensal', 
 ROUND(AVG(MAX_SALARY/12),2) AS 'Média máxima mensal'
 FROM hr.jobs
