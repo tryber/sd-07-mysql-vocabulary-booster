fc2686a4247026f952fdcb603ce4a4d2ba925f2d
BrunaSepulveda <sepulvedabruna@gmail.com>
2021-03-02 14:51:18 -0300

    muda a clausula

--- desafio7.sql
+++ desafio7.sql
@@ -1,10 +1,9 @@
 SELECT 
-UPPER(CONCAT(employees.first_name, " ",employees.last_name)) 
-AS `Nome completo`,
+UCASE(CONCAT(employees.first_name, " ",employees.last_name)) AS `Nome completo`,
 job_history.start_date AS `Data de início`,
 employees.salary AS `Salário`
 FROM hr.employees AS employees
 INNER JOIN hr.job_history AS job_history 
-ON job_history.job_id = employees.job_id
+ON job_history.employee_id = employees.employee_id
 WHERE MONTH(job_history.start_date) BETWEEN 1 AND 3
 ORDER BY `Nome completo`, `Data de início`;
