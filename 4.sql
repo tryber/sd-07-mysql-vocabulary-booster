44071759551174b830ca80091e4c6633ce0a821d
Elano Evaristo <elano3004@gmail.com>
2021-03-02 19:22:45 -0300

    Modificando arquivos para ver se o avaliador avalia

--- 4.sql
+++ 4.sql
@@ -1,17 +1,4 @@
 -- Desafio 4
--- Obtenha informações a respeito dos salários das pessoas empregadas.
--- Use o banco hr como referência
--- Crie uma query que exiba 03 colunas:
--- A primeira deve possuir o alias "Cargo" e exibir o nome do cargo.
--- A segunda deve possuir o alias "Média salarial" e exibir a média dos salários das pessoas empregadas que possuem o cargo em questão.
--- A terceira deve possuir o alias "Senioridade" e categorizar o nível de senioridade do cargo com base na média salarial, da seguinte forma:
--- Média salarial entre 2000 - 5800 -> Júnior
--- Média salarial entre 5801 - 7500 -> Pleno
--- Média salarial entre 7501 - 10500 -> Sênior
--- Média salarial acima de 10500 -> CEO
--- Sempre que fizer uso da média salarial, arredonde o valor para 02 casas decimais.
--- Os resultados devem estar ordenados pela média salarial em ordem crescente.
--- Em caso de empate na média, os resultados devem ser ordenados pelo nome do cargo em ordem alfabética.
 
 SELECT j.JOB_TITLE AS `Cargo`, ROUND(AVG(e.SALARY),2) AS 'Média salarial',
 CASE
