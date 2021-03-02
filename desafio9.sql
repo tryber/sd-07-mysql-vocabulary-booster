4adc21b1d64d9ee3ea2d79c8944f1e901d28a09d
rodrigoderkian <derkianpromo@gmail.com>
2021-03-02 11:35:52 -0300

    req9

---
+++ desafio9.sql
@@ -0,0 +1,10 @@
+SELECT 
+    CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo',
+    COUNT(o.EmployeeId) AS 'Total de pedidos'
+FROM
+    w3schools.orders AS o
+        INNER JOIN
+    w3schools.employees AS e ON o.EmployeeId = e.EmployeeId
+GROUP BY o.EmployeeId
+ORDER BY `Total de pedidos`;
+        
