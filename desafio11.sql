54dfe4a60961c187b22516d427994b0f21fc3778
nevesfreire <nando.neves@gmail.com>
2021-03-02 08:41:43 -0300

    teste 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,7 @@
 SELECT 
 t1.ContactName as Nome,
 t1.Country AS País,
-count(*)
+count(*) AS 'Número de compatriotas'
 FROM w3schools.customers AS t1, w3schools.customers AS t2
 WHERE t1.Country = t2.Country AND t1.CustomerID <> t2.CustomerID
 GROUP BY t1.CustomerID
