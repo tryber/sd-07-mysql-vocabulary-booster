61ea1b52672f72cf3b0022c6a0f9993b033d71b7
spawnred <bruno.spawnhell@gmail.com>
2021-03-02 00:40:15 -0300

    corrigindo item 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,5 @@
 SELECT
-CONCAT(employees.FIRST_NAME, " ", employees.LAST_NAME) AS "Nome completo",
+UCASE(CONCAT(employees.FIRST_NAME, " ", employees.LAST_NAME)) AS "Nome completo",
 START_DATE AS "Data de início",
 employees.SALARY AS "Salário"
 FROM hr.job_history
