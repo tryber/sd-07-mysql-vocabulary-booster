9065100f6a59c0558dcad708d918d0e0c3864a37
Dhiego Dias <ddias003@gmail.com>
2021-03-01 22:05:42 -0300

    Req 4, correçao acento

--- desafio4.sql
+++ desafio4.sql
@@ -4,7 +4,7 @@ j.job_title AS 'Cargo',
     CASE
 WHEN AVG(e.salary) BETWEEN 2000 AND 5800 THEN 'Júnior'
         WHEN AVG(e.salary) BETWEEN 5801 AND 7500 THEN 'Pleno'
-        WHEN AVG(e.salary) BETWEEN 7501 AND 10500 THEN 'Senior'
+        WHEN AVG(e.salary) BETWEEN 7501 AND 10500 THEN 'Sênior'
         WHEN AVG(e.salary) > 10500 THEN 'CEO'
 END AS 'Senioridade'
 FROM hr.jobs AS j
