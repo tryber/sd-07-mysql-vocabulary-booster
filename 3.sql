902bb362eb6eca226e080b0e82b4587fba4b380c
Elano Evaristo <elano3004@gmail.com>
2021-03-02 20:47:21 -0300

    Delete 3.sql

--- 3.sql
+++
@@ -1,7 +0,0 @@
--- Desafio 3
-
-SELECT 
-JOB_TITLE AS 'Cargo', 
-MAX_SALARY - MIN_SALARY AS 'Diferença entre salários máximo e mínimo'
-FROM hr.jobs 
-ORDER BY `Diferença entre salários máximo e mínimo`, Cargo;
