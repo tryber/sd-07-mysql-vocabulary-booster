a3add4c508abdd5729450f5accad6e0b0afd37e6
marcelosoliveira <msbobsk8@gmail.com>
2021-03-02 13:22:34 -0300

    Resolvendo CodeClimate

--- desafio15.sql
+++ desafio15.sql
@@ -2,12 +2,16 @@ USE hr
 DELIMITER $$ 
 CREATE PROCEDURE buscar_media_por_cargo(IN jobs VARCHAR(100))
   BEGIN
-	SELECT ROUND(AVG(e.SALARY), 2)
+	SELECT 
+    ROUND(AVG(e.SALARY), 2)
     AS 'Média salarial'
-    FROM hr.employees AS e 
-	INNER JOIN jobs AS j
+    FROM hr.employees
+    AS e 
+	INNER JOIN jobs
+    AS j
     ON e.JOB_ID = j.JOB_ID
-	WHERE j.JOB_TITLE = jobs;
+	WHERE
+    j.JOB_TITLE = jobs;
   END;
 
 $$ DELIMITER ;
--- desafio20.sql
+++ desafio20.sql
@@ -2,7 +2,9 @@ USE hr;
 DELIMITER $$ 
 CREATE PROCEDURE exibir_historico_completo_por_funcionario(IN email VARCHAR(100))
   BEGIN
-    SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
+    SELECT
+    CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)
+    AS 'Nome completo',
 	d.DEPARTMENT_NAME
     AS 'Departamento',
     j.JOB_TITLE
@@ -18,8 +20,10 @@ CREATE PROCEDURE exibir_historico_completo_por_funcionario(IN email VARCHAR(100)
     INNER JOIN hr.departments
     AS d
     ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
-    WHERE e.EMAIL = email
-	ORDER BY d.DEPARTMENT_NAME, j.JOB_TITLE;
+    WHERE
+    e.EMAIL = email
+	ORDER BY
+    d.DEPARTMENT_NAME, j.JOB_TITLE;
   END;
 
 $$ DELIMITER ;
