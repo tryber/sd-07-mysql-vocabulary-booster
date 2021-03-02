ad4f29a3adc488ac71213fcbb20389e36d9362b0
vitorAmorims <vtamorims@gmail.com>
2021-03-02 14:03:46 -0300

    corrigindo erro de sintaxe

--- desafio13.sql
+++ desafio13.sql
@@ -22,4 +22,3 @@ SELECT
 FROM products
 WHERE products.ProductID IN (SELECT order_details.ProductID FROM order_details GROUP BY order_details.ProductID HAVING SUM(order_details.Quantity) > 80)
 ORDER BY Produto;
-
