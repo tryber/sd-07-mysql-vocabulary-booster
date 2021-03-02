592ced6134af180516279e22fa777b705e7ae0f8
renatomak <renato.mark.silva@gmail.com>
2021-03-02 10:46:33 -0300

    Desafio 18-modificado-3

--- desafio18.sql
+++ desafio18.sql
@@ -2,7 +2,7 @@ SELECT
 CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) `Nome completo`,
 DATE_FORMAT(JH.START_DATE, '%d/%m/%Y') `Data de início`,
 DATE_FORMAT(JH.END_DATE, '%d/%m/%Y')  `Data de rescisão`,
-ROUND(DATEDIFF(JH.END_DATE, JH.START_DATE)/365.25, 2) `Anos trabalhados`
+ROUND(DATEDIFF(JH.END_DATE, JH.START_DATE)/365, 2) `Anos trabalhados`
 FROM hr.job_history JH, hr.employees E
 WHERE E.EMPLOYEE_ID=JH.EMPLOYEE_ID
 ORDER BY `Nome completo`, `Anos trabalhados`;
