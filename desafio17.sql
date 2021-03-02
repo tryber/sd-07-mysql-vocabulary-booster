0a73909e9c5e896c5875e30b540631f13f7c37f8
Andre Serudo <andreserudo@gmail.com>
2021-03-01 21:55:40 -0400

    desafio 12 e 17

---
+++ desafio17.sql
@@ -0,0 +1,14 @@
+-- Crie uma TRIGGER que, a cada nova inserção realizada na tabela orders, 
+-- insira automaticamente a data atual na coluna OrderDate.
+DELIMITER $$
+
+CREATE TRIGGER NEW_INSERT_ORDERS
+BEFORE INSERT ON w3schools.orders
+FOR EACH ROW
+BEGIN
+SET NEW.OrderDate = NOW();
+END;
+
+$$
+
+DELIMITER ;
