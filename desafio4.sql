5789d7726a0fcc661d16d51d358e5000f41ec207
Andre Serudo <andreserudo@gmail.com>
2021-03-02 17:33:24 -0400

    desafio 4 e seus misterios

--- desafio4.sql
+++ desafio4.sql
@@ -16,13 +16,13 @@
 -- Em caso de empate na média, os resultados devem ser ordenados pelo nome do cargo em ordem alfabética.
 SELECT
 J.JOB_TITLE AS "Cargo",
-(SELECT ROUND(AVG(SALARY), 2) FROM hr.employees AS E WHERE E.JOB_ID = J.JOB_ID) AS "Média Salarial",
+(SELECT ROUND(AVG(E.SALARY), 2) FROM hr.employees AS E WHERE E.JOB_ID = J.JOB_ID) AS "Média Salarial",
 (SELECT
 CASE
-WHEN (ROUND(AVG(SALARY), 2) >= 2000 AND ROUND(AVG(SALARY), 2) <= 5800) THEN 'Júnior'
-WHEN (ROUND(AVG(SALARY), 2) >= 5801 AND ROUND(AVG(SALARY), 2) <= 7500) THEN 'Pleno'
-WHEN (ROUND(AVG(SALARY), 2) >= 7501 AND ROUND(AVG(SALARY), 2) <= 10500) THEN 'Sénior'
-WHEN ROUND(AVG(SALARY), 2) > 10500 THEN 'CEO'
+WHEN (ROUND(AVG(E.SALARY), 2) >= 2000 AND ROUND(AVG(E.SALARY), 2) <= 5800) THEN 'Júnior'
+WHEN (ROUND(AVG(E.SALARY), 2) >= 5801 AND ROUND(AVG(E.SALARY), 2) <= 7500) THEN 'Pleno'
+WHEN (ROUND(AVG(E.SALARY), 2) >= 7501 AND ROUND(AVG(E.SALARY), 2) <= 10500) THEN 'Sénior'
+WHEN ROUND(AVG(E.SALARY), 2) > 10500 THEN 'CEO'
 END
 FROM hr.employees AS E WHERE E.JOB_ID = J.JOB_ID) AS "Senioridade"
 FROM	hr.jobs as J
