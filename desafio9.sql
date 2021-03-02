0ea8f5c67b2cd03440a4cb91720e3eae0318ad86
Camila Valentim <camilaifpb@hotmail.com>
2021-03-02 09:54:28 -0300

    corrigindo o desafio 9

--- desafio9.sql
+++ desafio9.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(e.FirstName,' ',e.lastName) AS "Nome completo", count(o.Em) AS "Total de pedidos"
+SELECT CONCAT(e.FirstName,' ',e.lastName) AS "Nome completo", count(o.EmployeeID) AS "Total de pedidos"
 FROM w3schools.employees e
 inner join w3schools.orders o ON o.EmployeeID = e.EmployeeID 
 GROUP BY `Nome Completo`
