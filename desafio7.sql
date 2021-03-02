24e74a3552b815ed2dc76929360f7bb514f4809a
samantabelow <samanta.below@gmail.com>
2021-03-02 14:52:32 -0300

    Desafio 7 completo

--- desafio7.sql
+++ desafio7.sql
@@ -4,4 +4,5 @@ emp.SALARY AS 'Salário'
 FROM hr.job_history AS hist
 INNER JOIN hr.employees AS emp
 ON hist.EMPLOYEE_ID = emp.EMPLOYEE_ID
+WHERE MONTH(START_DATE) < 4
 ORDER BY `Nome completo`, `Data de início`;
