84505f45e56ae2fb4c78bc0697ac9452e35169dd
Macarena Rodriguez <macarenar697@gmail.com>
2021-03-02 19:22:08 -0300

    req 6--

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,9 @@
-SELECT CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME ) AS "Nome completo", 
+SELECT CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME ) AS 'Nome completo', 
 jobs.JOB_TITLE AS Cargo,
-employees.HIRE_DATE AS 'Data de início do cargo',
+job_history.START_DATE AS 'Data de início do cargo',
 departments.DEPARTMENT_NAME AS 'Departamento'
-FROM hr.employees AS employees
-INNER JOIN hr.jobs AS jobs ON jobs.JOB_ID = employees.JOB_ID
-INNER JOIN hr.departments AS departments ON employees.DEPARTMENT_ID = departments.DEPARTMENT_ID
+FROM hr.job_history AS job_history
+INNER JOIN hr.jobs AS jobs ON jobs.JOB_ID = job_history.JOB_ID
+INNER JOIN hr.employees AS employees ON employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
+INNER JOIN hr.departments AS departments ON departments.DEPARTMENT_ID = job_history.DEPARTMENT_ID
 ORDER BY CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME ) DESC, JOB_TITLE;
