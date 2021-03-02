1f2426553ca1dd881c8e3144e8e586f9b6ad9a9d
BrunaSepulveda <sepulvedabruna@gmail.com>
2021-03-02 13:36:31 -0300

    refarotação TESTE ALTERA ORDEM INNER JOIN

--- desafio6.sql
+++ desafio6.sql
@@ -2,8 +2,8 @@ SELECT concat(employees.first_name, " ",employees.last_name) AS `Nome completo`,
 job.job_title AS `Cargo`,
 job_history.start_date AS `Data de início do cargo`,
 departments.department_name AS `Departamento`
-FROM hr.jobs AS job 
-INNER JOIN hr.employees AS employees ON job.job_id = employees.job_id
-INNER JOIN hr.departments AS departments ON departments.department_id = employees.department_id
-INNER JOIN hr.job_history AS job_history ON job_history.job_id = employees.job_id
+FROM hr.job_history AS job_history 
+INNER JOIN hr.employees AS employees  ON job_history.job_id = employees.job_id
+INNER JOIN hr.jobs AS job ON job_history.job_id = job.job_id
+INNER JOIN hr.departments AS departments ON job_history.department_id = departments.department_id 
 ORDER BY `Nome completo` DESC, `Cargo`;
