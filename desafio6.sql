8301affc891ff3c734c048b2268ca3646670ede2
BrunaSepulveda <sepulvedabruna@gmail.com>
2021-03-02 14:25:09 -0300

    base de comparação job_history baseado na resolução da vanessa bidinotto

--- desafio6.sql
+++ desafio6.sql
@@ -1,9 +1,12 @@
 SELECT concat(employees.first_name, " ",employees.last_name) AS `Nome completo`, 
-job.job_title AS `Cargo`,
+jobs.job_title AS `Cargo`,
 job_history.start_date AS `Data de início do cargo`,
 departments.department_name AS `Departamento`
-FROM hr.job_history AS job_history 
-INNER JOIN hr.employees AS employees  ON job_history.job_id = employees.job_id
-INNER JOIN hr.jobs AS job ON job_history.job_id = job.job_id
-INNER JOIN hr.departments AS departments ON job_history.department_id = departments.department_id 
+FROM hr.job_history AS job_history
+INNER JOIN hr.employees AS employees
+ON job_history.employee_id = employees.employee_id
+INNER JOIN hr.jobs AS jobs
+ON job_history.job_id = jobs.job_id
+INNER JOIN hr.departments AS departments
+ON job_history.department_id = departments.department_id
 ORDER BY `Nome completo` DESC, `Cargo`;
