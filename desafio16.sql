63bae2c84f2b0a28606e4915c3ff4ebdee3fc9ea
Muniz <vinicius.muniz.marin@gmail.com>
2021-03-02 20:20:25 -0300

    desafio16

--- desafio16.sql
+++ desafio16.sql
@@ -5,14 +5,14 @@ CREATE FUNCTION
 buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(100))
 RETURNS INT READS SQL DATA
 BEGIN
-    DECLARE resultado INT;
-    SELECT COUNT(*)
-    FROM job_history j
-    INNER JOIN employees e
-	ON j.employee_id=e.employee_id
-	WHERE e.email=email
-    GROUP BY email INTO resultado;
-    RETURN resultado;
+DECLARE resultado INT;
+SELECT COUNT(*)
+FROM job_history j
+INNER JOIN employees e
+ON j.employee_id=e.employee_id
+WHERE e.email=email
+GROUP BY email INTO resultado;
+RETURN resultado;
 END $$;
 
 DELIMITER ;
