904b2ba2db626a8a26681840b2a670be08f04b17
murilopereira006 <muriloedu006@gmail.com>
2021-03-02 18:44:49 -0300

    desafio 8 finalmente resolvido

--- desafio8.sql
+++ desafio8.sql
@@ -2,8 +2,9 @@ SELECT CUST.CustomerName AS "Nome de contato",
 SHIP.ShipperName AS "Empresa que fez o envio",
 ORD.OrderDate AS "Data do pedido"
 FROM w3schools.customers AS CUST
-INNER JOIN w3schools.shippers AS SHIP
 INNER JOIN w3schools.orders AS ORD
-ON CUST.CustomerID = ORD.CustomerID AND SHIP.ShipperID = ORD.ShipperID
+ON CUST.CustomerID = ORD.CustomerID
+INNER JOIN w3schools.shippers AS SHIP
+ON SHIP.ShipperID = ORD.ShipperID
 WHERE SHIP.ShipperName IN ('Speedy Express', 'United Package')
-ORDER BY CUST.CustomerName ASC, SHIP.ShipperName ASC, ORD.OrderDate ASC;
+ORDER BY `Nome de contato` ASC, `Empresa que fez o envio` ASC, `Data do pedido` ASC;
