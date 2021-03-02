9a5655e2ac2b41f72745011cd65df4092aaf2e6e
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2021-03-01 23:48:43 -0300

    fix: desafio4.

--- desafio4.sql
+++ desafio4.sql
@@ -2,7 +2,7 @@ SELECT j.JOB_TITLE AS 'Cargo', ROUND(avg(s.salary), 2) AS 'Média Salarial',
 CASE
 WHEN ROUND(avg(s.salary), 2) >= 2000.00 AND ROUND(avg(s.salary), 2) <= 5800.00 THEN 'Júnior'
 WHEN ROUND(avg(s.salary), 2) > 5800.00 AND ROUND(avg(s.salary), 2) <= 7500.00 THEN 'Pleno'
-WHEN ROUND(avg(s.salary), 2) > 7500.00 AND ROUND(avg(s.salary), 2) <= 10500.00 THEN 'Senior'
+WHEN ROUND(avg(s.salary), 2) > 7500.00 AND ROUND(avg(s.salary), 2) <= 10500.00 THEN 'Sênior'
 ELSE 'CEO'
 END AS 'Senioridade'
 FROM hr.jobs AS j
