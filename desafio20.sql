7ca84aecc2a4d1ee5aa3d2235e89cf6e6758c2e4
marcelosoliveira <msbobsk8@gmail.com>
2021-03-02 13:06:37 -0300

    Resolvendo CodeClimate

--- desafio20.sql
+++ desafio20.sql
@@ -1,17 +1,20 @@
 USE hr;
 DELIMITER $$
  
-  CREATE PROCEDURE exibir_historico_completo_por_funcionario(IN email VARCHAR(100))
-    BEGIN
-      SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
-	  d.DEPARTMENT_NAME Departamento, j.JOB_TITLE Cargo
-	  FROM hr.employees AS e
-      INNER JOIN hr.job_history AS jh ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
-      INNER JOIN hr.jobs AS j ON j.JOB_ID = jh.JOB_ID
-      INNER JOIN hr.departments AS d ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
-      WHERE e.EMAIL = email
-	  ORDER BY d.DEPARTMENT_NAME, j.JOB_TITLE;
-    END;
+CREATE PROCEDURE exibir_historico_completo_por_funcionario(IN email VARCHAR(100))
+  BEGIN
+    SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
+	d.DEPARTMENT_NAME AS 'Departamento', j.JOB_TITLE AS 'Cargo'
+    FROM hr.employees AS e
+    INNER JOIN hr.job_history AS jh
+    ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+    INNER JOIN hr.jobs AS j
+    ON j.JOB_ID = jh.JOB_ID
+    INNER JOIN hr.departments AS d
+    ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
+    WHERE e.EMAIL = email
+	ORDER BY d.DEPARTMENT_NAME, j.JOB_TITLE;
+  END;
 
 $$ DELIMITER ;
 
