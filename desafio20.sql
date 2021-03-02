bd4d0f99c324bd09fee3d67bc8e1e63485bdf8d3
marcelosoliveira <msbobsk8@gmail.com>
2021-03-02 13:34:29 -0300

    Resolvendo CodeClimate

--- desafio20.sql
+++ desafio20.sql
@@ -4,8 +4,8 @@ CREATE PROCEDURE exibir_historico_completo_por_funcionario(IN email VARCHAR(100)
 BEGIN
   SELECT
     CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
-	d.DEPARTMENT_NAME AS 'Departamento',
-    j.JOB_TITLE AS 'Cargo'
+    j.JOB_TITLE AS 'Cargo',
+    d.DEPARTMENT_NAME AS 'Departamento'
   FROM
     hr.employees AS e
   INNER JOIN hr.job_history AS jh
