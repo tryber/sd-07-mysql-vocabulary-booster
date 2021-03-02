fd422eca459613ec77e80dc85bea3278a470626b
BrunaSepulveda <sepulvedabruna@gmail.com>
2021-03-02 12:27:20 -0300

    refarotação de cc

--- desafio7.sql
+++ desafio7.sql
@@ -1,8 +1,8 @@
 SELECT 
-	UPPER(CONCAT(employees.first_name, " ",employees.last_name)) 
-    AS `Nome completo`,
-	job_history.start_date AS `Data de início`,
-    employees.salary AS `Salário`
+UPPER(CONCAT(employees.first_name, " ",employees.last_name)) 
+AS `Nome completo`,
+job_history.start_date AS `Data de início`,
+employees.salary AS `Salário`
 FROM hr.employees AS employees
 INNER JOIN hr.job_history AS job_history 
 ON job_history.job_id = employees.job_id
