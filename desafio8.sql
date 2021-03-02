2147f886e38ed182778be57a3ecee7a073374bff
nevesfreire <nando.neves@gmail.com>
2021-03-02 01:32:00 -0300

    teste 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,6 +1,6 @@
 
 SELECT 
-customers.CustomerName AS 'Nome de contato',
+customers.ContactName AS 'Nome de contato',
 shippers.ShipperName AS 'Empresa que fez o envio',
 orders.OrderDate  AS 'Data do pedido'
 FROM w3schools.customers AS customers 
@@ -9,4 +9,4 @@ ON customers.CustomerID = orders.CustomerID
 INNER JOIN w3schools.shippers AS shippers
 ON shippers.ShipperId = orders.ShipperID
 WHERE orders.ShipperID <>3
-ORDER BY customers.CustomerName, shippers.ShipperName, orders.OrderDate;
+ORDER BY customers.ContactName, shippers.ShipperName, orders.OrderDate;
