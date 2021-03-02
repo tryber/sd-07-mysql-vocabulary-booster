8d0b93b99ded35665a42989f9a4891c239d848d4
Danwhat <daanwhant@gmail.com>
2021-03-02 09:43:15 -0300

    8 e 17

--- desafio8.sql
+++ desafio8.sql
@@ -6,4 +6,4 @@ FROM w3schools.orders O
 JOIN w3schools.shippers S ON O.ShipperID = S.ShipperID
 JOIN w3schools.customers C ON O.CustomerID = C.CustomerID
 WHERE O.ShipperID = 1 OR O.ShipperID = 2
-ORDER BY C.ContactName, S.ShipperName;
+ORDER BY C.ContactName, S.ShipperName, O.OrderDate;
