67a9ffe80bf796935b346a41995718eeac41904a
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-03-02 13:25:31 -0300

    ajuste no requisito 11 - Uso do comando JOIN

---
+++ desafio10.sql
@@ -0,0 +1,10 @@
+SELECT p.ProductName AS "Produto",
+MIN(d.Quantity) AS "Mínima",
+MAX(d.Quantity) AS "Máximo",
+ROUND(AVG(d.Quantity), 2) AS "Média"
+FROM w3schools.products p
+INNER JOIN w3schools.order_details d
+ON p.ProductID = d.ProductID
+GROUP BY (d.ProductID)
+HAVING AVG(d.Quantity) > 20
+ORDER BY 4,1;
