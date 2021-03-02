727a3a004ad4e118b9c3451c245ce6752ead8c4f
nevesfreire <nando.neves@gmail.com>
2021-03-02 01:54:51 -0300

    teste

--- desafio9.sql
+++ desafio9.sql
@@ -1,6 +1,6 @@
 SELECT 
 concat(employees.FirstName, ' ', employees.LastName) AS 'Nome completo',
-count(*)
+count(*) AS 'Total de pedidos'
 FROM w3schools.employees AS employees
 INNER JOIN w3schools.orders AS orders
 ON employees.EmployeeID = orders.EmployeeID
