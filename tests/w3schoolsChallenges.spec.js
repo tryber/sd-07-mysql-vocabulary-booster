78ab6f0fdd3bb4681d391db0128e93823a9b0fd8
TIAGO <tiagoesdrasvieira@gmail.com>
2021-03-01 22:18:16 -0300

    Desafio 10

--- tests/w3schoolsChallenges.spec.js
+++ tests/w3schoolsChallenges.spec.js
@@ -50,7 +50,7 @@ describe('Desafios iniciais', () => {
   });
 
   describe('Exibe todos os produtos que já foram pedidos, que possuem uma média de quantidade nos pedidos registrados acima de `20.00`', () => {
-    it.skip('Verifica o desafio 10', async () => {
+    it('Verifica o desafio 10', async () => {
       const challengeQuery = readFileSync('desafio10.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult10');
 
