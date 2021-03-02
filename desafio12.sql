615c482c69f762c882793ebb31e0a65cc5d88ef5
Raphael Caputo <faelelf2@gmail.com>
2021-03-02 00:07:12 -0300

    Tentando consertar mais um

--- desafio12.sql
+++ desafio12.sql
@@ -6,10 +6,10 @@ SELECT
   emp2.SALARY AS 'Salário funcionário 2',
   emp2.PHONE_NUMBER AS 'Telefone funcionário 2'
 FROM
-  hr.employees AS emp1, hr.employees AS emp2
+  hr.employees AS `emp1`, hr.employees AS `emp2`
 WHERE
-  CONCAT(emp1.FIRST_NAME, ' ', emp1.LAST_NAME) <> CONCAT(emp2.FIRST_NAME, ' ', emp2.LAST_NAME)
-  AND emp1.JOB_ID = emp2.JOB_ID
+  emp1.JOB_ID = emp2.JOB_ID
+AND emp1.EMPLOYEE_ID <> emp2.EMPLOYEE_ID
 ORDER BY
   'Nome completo funcionário 1',
   'Nome completo funcionário 2';
