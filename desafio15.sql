4a1ca4f62d06f16f5d56a95cd265886e89ff866d
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2021-03-03 09:06:01 -0300

    Resolvendo codeclimate - 01/03/2021

--- desafio15.sql
+++ desafio15.sql
@@ -1,10 +1,9 @@
 USE hr;
 DELIMITER $$
-
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(100))
 BEGIN
-SELECT 
-ROUND(AVG(t1.SALARY), 2)
+SELECT
+ROUND(AVG(t1.SALARY), 2) AS `Média salarial`
 FROM
 employees AS t1
 INNER JOIN
@@ -12,5 +11,4 @@ jobs AS t2 ON t1.JOB_ID = t2.JOB_ID
 WHERE
 t2.JOB_TITLE = cargo;
 END $$
-
 DELIMITER ;
--- tests/hrChallenges.spec.js
+++ tests/hrChallenges.spec.js
@@ -163,7 +163,7 @@ describe('Desafios iniciais', () => {
     });
   });
 
-  describe('Toda pessoa funcionária no banco `hr` possui um histórico completo de cargos. Logo, crie uma procedure chamada `exibir_historico_completo_por_funcionario` que, dado o e-mail de uma pessoa funcionária, retorna todos os cargos em seu histórico', () => {
+  describe.only('Toda pessoa funcionária no banco `hr` possui um histórico completo de cargos. Logo, crie uma procedure chamada `exibir_historico_completo_por_funcionario` que, dado o e-mail de uma pessoa funcionária, retorna todos os cargos em seu histórico', () => {
     it('Verifica o desafio 20', async () => {
       const challengeQuery = readFileSync('desafio20.sql', 'utf8').trim();
       const createProcedureQuery = /CREATE PROCEDURE.*END/si.exec(challengeQuery)[0];
