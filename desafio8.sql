5f88143a21b2e94393cf0d260659257512a5d3bd
Lucas <lafdesouza2002@outlook.com>
2021-03-03 14:40:27 -0300

    Pq que nao ta passando maluco

--- desafio8.sql
+++ desafio8.sql
@@ -1,5 +1,5 @@
-SELECT t3.CustomerName AS `Nome de Contato`, t2.ShipperName AS `Empresa que fez o envio`, t1.OrderDate AS `Data do pedido`
+SELECT t3.CustomerName AS "Nome de Contato", t2.ShipperName AS "Empresa que fez o envio", t1.OrderDate AS "Data do pedido"
 FROM w3schools.orders AS t1
 INNER JOIN w3schools.shippers AS t2
 INNER JOIN w3schools.employees AS t3
-ORDER BY `Nome de Contato`, `Empresa que fez o envio`, `Data do pedido`
+ORDER BY "Nome de Contato", "Empresa que fez o envio", "Data do pedido"
