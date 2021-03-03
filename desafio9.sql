23677871523139c833160744933d5cd3550af687
Lucas <lafdesouza2002@outlook.com>
2021-03-03 12:39:48 -0300

    Desafio 8 e 9

---
+++ desafio9.sql
@@ -0,0 +1,6 @@
+SELECT t1.FirstName AS `Nome completo`, Count(t2.OrderID) AS `Total de pedidos`
+FROM w3schoools.Employees AS t1
+INNER JOIN w3schoools.Orders AS t2
+ON t1.EmployeeID = t2.EmployeeID
+GROUP BY `Nome completo`
+ORDER BY `Total de pedidos` DESC 
