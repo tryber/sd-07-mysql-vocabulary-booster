3f30d2a9b4e06d8df5461d24ccaf0bc812961e5a
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2021-03-03 07:45:22 -0300

    Resolvendo codeclimate - 01/03/2021

--- desafio16.sql
+++ desafio16.sql
@@ -5,13 +5,13 @@ RETURNS INT READS SQL DATA
 BEGIN
 DECLARE quantidadeDeEmpregos INT;
 SELECT 
-	COUNT(*)
+COUNT(*)
 FROM
-	job_history AS t1
-		INNER JOIN
-	employees AS t2 ON t1.EMPLOYEE_ID = t2.EMPLOYEE_ID
+job_history AS t1
+	INNER JOIN
+employees AS t2 ON t1.EMPLOYEE_ID = t2.EMPLOYEE_ID
 WHERE
-	t2.EMAIL = email INTO quantidadeDeEmpregos;
+t2.EMAIL = email INTO quantidadeDeEmpregos;
 RETURN quantidadeDeEmpregos;
 END $$
 DELIMITER ;
