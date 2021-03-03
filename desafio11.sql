a9a6dadfe7b5c9a1a1b1c4c46ba5477ae5937022
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2021-03-03 18:38:21 -0300

    Exercise 11 - done-2

--- desafio11.sql
+++ desafio11.sql
@@ -3,6 +3,6 @@ cus01.CustomerName AS `Nome`,
 cus01.Country AS `País`,
 COUNT(cus02.Country) AS `Número de compatriotas`
 FROM w3schools.customers AS cus01, w3schools.customers AS cus02
-WHERE cus01.Country = cus02.Country
-GROUP BY `Nome`
-ORDER BY `Nome`;
+WHERE cus01.ContactName <> cus02.ContactName AND cus01.Country = cus02.Country
+GROUP BY cus01.CustomerID
+ORDER BY cus01.ContactName;
