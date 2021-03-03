8992f6b7b9b10ecea7c38f79f83b2202840887a4
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2021-03-03 11:32:36 -0300

    Exercise 7 - done-2

--- desafio7.sql
+++ desafio7.sql
@@ -5,4 +5,5 @@ emp.SALARY AS `Salário`
 FROM hr.employees AS emp
 INNER JOIN hr.job_history AS his
 ON emp.EMPLOYEE_ID = his.EMPLOYEE_ID
+WHERE MONTH(his.START_DATE) IN (1, 2, 3)
 ORDER BY `Nome completo` ASC, `Data de início` ASC;
