d80a0bad125063795e2f62a2071b3677d9353ff5
Lucas <lafdesouza2002@outlook.com>
2021-03-03 19:55:22 -0300

    zzz

--- desafio8.sql
+++ desafio8.sql
@@ -1,8 +1,7 @@
-SELECT t3.CustomerName AS "Nome de Contato", t2.ShipperName AS "Empresa que fez o envio", DATE(t1.OrderDate) AS "Data do pedido"
+SELECT t3.CustomerName AS `Nome de Contato`, t2.ShipperName AS `Empresa que fez o envio`, DATE(t1.OrderDate) AS `Data do pedido`
 FROM w3schools.orders AS t1
 INNER JOIN w3schools.shippers AS t2
 INNER JOIN w3schools.customers AS t3
-ON t1.ShipperID = t2.ShipperID AND t1.CustomerID = t3.CustomerID
-GROUP BY "Nome de Contato"
-HAVING "Empresa que fez o envio" = "Speedy Express" OR "Empresa que fez o envio" = "United Package"
-ORDER BY "Nome de Contato", "Empresa que fez o envio", "Data do pedido";
+ON t1.ShipperID = t2.ShipperID t1.CustomerID = t3.CustomerID
+HAVING `Empresa que fez o envio` = `Speedy Express` OR `Empresa que fez o envio` = `United Package`
+ORDER BY `Nome de Contato`, `Empresa que fez o envio`, `Data do pedido`;
