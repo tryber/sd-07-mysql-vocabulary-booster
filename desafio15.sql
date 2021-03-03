9743d3aeea200f87169aae6a6607b085fb73aaba
Carol Bezerra <carol.arbe@gmail.com>
2021-03-03 18:22:30 -0300

    Requisito 15 com CC tentativa 2

--- desafio15.sql
+++ desafio15.sql
@@ -2,8 +2,7 @@ USE hr;
 DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN jobTitle VARCHAR(100))
-BEGIN
-	SELECT ROUND(AVG(e.salary), 2) AS 'Média salarial' FROM hr.employees AS e INNER JOIN hr.jobs AS j ON e.job_id = j.job_id WHERE j.job_title = jobTitle;
+BEGIN SELECT ROUND(AVG(e.salary), 2) AS 'Média salarial' FROM hr.employees AS e INNER JOIN hr.jobs AS j ON e.job_id = j.job_id WHERE j.job_title = jobTitle;
 END $$
 
 DELIMITER ;
