33fd471309c6ad0966e3046c735d711e02fce9d3
Israel Menon <zezomenon@gmail.com>
2021-03-03 20:00:12 -0300

    Requisito 2 - corrigindo erro de relacionamento, que nao precisava

--- desafio2.sql
+++ desafio2.sql
@@ -1,11 +1,9 @@
   SELECT jobs.JOB_TITLE AS "Cargo", 
   CASE
-    WHEN employees.SALARY >= 5000 AND employees.SALARY <= 10000 THEN  "Baixo"
-    WHEN employees.SALARY >= 10001 AND employees.SALARY <= 20000 THEN  "Médio"
-    WHEN employees.SALARY >= 20001 AND employees.SALARY <= 30000 THEN  "Alto"
-    WHEN employees.SALARY > 30000 THEN  "Altíssimo"
+    WHEN jobs.MAX_SALARY >= 5000 AND jobs.MAX_SALARY <= 10000 THEN  "Baixo"
+    WHEN jobs.MAX_SALARY >= 10001 AND jobs.MAX_SALARY <= 20000 THEN  "Médio"
+    WHEN jobs.MAX_SALARY >= 20001 AND jobs.MAX_SALARY <= 30000 THEN  "Alto"
+    WHEN jobs.MAX_SALARY > 30000 THEN  "Altíssimo"
   END AS "Nível"
   FROM hr.jobs AS jobs
-  INNER JOIN hr.employees AS employees
-  ON jobs.JOB_ID = employees.JOB_ID
   ORDER BY jobs.JOB_TITLE;
