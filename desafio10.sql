5e35bd5312729c2b235333357c135ab710601e73
Macarena Rodriguez <macarenar697@gmail.com>
2021-03-02 21:14:37 -0300

    req 10-

--- desafio10.sql
+++ desafio10.sql
@@ -1,7 +1,7 @@
 SELECT products.ProductName AS Produto,
 MIN(order_details.Quantity) AS Mínima,
 MAX(order_details.Quantity) AS Máxima,
-AVG(order_details.Quantity) AS Média
+ROUND(AVG(order_details.Quantity), 2) AS Média
 FROM w3schools.order_details AS order_details
 INNER JOIN w3schools.products AS products ON products.ProductID = order_details.ProductID
 GROUP BY ProductName
