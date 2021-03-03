1187d20f0d8fce71045c61c82430680960d3a6b6
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-03-03 20:55:15 -0300

    ajuste no requisito 08

--- desafio8.sql
+++ desafio8.sql
@@ -5,5 +5,5 @@ FROM w3schools.orders AS o
 INNER JOIN w3schools.customers AS c
 ON c.CustomerID = o.CustomerID
 INNER JOIN w3schools.shippers AS s
-ON s.ShipperID = o.ShipperID
+ON s.ShipperID = o.ShipperID AND s.ShipperName = "Speedy Express" OR s.ShipperName = "United Package"
 ORDER BY 1,2;
