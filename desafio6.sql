66d8cd7f39788c5fb65bf2703b18eb0b325c801e
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2021-03-02 23:06:53 -0300

    desafios 7, 8 e 9.

--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,7 @@
-SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS "Nome completo", j.JOB_TITLE AS "Cargo", h.START_DATE AS "Data de início do cargo", d.DEPARTMENT_NAME AS "Departamento"
+SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS "Nome completo",
+j.JOB_TITLE AS "Cargo",
+h.START_DATE AS "Data de início do cargo",
+d.DEPARTMENT_NAME AS "Departamento"
 FROM hr.job_history AS h
 INNER JOIN hr.jobs AS j
 ON h.JOB_ID = j.JOB_ID
