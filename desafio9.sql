728c001dbc5ec033c05ac625f6b5569b851ad9d8
Lucas <lafdesouza2002@outlook.com>
2021-03-03 20:05:44 -0300

    Bora

--- desafio9.sql
+++ desafio9.sql
@@ -1,6 +1,6 @@
 SELECT CONCAT(t1.FirstName, ' ', t1.LastName) AS `Nome completo`, Count(t2.OrderID) AS `Total de pedidos`
-FROM w3schoools.Employees AS t1
-INNER JOIN w3schoools.Orders AS t2
+FROM w3schoools.employees AS t1
+INNER JOIN w3schoools.orders AS t2
 ON t1.EmployeeID = t2.EmployeeID
 GROUP BY `Nome completo`
 ORDER BY `Total de pedidos` DESC;
