ff1e5212da87cbdf603aff8a4ddce9e5b8c0b1ed
PedroMarqDev <PedroMarqDev@gmail.com>
2021-03-02 23:05:57 -0300

    Added  spaces 11

--- desafio11.sql
+++ desafio11.sql
@@ -5,4 +5,4 @@ COUNT(t1.Country ) AS `Número de compatriotas`
 FROM w3schools.customers AS t1, w3schools.customers AS t2
 WHERE t2.Country = t1.Country AND t1.ContactName <> t2.ContactName
 GROUP BY t1.CustomerID
-ORDER BY t1.ContactName
+ORDER BY t1.ContactName;
