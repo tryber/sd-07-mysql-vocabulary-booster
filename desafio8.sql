999c789d316ee1562186f541077148f58cef28ea
Lucas Andrade <64823667+lucas-andrad@users.noreply.github.com>
2021-03-03 14:09:46 -0300

    Update desafio8.sql

--- desafio8.sql
+++ desafio8.sql
@@ -1,5 +1,5 @@
 SELECT t3.CustomerName AS `Nome de Contato`, 
-	   t2.ShipperName AS `Empresa que fez o envio`, 
+	     t2.ShipperName AS `Empresa que fez o envio`, 
        t1.OrderDate AS `Data do pedido`
 FROM w3schools.orders AS t1
 INNER JOIN w3schools.shippers AS t2
