8516f42dee60e963c94bb65615d2005459cc49a6
Lucas <lafdesouza2002@outlook.com>
2021-03-03 19:25:25 -0300

    Tentando desvendar o misterio

--- desafio8.sql
+++ desafio8.sql
@@ -1,4 +1,4 @@
-SELECT t3.CustomerName AS "Nome de Contato", t2.ShipperName AS "Empresa que fez o envio", t1.OrderDate AS "Data do pedido"
+SELECT t3.CustomerName AS "Nome de Contato", t2.ShipperName AS "Empresa que fez o envio", DATE(t1.OrderDate) AS "Data do pedido"
 FROM w3schools.orders AS t1
 INNER JOIN w3schools.shippers AS t2
 INNER JOIN w3schools.employees AS t3
--- desafio9.sql
+++ desafio9.sql
@@ -1,4 +1,4 @@
-SELECT t1.FirstName AS "Nome completo", Count(t2.OrderID) AS "Total de pedidos"
+SELECT CONCAT(t1.FirstName, ' ', t1.LastName) AS "Nome completo", Count(t2.OrderID) AS "Total de pedidos"
 FROM w3schoools.Employees AS t1
 INNER JOIN w3schoools.Orders AS t2
 ON t1.EmployeeID = t2.EmployeeID
