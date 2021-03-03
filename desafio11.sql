1c68298e7d43b60335daec2f1aeacf12733311bb
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2021-03-03 18:55:51 -0300

    Exercise 11 - done-5-lastest

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,5 @@
 SELECT
-cus01.CustomerName AS `Nome`,
+cus01.ContactName AS `Nome`,
 cus01.Country AS `País`,
 COUNT(cus02.Country) AS `Número de compatriotas`
 FROM w3schools.customers AS cus01, w3schools.customers AS cus02
