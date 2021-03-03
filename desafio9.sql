dc90da513524910faf0fd2c331dbe3abbebfcc01
Flavio Oliveira <flavsoliver@gmail.com>
2021-03-03 15:30:28 -0300

    desafio 9 - correção nome coluna

--- desafio9.sql
+++ desafio9.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(FirstName, " ", LastName) AS 'Nome Completo',
+SELECT CONCAT(FirstName, " ", LastName) AS 'Nome completo',
   COUNT(o.EmployeeID) AS 'Total de pedidos'
 FROM w3schools.employees AS e
   JOIN w3schools.orders AS o ON e.EmployeeID = o.EmployeeID
