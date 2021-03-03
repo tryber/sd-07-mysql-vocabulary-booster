b2d8f9653f424cf8bcb0453ef881a27a3a00e612
mateus <mateuslaalmeida@gmail.com>
2021-03-03 11:41:52 -0300

    Solucionados problemas de codeclimate

--- desafio15.sql
+++ desafio15.sql
@@ -3,12 +3,11 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(50))
 BEGIN
-    SELECT ROUND(AVG(e.SALARY), 2) AS 'Média salarial'
-    FROM employees AS e
-    INNER JOIN jobs AS j
-		ON j.JOB_ID = e.JOB_ID
-    WHERE JOB_TITLE = cargo;
+SELECT ROUND(AVG(e.SALARY), 2) AS 'Média salarial'
+FROM employees AS e
+INNER JOIN jobs AS j
+ON j.JOB_ID = e.JOB_ID
+WHERE JOB_TITLE = cargo;
 END $$
 
 DELIMITER ;
-
