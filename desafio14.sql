3d182c6cd5d23728958d3a664a6fab0532e1df9e
Elano Evaristo <elano3004@gmail.com>
2021-03-02 21:02:10 -0300

    correção desafio 14

--- desafio14.sql
+++ desafio14.sql
@@ -1,5 +1,4 @@
-SELECT DISTINCT Country AS 'País' FROM w3schools.customers
+SELECT Country AS 'País' FROM w3schools.customers
 UNION
-SELECT DISTINCT Country FROM w3schools.suppliers
-ORDER BY 'País'
-LIMIT 5;
+SELECT Country FROM w3schools.suppliers
+ORDER BY `País` LIMIT 5;
