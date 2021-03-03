7573c85b30c8a2b67cba01dd85c7e8efc612a9f0
Macarena Rodriguez <macarenar697@gmail.com>
2021-03-02 21:44:08 -0300

    req 10--

--- desafio10.sql
+++ desafio10.sql
@@ -6,4 +6,4 @@ FROM w3schools.order_details AS order_details
 INNER JOIN w3schools.products AS products ON products.ProductID = order_details.ProductID
 GROUP BY ProductName
 HAVING AVG(order_details.Quantity) > 20
-ORDER BY Média;
+ORDER BY Média, Produto;
