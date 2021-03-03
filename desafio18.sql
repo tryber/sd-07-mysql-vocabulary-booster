3cbebc6415e3746719de0e0acfcbc062933c3a99
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2021-03-03 06:57:58 -0300

    Resolvendo req 17, 18, 19 e 20 - 01/03/2021

---
+++ desafio18.sql
@@ -0,0 +1,19 @@
+SELECT 
+    CONCAT(t1.FIRST_NAME, ' ', t1.LAST_NAME) AS `Nome completo`,
+    CONCAT(DAY(t2.START_DATE),
+            '/',
+            MONTH(t2.START_DATE),
+            '/',
+            YEAR(t2.START_DATE)) AS `Data de início`,
+    CONCAT(DAY(t2.END_DATE),
+            '/',
+            MONTH(t2.END_DATE),
+            '/',
+            YEAR(t2.END_DATE)) AS `Data de rescisão`,
+    ROUND((YEAR(t2.END_DATE) - YEAR(t2.START_DATE)) + ((MONTH(t2.END_DATE) - MONTH(t2.START_DATE)) / 12) + ((DAY(t2.END_DATE) - DAY(t2.START_DATE)) / 360),
+            2) AS `Anos trabalhados`
+FROM
+    hr.employees AS t1
+        INNER JOIN
+    hr.job_history AS t2 ON t1.EMPLOYEE_ID = t2.EMPLOYEE_ID
+ORDER BY `Nome completo` ASC , `Anos trabalhados` ASC;
