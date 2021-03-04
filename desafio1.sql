567402a5ef03548982b9f939fbd64b0b16a14bf5
Strongreen <strongreen@live.com>
2021-03-04 13:35:38 -0300

    correçoes agora feitas no entendimento das logicas

--- desafio1.sql
+++ desafio1.sql
@@ -5,6 +5,6 @@ SELECT
         'não incluído') AS 'Status Inclusão'
 FROM
     hr.countries AS countries
-        LEFT JOIN
+        INNER JOIN
     hr.regions AS regions ON regions.REGION_ID = countries.REGION_ID
 ORDER BY `Pais`;
