f29d881cbc627a2280b320985524449365d3740e
Anderson Santana <anderslash@gmail.com>
2021-03-04 09:48:03 -0300

    Remocao de tabulacao no desafio 17

--- desafio17.sql
+++ desafio17.sql
@@ -1,7 +1,7 @@
 USE w3schools;
 DELIMITER $$
     CREATE TRIGGER value_column_insert
-	BEFORE INSERT ON w3schools.orders
+    BEFORE INSERT ON w3schools.orders
     FOR EACH ROW
         BEGIN
             SET NEW.OrderDate = NOW();
