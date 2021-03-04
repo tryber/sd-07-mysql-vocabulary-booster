426d1a7eaddcd1d3d70316df4c5a492181083869
CleberFontinele <cleberfontinele2387@gmail.com>
2021-03-04 14:05:51 -0300

    desafio 11 tirando quem nao tem compatriota com having <> 0

--- desafio11.sql
+++ desafio11.sql
@@ -2,6 +2,7 @@ SELECT A.ContactName AS 'Nome',
 A.Country AS 'País', 
 COUNT(B.Country) - 1 AS 'Número de compatriotas'
 FROM w3schools.customers AS A, w3schools.customers AS B
-WHERE A.Country = B.Country
+WHERE A.Country = B.Country 
 GROUP BY A.CustomerID
+HAVING `Número de compatriotas` <> 0
 ORDER BY A.ContactName;
