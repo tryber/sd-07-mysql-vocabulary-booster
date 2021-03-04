e651f871aada0d365968b828cf9ba4c74cabaaae
Lucas <lafdesouza2002@outlook.com>
2021-03-03 23:37:31 -0300

    Duvido que agora nao passa

--- desafio8.sql
+++ desafio8.sql
@@ -1,4 +1,4 @@
-SELECT t3.ContactName AS `Nome de Contato`, t2.ShipperName AS `Empresa que fez o envio`, DATE(t1.OrderDate) AS `Data do pedido`
+SELECT t3.ContactName AS `Nome de contato`, t2.ShipperName AS `Empresa que fez o envio`, DATE(t1.OrderDate) AS `Data do pedido`
 FROM w3schools.orders AS t1
 INNER JOIN w3schools.shippers AS t2
 ON t1.ShipperID = t2.ShipperID
