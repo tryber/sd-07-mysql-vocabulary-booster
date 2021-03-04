fe7e2582b73b75599e6f077efe377cb2829b1bcc
Lucas <lafdesouza2002@outlook.com>
2021-03-04 00:26:11 -0300

    Ajustes e ajustes

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,5 @@
-SELECT DISTINCT t1.ContactName AS `Nome`, t1.Country AS `País`, (SELECT COUNT(Country) FROM Customers WHERE Country = t2.Country GROUP BY Country) AS `Número de compatriotas`
-FROM Customers as t1, Customers as t2
+SELECT DISTINCT t1.ContactName AS `Nome`, t1.Country AS `País`, (SELECT COUNT(Country) FROM w3schools.customers WHERE Country = t2.Country GROUP BY Country) AS `Número de compatriotas`
+FROM w3schools.customers as t1, w3schools.customers as t2
 WHERE t1.ContactName <> t2.ContactName
 AND t1.Country = t2.Country
 ORDER BY `Nome`;
