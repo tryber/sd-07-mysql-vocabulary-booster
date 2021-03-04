18524ba064c4bf1ef89713566f6677a06f39e177
ericvini <ericcobain3@gmail.com>
2021-03-04 18:45:36 -0300

    fixing errors

--- desafio14.sql
+++ desafio14.sql
@@ -1,5 +1,5 @@
-(SELECT Country AS 'País' FROM w3schools.customers)
+(SELECT Country AS 'País' FROM customers)
 UNION 
-(SELECT  Country FROM w3schools.suppliers) 
+(SELECT  Country FROM suppliers) 
 ORDER BY 'País'
 LIMIT 5;
