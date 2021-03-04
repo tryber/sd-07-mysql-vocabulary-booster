6d72e83d95abff747b4b67d05a82c4024050bce3
Lucas <lafdesouza2002@outlook.com>
2021-03-04 12:33:52 -0300

    Ajustes

--- desafio10.sql
+++ desafio10.sql
@@ -1,5 +1,6 @@
-SELECT DISTINCT t2.ProductName AS `Produto`, (SELECT MIN(Quantity) FROM w3schools.orderdetails WHERE ProductID = t2.ProductID) AS `Mínima`, (SELECT MAX(Quantity) FROM w3schools.orderdetails WHERE ProductID = t2.ProductID) AS `Máxima`,(SELECT ROUND(AVG(Quantity), 2) FROM w3schools.orderdetails WHERE ProductID = t2.ProductID) AS `Média`
-FROM w3schools.orderdetails as t1
+SELECT DISTINCT t2.ProductName AS `Produto`, (SELECT MIN(Quantity) FROM w3schools.order_details WHERE ProductID = t2.ProductID) AS `Mínima`, (SELECT MAX(Quantity) FROM w3schools.order_details WHERE ProductID = t2.ProductID) AS `Máxima`,(SELECT ROUND(AVG(Quantity), 2) FROM w3schools.order_details WHERE ProductID = t2.ProductID) AS `Média`
+FROM w3schools.order_details as t1
 INNER JOIN w3schools.products as t2
 ON t1.ProductID = t2.ProductID
+WHERE `Média` > 20.00
 ORDER BY `Média`, `Produto`;
