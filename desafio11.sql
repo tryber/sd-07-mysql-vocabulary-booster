35af52f7b09a61e944b8d661dd165fe2f202565c
dennerpl <denner.souzapl@gmail.com>
2021-03-04 18:59:22 -0300

    finalmente resolvendo o bug do requisito 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,9 +1,8 @@
 SELECT t1.ContactName AS `Nome`,
   t1.Country AS `País`,
-  COUNT(t1.Country) - 1 AS `Número de compatriotas`
+  COUNT(t1.Country) AS `Número de compatriotas`
 FROM w3schools.customers AS t1,
   w3schools.customers AS t2
-WHERE t1.Country = t2.Country
-GROUP BY t1.ContactName
-HAVING `Número de compatriotas` > 0
+WHERE t1.Country = t2.Country AND t1.CustomerID <> t2.CustomerID
+GROUP BY t1.CustomerID
 ORDER BY t1.ContactName;
