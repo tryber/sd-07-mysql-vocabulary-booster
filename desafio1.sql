dced6f85fbc23128bc71c2883f38df24a1d412aa
jvitorof <j.vitoroflores@gmail.com>
2021-03-03 22:57:30 -0300

    VOCABULARY BOOSTER: :metal::rocket:

--- desafio1.sql
+++ desafio1.sql
@@ -1,7 +1,7 @@
 SELECT co.COUNTRY_NAME AS `País`,
 CASE
-	WHEN re.REGION_NAME = 'Europe' THEN 'incluído'
-	ELSE 'não incluído'
+WHEN re.REGION_NAME = 'Europe' THEN 'incluído'
+ELSE 'não incluído'
 END AS `Status Inclusão`
 FROM hr.countries AS co
 INNER JOIN regions AS re
