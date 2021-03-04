fa4b51da4d431946afee6b559fd35af72426b046
Strongreen <strongreen@live.com>
2021-03-04 11:51:55 -0300

    correçoes agora feitas no entendimento das logicas

--- desafio7.sql
+++ desafio7.sql
@@ -8,4 +8,8 @@ FROM
     hr.job_history AS histor
         INNER JOIN
     hr.employees AS employees ON histor.EMPLOYEE_id = employees.EMPLOYEE_ID
+    WHERE 
+		histor.START_DATE LIKE "%-01-%" OR 
+        histor.START_DATE LIKE "%-02-%" OR 
+        histor.START_DATE LIKE "%-03-%"
 ORDER BY `Nome completo` , `Data de início`;
