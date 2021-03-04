b8ef2f06d8fa739f6a344910a0c8267c1a3989fe
Lucas <lafdesouza2002@outlook.com>
2021-03-04 14:16:47 -0300

    Acho que agora vai

--- desafio14.sql
+++ desafio14.sql
@@ -1,7 +1,4 @@
-SELECT t1.Country AS `País`
-FROM w3schools.customers AS t1
-INNER JOIN w3schools.suppliers AS t2
-ON t1.Country <> t2.Country
-GROUP BY t1.Country
-ORDER BY t1.Country
+SELECT Country AS `País` FROM w3schools.customers 
+UNION
+SELECT Country FROM w3schools.uppliers
 LIMIT 5;
