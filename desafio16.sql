70297572fd1a382bfff764ff5cb6f5d7a6508562
josielvs <josielvs@gmail.com>
2021-03-04 20:07:46 -0300

    Desafio 16

--- desafio16.sql
+++ desafio16.sql
@@ -5,10 +5,10 @@ CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email varchar(100)
 RETURNS INT READS SQL DATA
 BEGIN
     DECLARE quantity INT;
-    SELECT COUNT(jh.EMPLOYEE_ID)
+    SELECT COUNT(hj.EMPLOYEE_ID)
     FROM hr.employees AS e
     INNER JOIN hr.job_history AS jh
-    ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+    ON e.EMPLOYEE_ID = hj.EMPLOYEE_ID
     WHERE e.EMAIL = email INTO quantity;
     RETURN quantity;
 END $$
