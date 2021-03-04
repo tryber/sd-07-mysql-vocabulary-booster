7ee1c7aeecf32ca40db0a0b3c057d46bde8fd890
Strongreen <strongreen@live.com>
2021-03-04 19:46:14 -0300

    exercicio 16 concluido

--- desafio16.sql
+++ desafio16.sql
@@ -4,14 +4,13 @@ CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(EMAIL VARCHAR(25))
 RETURNS INT READS SQL DATA
 BEGIN
     DECLARE jobsCount INT;
-    SELECT 
-    COUNT(*) 
+SELECT 
+    COUNT(*)
 FROM
     hr.employees AS employees
-    INNER JOIN
-    hr.job_history AS histor
-    ON 	employees.EMPLOYEE_ID = histor.EMPLOYEE_ID
- WHERE 
+        INNER JOIN
+    hr.job_history AS histor ON employees.EMPLOYEE_ID = histor.EMPLOYEE_ID
+WHERE
     employees.EMAIL = EMAIL INTO jobsCount;
   RETURN jobsCount;
 END $$
