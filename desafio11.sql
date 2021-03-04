86bfddab755edc3393296c8a80905c37010e057d
CleberFontinele <cleberfontinele2387@gmail.com>
2021-03-03 22:28:15 -0300

    desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,7 @@
+SELECT A.ContactName AS 'Nome', 
+A.Country AS 'País', 
+COUNT(B.Country) - 1 AS 'Número de compatriotas'
+FROM w3schools.customers AS A, w3schools.customers AS B
+WHERE A.Country = B.Country
+GROUP BY A.CustomerID
+ORDER BY A.ContactName;
