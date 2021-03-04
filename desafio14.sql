55535776e2691c76a8290300dbf5bb1207c1e3a2
Lucas <lafdesouza2002@outlook.com>
2021-03-04 13:49:20 -0300

    Ajuste

--- desafio14.sql
+++ desafio14.sql
@@ -1,6 +1,7 @@
-SELECT t1.Country
+SELECT t1.Country AS `País`
 FROM w3schools.customers AS t1
 INNER JOIN w3schools.suppliers AS t2
-ON t1.Country = t2.Country
+ON t1.Country <> t2.Country
 GROUP BY t1.Country
-ORDER BY t1.Country;
+ORDER BY t1.Country
+LIMIT 5;
