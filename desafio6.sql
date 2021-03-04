fc89b66cb3fcf02ace3e0c77bf00fad22a8d968d
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-03-03 21:27:22 -0300

    ajuste no requisito 06

--- desafio6.sql
+++ desafio6.sql
@@ -3,10 +3,10 @@ jobs.job_title AS "Cargo",
 historys.start_date AS "Data de início do cargo",
 departments.department_name AS "Departamento"
 FROM hr.employees AS employees
-INNER JOIN hr.jobs AS jobs
-ON employees.job_id = jobs.job_id
-INNER JOIN hr.departments AS departments
-ON employees.department_id = departments.department_id
 INNER JOIN hr.job_history AS historys
 ON employees.employee_id = historys.employee_id
+INNER JOIN hr.jobs AS jobs
+ON historys.job_id = jobs.job_id
+INNER JOIN hr.departments AS departments
+ON historys.department_id = departments.department_id
 ORDER BY 1 DESC, 2;
