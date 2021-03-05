ffd8cd23256d08c93e7fdb8637ab569130fe9e8e
Eduardo Pizorno <pizorno@gmail.com>
2021-03-05 08:50:55 -0300

    Challenge 1 refactor and challenge 2

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,5 @@
-SELECT ctr.COUNTRY_NAME AS 'País',
-  IF (rgn.REGION_NAME = 'Europe', 'incluído', 'não incluído')
+SELECT ctr.country_name AS 'País',
+  IF (rgn.region_name = 'Europe', 'incluído', 'não incluído')
   AS 'Status Inclusão' FROM hr.countries AS ctr
-  INNER JOIN hr.regions AS rgn ON ctr.REGION_ID = rgn.REGION_ID
-  ORDER BY c.COUNTRY_NAME ASC;
+  INNER JOIN hr.regions AS rgn ON ctr.region_id = rgn.region_id
+  ORDER BY ctr.country_name ASC;
