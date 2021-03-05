b222c24a5b991a790f2663812b2e5b3e131a64a6
Israel Menon <zezomenon@gmail.com>
2021-03-05 19:28:05 -0300

    Desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,13 @@
+  SELECT jobs.JOB_TITLE AS "Cargo", 
+  ROUND(((jobs.MAX_SALARY + jobs.MIN_SALARY) / 2), 2) AS "Média salarial",
+  CASE
+    WHEN ROUND(((jobs.MAX_SALARY + jobs.MIN_SALARY) / 2), 2) >= 2000 
+      AND ROUND(((jobs.MAX_SALARY + jobs.MIN_SALARY) / 2), 2) <= 5800 THEN  "Júnior"
+    WHEN ROUND(((jobs.MAX_SALARY + jobs.MIN_SALARY) / 2), 2) >= 5801 
+      AND ROUND(((jobs.MAX_SALARY + jobs.MIN_SALARY) / 2), 2) <= 7500 THEN  "Pleno"
+    WHEN ROUND(((jobs.MAX_SALARY + jobs.MIN_SALARY) / 2), 2) >= 7501 
+      AND ROUND(((jobs.MAX_SALARY + jobs.MIN_SALARY) / 2), 2) <= 10500 THEN  "Sênior"
+    WHEN ROUND(((jobs.MAX_SALARY + jobs.MIN_SALARY) / 2), 2) > 10500 THEN "CEO"
+  END AS "Senioridade"
+  FROM hr.jobs AS jobs
+  ORDER BY jobs.JOB_TITLE;
