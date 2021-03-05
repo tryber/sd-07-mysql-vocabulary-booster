99f834abce2f196a0a3e500b4a03d89e49099399
BrunaSepulveda <sepulvedabruna@gmail.com>
2021-03-05 18:42:30 -0300

    desafio 14

--- desafio14.sql
+++ desafio14.sql
@@ -1,7 +1,7 @@
-SELECT customers.country AS `País`
+SELECT customers.Country AS `País`
 FROM w3schools.customers AS customers 
-INNER JOIN w3schools.suppliers AS suppliers 
-ON customers.Country <> suppliers.Country
-GROUP BY customers.country
-ORDER BY customers.country
+UNION
+SELECT suppliers.Country AS `País`
+FROM w3schools.suppliers AS suppliers 
+ORDER BY `País`
 LIMIT 5;
