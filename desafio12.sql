c9b48ab2b8aca6993fc967e0a3cf4471898a8d53
CleberFontinele <cleberfontinele2387@gmail.com>
2021-03-04 21:58:40 -0300

    desafio 12

---
+++ desafio12.sql
@@ -0,0 +1,12 @@
+SELECT 
+CONCAT(F1.FIRST_NAME, ' ', F1.LAST_NAME) AS 'Nome completo funcionário 1',
+F1.SALARY AS 'Salário funcionário 1', 
+F1.PHONE_NUMBER AS 'Telefone funcionário 1',
+CONCAT(F2.FIRST_NAME, ' ', F2.LAST_NAME) AS 'Nome completo funcionário 1',
+F2.SALARY AS 'Salário funcionário 1', 
+F2.PHONE_NUMBER AS 'Telefone funcionário 1'
+FROM hr.employees AS F1
+INNER JOIN  hr.employees AS F2
+ON F1.JOB_ID = F2.JOB_ID 
+WHERE F1.EMPLOYEE_ID <> F2.EMPLOYEE_ID
+ORDER BY CONCAT(F1.FIRST_NAME, ' ', F1.LAST_NAME), CONCAT(F2.FIRST_NAME, ' ', F2.LAST_NAME);
