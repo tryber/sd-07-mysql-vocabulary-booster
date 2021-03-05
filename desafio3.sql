3704f5c3833081e0ca738666272bbf64a10400b6
Eduardo Pizorno <pizorno@gmail.com>
2021-03-05 09:42:22 -0300

    Challenge 3 refactor

--- desafio3.sql
+++ desafio3.sql
@@ -1,3 +1,4 @@
-SELECT hr.job_title AS 'Cargo', (MAX_SALARY - MIN_SALARY)
-AS 'Diferença entre salários máximo e mínimo' FROM hr.jobs 
-ORDER BY (MAX_SALARY - MIN_SALARY), hr.jb_title;
+SELECT jobs.job_title AS 'Cargo', (MAX_SALARY - MIN_SALARY)
+AS 'Diferença entre salários máximo e mínimo' FROM hr.jobs AS jobs
+ORDER BY (MAX_SALARY - MIN_SALARY), jobs.job_title;
+
