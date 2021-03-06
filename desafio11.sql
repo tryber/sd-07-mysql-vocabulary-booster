a9f803996940563ae3ab13be86d5325b5bd49e1a
Vinicius <vini@pop-os.localdomain>
2021-03-06 04:06:22 -0300

    Requisitos 11 Correção

--- desafio11.sql
+++ desafio11.sql
@@ -4,4 +4,6 @@ c1.Country as 'País',
 count(c2.Country) - 1 as 'Número de compatriotas'
 from w3schools.customers as c1
 inner join w3schools.customers as c2 on c1.Country = c2.Country
-group by `Nome` order by `Nome`;
+group by c1.CustomerID
+having `Número de compatriotas` != 0
+order by `Nome`;
