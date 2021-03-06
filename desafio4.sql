9a5163c433c860453683718b9cbf39516485705f
Quintiliano Dalete <quintilianodalete@gmail.com>
2021-03-06 14:54:25 -0300

    requirement5, ajusting requirements 3 and 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,7 +1,8 @@
-SELECT j.job_title AS 'Cargo', CAST(AVG(salary) AS DECIMAL(10,2)) AS '',
-CASE WHEN AVG(salary) BETWEEN 2000 AND 5800 THEN 'Júnior'
-CASE WHEN AVG(salary) BETWEEN 5801 AND 7500 THEN 'Pleno'
-CASE WHEN AVG(salary) BETWEEN 7501 AND 10500 THEN 'Sênior'
+SELECT j.job_title AS 'Cargo', CAST(AVG(salary) AS DECIMAL(10,2)) AS 'Média salárial',
+CASE 
+WHEN AVG(salary) BETWEEN 2000 AND 5800 THEN 'Júnior'
+WHEN AVG(salary) BETWEEN 5801 AND 7500 THEN 'Pleno'
+WHEN AVG(salary) BETWEEN 7501 AND 10500 THEN 'Sênior'
 ELSE 'CEO'
 END AS 'Senioridade'
 FROM hr.employees e
