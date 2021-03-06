8517e8b40c50c6c845129c2e50e58fa88736e0e8
Lorena Góes <goesloren@gmail.com>
2021-03-06 18:19:23 -0300

    fixing challenge 16

--- desafio16.sql
+++ desafio16.sql
@@ -1,10 +1,3 @@
--- SELECT * FROM hr.employees;
--- SELECT * FROM hr.job_history;
--- SELECT COUNT(*)
--- FROM hr.job_history jh
--- INNER JOIN hr.employees AS e
--- ON e.JOB_ID = jh.JOB_ID
--- WHERE e.EMAIL = email;
 USE hr;
 DELIMITER $$
 
@@ -13,11 +6,10 @@ RETURNS INT READS SQL DATA
 BEGIN
   DECLARE jobs_quantity INT;
   SELECT COUNT(*)
-  FROM hr.job_history jh
-  INNER JOIN hr.employees e
-  ON e.JOB_ID = jh.JOB_ID
+  FROM hr.employees e
+  INNER JOIN hr.job_history jh
+  ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
   WHERE e.EMAIL = email
-  GROUP BY jh.JOB_ID
   INTO jobs_quantity;
   RETURN jobs_quantity;
 END $$
