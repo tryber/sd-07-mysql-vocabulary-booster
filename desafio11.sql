beaf467c7efb0841a5ce30027c8b4e068acaaa51
Lorena Góes <goesloren@gmail.com>
2021-03-06 17:23:47 -0300

    fixing challenge 11

--- desafio11.sql
+++ desafio11.sql
@@ -4,5 +4,4 @@ COUNT(*) AS 'Número de compatriotas'
 FROM w3schools.customers AS c1, w3schools.customers AS c2
 WHERE c1.Country = c2.Country AND c1.CustomerID <> c2.CustomerID
 GROUP BY c1.CustomerID
-HAVING `Número de compatriotas` > 1
 ORDER BY `Nome`;
