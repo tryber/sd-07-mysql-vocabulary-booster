3a8b4d3163b03f0bc9a1aa9fb06dfb33c181d5da
Vinicius <vini@pop-os.localdomain>
2021-03-06 02:23:18 -0300

    Requisitos 6 Correção

--- desafio6.sql
+++ desafio6.sql
@@ -4,8 +4,8 @@ j.JOB_TITLE as 'Cargo',
 jh.START_DATE as 'Data de início do cargo',
 (select DEPARTMENT_NAME
 from hr.departments
-where DEPARTMENT_ID = e.DEPARTMENT_ID) as 'Departamento'
+where DEPARTMENT_ID = jh.DEPARTMENT_ID) as 'Departamento'
 from hr.employees as e
-inner join hr.jobs as j on e.JOB_ID = j.JOB_ID
 inner join hr.job_history as jh on e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+inner join hr.jobs as j on jh.JOB_ID = j.JOB_ID
 order by `Nome Completo` desc, `Cargo`;
