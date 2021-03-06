83f74c7c3fc87f74a0091aaedc11a3ad397c3480
Camila Valentim <camilaifpb@hotmail.com>
2021-03-06 14:53:09 -0300

    chega nos 80

---
+++ desafio8.sql
@@ -0,0 +1,13 @@
+SELECT
+c.ContactName AS "Nome de contato",
+s.ShipperName AS "Empresa que fez o envio",
+o.OrderDate AS "Data do pedido"
+
+FROM w3schools.customers AS c
+INNER JOIN w3schools.orders AS o
+ON c.CustomerID = o.CustomerID
+INNER JOIN w3schools.shippers AS s
+ON s.ShipperID = o.ShipperID
+/*Referencia: Felipe Alcatara*/
+WHERE o.ShipperID IN(1, 2)
+ORDER BY c.contactName, s.ShipperName, o.OrderDate;
