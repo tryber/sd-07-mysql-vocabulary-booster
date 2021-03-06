56b5c480ec70d998aa0b483d600696811ef96d3d
Quintiliano Dalete <quintilianodalete@gmail.com>
2021-03-06 14:22:20 -0300

    ajusting requirement 1 again

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,5 @@
 SELECT country_name AS 'País',
-IF (region_id = 1, 'Incluído', 'não Incluído')
+IF (region_id = 1, 'incluído', 'não incluído')
 AS 'Status Inclusão'
 FROM hr.countries
 ORDER BY country_name;
