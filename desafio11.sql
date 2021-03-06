1b0b06fce8492c4bb5de059d571403f5b782d6f9
Zeca Costa <jccostaso@gmail.com>
2021-03-06 09:24:27 -0300

    refact2 desafio11.sql

--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,10 @@
 SELECT
-ContactName AS Nome,
-Country AS País,
-COUNT(Country) - 1 AS `Número de compatriotas`
-FROM w3schools.customers
-GROUP BY País
+C1.ContactName AS Nome,
+C1.Country AS País,
+COUNT(C2.Country) - 1 AS `Número de compatriotas`
+FROM w3schools.customers AS C1
+INNER JOIN w3schools.customers AS C2
+ON C1.Country = C2.Country 
+GROUP BY C1.CustomerID
 HAVING `Número de compatriotas` > 0
 ORDER BY Nome;
