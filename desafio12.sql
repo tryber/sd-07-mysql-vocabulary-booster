c97ecc393db708845d024e280af005755fcf8dec
Quintiliano Dalete <quintilianodalete@gmail.com>
2021-03-06 16:44:43 -0300

    requirement 14, ajusting requirement 12

--- desafio12.sql
+++ desafio12.sql
@@ -4,7 +4,7 @@ e1.SALARY AS 'Salário funcionário 1',
 e1.phone_number AS 'Telefone funcionário 1',
 CONCAT(e2.first_name, ' ', e2.last_name) AS 'Nome completo funcionário 2',
 e2.SALARY AS 'Salário funcionário 2',
-e2.phone_number AS 'Telefone funcionário 2',
+e2.phone_number AS 'Telefone funcionário 2'
 FROM hr.employees e1
 JOIN hr.employees e2 ON e1.job_id = e2.job_id
 WHERE CONCAT(e1.first_name, ' ', e1.last_name) <> CONCAT(e2.first_name, ' ', e2.last_name)
