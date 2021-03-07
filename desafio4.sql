c6386c844f05a6b170a92c18c198e9cbba5c5db7
moacyrrln <moacyrrln@gmail.com>
2021-03-07 14:39:09 -0300

    correção desafio4

--- desafio4.sql
+++ desafio4.sql
@@ -1,10 +1,12 @@
-SELECT JOB_ID as Cargo, round(avg(SALARY),2) as "Média salarial",
+SELECT hr.jobs.JOB_TITLE as Cargo, round(avg(hr.employees.SALARY),2) as "Média salarial",
 CASE
-    WHEN avg(SALARY) >= 2000 and avg(SALARY) <= 5800 THEN "Júnior"
-    WHEN avg(SALARY) >= 5801 and avg(SALARY) <= 7500 THEN "Pleno"
-    WHEN avg(SALARY) >= 7501 and avg(SALARY) <= 10500 THEN "Sênior"
-    WHEN avg(SALARY) > 10500 THEN "CEO"
+    WHEN avg(hr.employees.SALARY) >= 2000 and avg(hr.employees.SALARY) <= 5800 THEN "Júnior"
+    WHEN avg(hr.employees.SALARY) >= 5801 and avg(hr.employees.SALARY) <= 7500 THEN "Pleno"
+    WHEN avg(hr.employees.SALARY) >= 7501 and avg(hr.employees.SALARY) <= 10500 THEN "Sênior"
+    WHEN avg(hr.employees.SALARY) > 10500 THEN "CEO"
 end as "Senioridade"
 FROM hr.employees
-group by JOB_ID
-order by avg(SALARY), JOB_ID;
+INNER JOIN hr.jobs
+on hr.employees.JOB_ID = hr.jobs.JOB_ID
+group by hr.employees.JOB_ID
+order by avg(hr.employees.SALARY), hr.employees.JOB_ID;
