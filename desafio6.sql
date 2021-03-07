616efb87e8d04abd5975b711b5bc7e82c9b5d5e3
brendalima <brenda.c.lima@poli.ufrj.br>
2021-03-07 18:48:49 -0300

    attempt n2 to fix challenge n6

--- desafio6.sql
+++ desafio6.sql
@@ -1,22 +1,13 @@
-SELECT CONCAT(FIRST_NAME, ' ', LAST_NAME) AS `Nome completo`,
-(SELECT JOB_TITLE
-FROM hr.jobs
-WHERE hr.jobs.JOB_ID = hr.employees.JOB_ID) AS Cargo,
-CASE
-WHEN (
-SELECT START_DATE
-FROM hr.job_history
-WHERE hr.job_history.EMPLOYEE_ID = hr.employees.EMPLOYEE_ID) THEN hr.job_history.START_DATE
-ELSE HIRE_DATE
-END AS `Data de início do cargo`,
-(SELECT DEPARTMENT_NAME
-FROM hr.departments
-WHERE hr.departments.DEPARTMENT_ID = hr.employees.DEPARTMENT_ID) AS Departamento
-FROM hr.employees
-ORDER BY `Nome completo` DESC, Cargo;
+SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
+j.JOB_TITLE AS Cargo,
+j_h.START_DATE AS `Data de início do cargo`,
+d.DEPARTMENT_NAME AS Departamento
+FROM hr.job_history AS j_h
+INNER JOIN hr.employees AS e
+ON j_h.EMPLOYEE_ID = e.EMPLOYEE_ID
+INNER JOIN hr.jobs AS j
+ON j_h.JOB_ID = j.JOB_ID
+INNER JOIN hr.departments AS d
+ON j_h.DEPARTMENT_ID = d.DEPARTMENT_ID;
 
-SELECT * FROM hr.employees
-WHERE FIRST_NAME = 'Jennifer';
-SELECT * FROM hr.job_history;
-SELECT * FROM hr.departments;
-SELECT * FROM hr.jobs;
+/* Referência @vanessaberbidi */
