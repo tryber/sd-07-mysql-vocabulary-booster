7effbc2a3cae09aeaf6c081d86012b456ab4ba53
moacyrrln <moacyrrln@gmail.com>
2021-03-07 12:56:21 -0300

    desafio6

---
+++ desafio6.sql
@@ -0,0 +1,10 @@
+SELECT CONCAT(hr.employees.FIRST_NAME, " ", hr.employees.LAST_NAME) AS "Nome completo",
+hr.jobs.JOB_TITLE as "Cargo",
+hr.employees.HIRE_DATE as "Data de início do cargo",
+hr.departments.DEPARTMENT_NAME
+FROM hr.employees
+INNER JOIN hr.jobs
+ON hr.employees.JOB_ID = hr.jobs.JOB_ID
+INNER JOIN hr.departments
+ON hr.employees.DEPARTMENT_ID = hr.departments.DEPARTMENT_ID
+order by CONCAT(hr.employees.FIRST_NAME, " ", hr.employees.LAST_NAME) desc, hr.jobs.JOB_TITLE
