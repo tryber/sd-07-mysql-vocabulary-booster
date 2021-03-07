fdbde5ae4e9c2270d1bfe28ce277a18477f0401c
laurods <itrafor@gmail.com>
2021-03-07 08:56:59 -0300

    Correçoes desafio 8b

--- desafio8.sql
+++ desafio8.sql
@@ -5,4 +5,5 @@ o.OrderDate AS "Data do pedido"
 FROM w3schools.customers AS c
 INNER JOIN w3schools.orders AS o ON c.CustomerID = o.CustomerID
 INNER JOIN w3schools.shippers AS s ON o.CustomerID = s.ShipperID
+WHERE s.ShipperName IN ("Speedy Express", "United Package")
 ORDER BY c.ContactName ASC, s.ShipperName ASC, o.OrderDate ASC;
