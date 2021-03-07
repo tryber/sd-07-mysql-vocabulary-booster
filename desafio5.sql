ac337cd94a3c5b779f15e75bdafb3811e2f6cfff
Marcio <marcio@unknown6432a883dae2.localdomain>
2021-03-07 16:16:02 -0300

    ate o 6

---
+++ desafio5.sql
@@ -0,0 +1,11 @@
+select
+j.JOB_TITLE as 'Cargo',
+(max(j.MAX_SALARY) - min(j.MIN_SALARY)) as 'Variação Salarial',
+round(min(e.SALARY), 2) as 'Média mínima mensal',
+round(max(e.SALARY), 2) as 'Média máxima mensal'
+from hr.employees e
+left join hr.jobs j on e.JOB_ID = j.JOB_ID
+group by 1
+order by
+2 ASC,
+1 ASC;
---
+++ desafio6.sql
@@ -0,0 +1,10 @@
+select
+concat(e.FIRST_NAME, ' ', e.LAST_NAME) as 'Nome completo',
+j.JOB_TITLE as 'Cargo',
+HIRE_DATE as 'Data de início do cargo',
+d.DEPARTMENT_NAME as 'Departamento'
+from hr.employees e
+left join
+hr.departments d on d.DEPARTMENT_ID = e.DEPARTMENT_ID
+left join
+hr.jobs j on j.JOB_ID = e.JOB_ID;
