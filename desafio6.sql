c4cfe8b2d86e1741bde55426e6daf896f44e098c
moacyrrln <moacyrrln@gmail.com>
2021-03-07 17:42:40 -0300

    aula com o mestre Emerson

--- desafio6.sql
+++ desafio6.sql
@@ -1,10 +1,12 @@
 SELECT CONCAT(hr.employees.FIRST_NAME, " ", hr.employees.LAST_NAME) AS "Nome completo",
 hr.jobs.JOB_TITLE as "Cargo",
-hr.employees.HIRE_DATE as "Data de início do cargo",
+hr.job_history.START_DATE as "Data de início do cargo",
 hr.departments.DEPARTMENT_NAME as "Departamento"
 FROM hr.employees
-INNER JOIN hr.jobs
-ON hr.employees.JOB_ID = hr.jobs.JOB_ID
+INNER JOIN hr.job_history
+ON hr.employees.EMPLOYEE_ID = hr.job_history.EMPLOYEE_ID
 INNER JOIN hr.departments
-ON hr.employees.DEPARTMENT_ID = hr.departments.DEPARTMENT_ID
-order by CONCAT(hr.employees.FIRST_NAME, " ", hr.employees.LAST_NAME) desc, hr.jobs.JOB_TITLE;
+ON hr.job_history.DEPARTMENT_ID = hr.departments.DEPARTMENT_ID
+inner join hr.jobs
+on hr.jobs.JOB_ID = hr.job_history.JOB_ID
+order by 1 desc, 2;
--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,9 @@
-SELECT UPPER(CONCAT(FIRST_NAME, " ", LAST_NAME)) AS "Nome completo",
-HIRE_DATE as "Data de início",
-SALARY as "Salário"
+SELECT
+UCASE(CONCAT(hr.employees.FIRST_NAME, ' ', hr.employees.LAST_NAME)) AS 'Nome completo',
+hr.job_history.START_DATE AS 'Data de início',
+hr.employees.SALARY AS 'Salário'
 FROM hr.employees
-
-order by CONCAT(FIRST_NAME, " ", LAST_NAME), HIRE_DATE;
+INNER JOIN hr.job_history
+ON hr.job_history.EMPLOYEE_ID = hr.employees.EMPLOYEE_ID
+WHERE MONTH(hr.job_history.START_DATE) BETWEEN 1 AND 3
+ORDER BY 1, 2; 
