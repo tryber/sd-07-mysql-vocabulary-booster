67844bb37c3e8f23cab5e10f4df51844db3839a0
laurods <itrafor@gmail.com>
2021-03-07 08:01:33 -0300

    Correçoes desafios 7 e 8

--- desafio8.sql
+++ desafio8.sql
@@ -2,7 +2,8 @@ SELECT
 c.ContactName AS "Nome de contato",
 s.ShipperName AS "Empresa que fez o envio",
 o.OrderDate AS "Data do pedido"
-FROM w3schools.orders AS o
-INNER JOIN w3schools.customers AS c ON c.CustomerID = o.CustomerID
-INNER JOIN w3schools.shippers AS s ON s.ShipperID = o.ShipperID
-ORDER BY c.ContactName ASC, o.OrderDate ASC;
+FROM w3schools.customers AS c
+INNER JOIN w3schools.orders AS o ON c.CustomerID = o.CustomerID
+INNER JOIN w3schools.shippers AS s ON o.CustomerID = s.ShipperID
+ORDER BY c.ContactName ASC, s.ShipperName ASC, o.OrderDate ASC;
+
