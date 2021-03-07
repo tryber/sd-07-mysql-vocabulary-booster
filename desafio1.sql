302c8670066418260ef8b32364cb5d6f4a260eba
moacyrrln <moacyrrln@gmail.com>
2021-03-07 10:49:44 -0300

    1049

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,5 @@
-SELECT hr.countries.COUNTRY_NAME AS País, hr.countries.REGION_ID, hr.regions.REGION_NAME, if(hr.regions.REGION_NAME = "Europe", "incluído", "não incluído") AS "Status Inclusão"
+SELECT hr.countries.COUNTRY_NAME AS País, if(hr.regions.REGION_NAME = "Europe", "incluído", "não incluído") AS "Status Inclusão"
 FROM hr.countries
 INNER JOIN hr.regions
-ON hr.countries.REGION_ID = hr.regions.REGION_ID;
+ON hr.countries.REGION_ID = hr.regions.REGION_ID
+order by hr.countries.COUNTRY_NAME;
