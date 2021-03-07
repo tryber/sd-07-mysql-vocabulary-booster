95887bc289c8c6576b47a05f20567b4ae0dc5663
moacyrrln <moacyrrln@gmail.com>
2021-03-07 15:53:38 -0300

    desafio8

---
+++ desafio8.sql
@@ -0,0 +1,10 @@
+SELECT w3schools.customers.CustomerName as "Nome de contato",
+w3schools.shippers.ShipperName as "Empresa que fez o envio",
+w3schools.orders.OrderDate as "Data do pedido"
+FROM w3schools.orders
+inner join w3schools.customers
+on w3schools.orders.CustomerID = w3schools.customers.CustomerID
+inner join w3schools.shippers
+on w3schools.orders.ShipperID = w3schools.shippers.ShipperID
+where w3schools.shippers.ShipperName in ("Speedy Express", "United Package")
+order by w3schools.customers.CustomerName, w3schools.shippers.ShipperName, w3schools.orders.OrderDate;
