dc2f20b1ec4c96480ff32a75bcec0a639ee5bb11
wfelipefarina <wfelipefarina@gmail.com>
2021-03-07 18:08:50 -0300

    requisitos 1 ao 10, codeclimate

--- desafio10.sql
+++ desafio10.sql
@@ -1,7 +1,7 @@
 SELECT
- CUSTOMERS.ContactName AS 'Nome',
- CUSTOMERS.Country AS 'País',
- COUNT(CUSTOMERS2.Country) - 1 AS 'Número de compatriotas'
+  CUSTOMERS.ContactName AS 'Nome',
+  CUSTOMERS.Country AS 'País',
+  COUNT(CUSTOMERS2.Country) - 1 AS 'Número de compatriotas'
 FROM w3schools.customers  AS CUSTOMERS,
 w3schools.customers AS CUSTOMERS2
 WHERE CUSTOMERS.Country = CUSTOMERS2.Country
