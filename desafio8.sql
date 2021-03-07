0ea578b6a30f14c59c7eb223e27657bb3ce484cd
brendalima <brenda.c.lima@poli.ufrj.br>
2021-03-07 19:07:32 -0300

    finishing query for challenge n8

--- desafio8.sql
+++ desafio8.sql
@@ -6,7 +6,9 @@ WHERE w3schools.customers.CustomerID = w3schools.orders.CustomerID
 SELECT ShipperName
 FROM w3schools.shippers
 WHERE w3schools.shippers.ShipperID = w3schools.orders.ShipperID
-) AS `Empresa que fez o envio`
+) AS `Empresa que fez o envio`,
+OrderDate AS `Data do pedido`
 FROM w3schools.orders
 WHERE w3schools.orders.ShipperID = 1
-OR w3schools.orders.ShipperID = 2;
+OR w3schools.orders.ShipperID = 2
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
