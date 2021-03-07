697378a13c0dfbd54efc6d11e26a249e2815798c
moacyrrln <moacyrrln@gmail.com>
2021-03-07 12:57:49 -0300

    desafio6 cc

--- desafio6.sql
+++ desafio6.sql
@@ -7,4 +7,4 @@ INNER JOIN hr.jobs
 ON hr.employees.JOB_ID = hr.jobs.JOB_ID
 INNER JOIN hr.departments
 ON hr.employees.DEPARTMENT_ID = hr.departments.DEPARTMENT_ID
-order by CONCAT(hr.employees.FIRST_NAME, " ", hr.employees.LAST_NAME) desc, hr.jobs.JOB_TITLE
+order by CONCAT(hr.employees.FIRST_NAME, " ", hr.employees.LAST_NAME) desc, hr.jobs.JOB_TITLE;
