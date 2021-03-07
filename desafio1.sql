e75a45cba3181460165b6c470b0c8d21667b8032
Marcio <marcio@unknown6432a883dae2.localdomain>
2021-03-07 20:35:16 -0300

    refatorando 1 para teste

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,6 @@
-SELECT
-COUNTRY_NAME as País,
-if(REGION_ID = 1, 'incluído', 'não incluído') as 'Status Inclusão'
-from hr.countries;
+select
+c.COUNTRY_NAME as 'País',
+if(r.REGION_NAME = 'Europe', 'incluído', 'não incluído') as 'Status Inclusão'
+from hr.countries c
+left join hr.regions r on r.REGION_ID = c.REGION_ID
+order by c.COUNTRY_NAME;
