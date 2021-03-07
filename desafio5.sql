016ac667aa99f59e3cef2a89866c6653a05e56f3
laurods <itrafor@gmail.com>
2021-03-07 08:24:04 -0300

    Correçoes desafio5

--- desafio5.sql
+++ desafio5.sql
@@ -1,9 +1,10 @@
 SELECT 
-j.JOB_TITLE AS "Cargo",
-SUM((j.MAX_SALARY - j.MIN_SALARY)/12) AS "Variação Salarial",
-ROUND(MIN(e.SALARY),2) AS "Média mínima mensal",
-ROUND(MAX(e.SALARY),2) AS "Média máxima mensal" 
-FROM hr.employees AS e
-INNER JOIN hr.jobs AS j ON j.JOB_ID = e.JOB_ID
-GROUP BY j.JOB_TITLE
-ORDER BY ROUND(AVG(e.SALARY),2), j.JOB_TITLE ASC;
+JOB_TITLE AS "Cargo",
+MAX_SALARY - MIN_SALARY AS "Variação Salarial",
+ROUND(MIN(MIN_SALARY / 12),2) AS "Média mínima mensal",
+ROUND(MAX(MAX_SALARY / 12),2) AS "Média máxima mensal" 
+FROM hr.jobs
+GROUP BY JOB_TITLE, MAX_SALARY - MIN_SALARY
+ORDER BY MAX_SALARY - MIN_SALARY, JOB_TITLE;
+
+
