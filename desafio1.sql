04d123e1445ca1a244bebb7a782085a438338e9e
laurods <itrafor@gmail.com>
2021-03-07 07:28:43 -0300

    Desafio1 corrigido

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,6 @@
-SELECT COUNTRY_NAME AS "País", IF(REGION_ID=1, "incluído", "não incluído") AS  "Status Inclusão" FROM hr.countries;
+SELECT
+c.COUNTRY_NAME AS "País", 
+IF(REGION_NAME='Europe', "incluído", "não incluído") AS  "Status Inclusão" 
+FROM hr.countries AS c
+INNER JOIN hr.regions AS r ON r.REGION_ID = c.REGION_ID
+ORDER BY c.COUNTRY_NAME ASC;
