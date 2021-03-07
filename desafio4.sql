86ddebed1094251743bd0dab7b80cba8a8fb7317
moacyrrln <moacyrrln@gmail.com>
2021-03-07 12:33:58 -0300

    desafio5 CORREÇÃO

--- desafio4.sql
+++ desafio4.sql
@@ -1,4 +1,4 @@
-SELECT JOB_ID as Cargo, round(avg(SALARY),2) as "Média salarial",
+SELECT JOB_TITLE as Cargo, round(avg(SALARY),2) as "Média salarial",
 CASE
     WHEN avg(SALARY) >= 2000 and avg(SALARY) <= 5800 THEN "Júnior"
     WHEN avg(SALARY) >= 5801 and avg(SALARY) <= 7500 THEN "Pleno"
@@ -6,5 +6,5 @@ CASE
     WHEN avg(SALARY) > 10500 THEN "CEO"
 end as "Senioridade"
 FROM hr.employees
-group by JOB_ID
-order by avg(SALARY), JOB_ID;
+group by JOB_TITLE
+order by avg(SALARY), JOB_TITLE;
