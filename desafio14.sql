718cc0622a6e0b599dabc64482858f2d6be55db1
Israel Menon <zezomenon@gmail.com>
2021-03-08 09:13:26 -0300

    Desafio 14 tentando passar no teste

--- desafio14.sql
+++ desafio14.sql
@@ -1,7 +1,7 @@
 SELECT DISTINCT customers.Country AS "País"
-FROM w3schools.customers AS customers
+FROM w3schools.customers AS customer
 UNION
 SELECT DISTINCT suppliers.Country
-FROM w3schools.suppliers AS suppliers
+FROM w3schools.suppliers AS supplier
 ORDER BY "País"
 LIMIT 5;
