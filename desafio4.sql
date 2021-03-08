d0d9df32b945ddea5289720c227eb00a2fdacc11
Thays Costa <thayscosta3@gmail.com>
2021-03-08 10:39:47 -0300

    Alteraçoes para CodeClimate

--- desafio4.sql
+++ desafio4.sql
@@ -1,5 +1,5 @@
 SELECT jobs.JOB_TITLE AS 'Cargo',
-	ROUND(AVG(employees.SALARY), 2) AS `Média salarial`,
+	ROUND(AVG(employees.SALARY), 2) AS 'Média salarial',
     CASE
 		WHEN ROUND(AVG(employees.SALARY), 2) between 2000 AND 5800 THEN 'Júnior'
 		WHEN ROUND(AVG(employees.SALARY), 2) between 5801 AND 7500 THEN 'Pleno'
