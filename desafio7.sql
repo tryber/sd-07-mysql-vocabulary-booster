9fad8a658175c375f561013e0733118db9eb5d67
Bruna Fonsêca <1998bfonseca@gmail.com>
2021-03-08 09:21:02 -0300

    será q vai?

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,8 @@
-SELECT UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
+SELECT
+UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
 h.START_DATE AS 'Data de início',
 e.SALARY AS 'Salário'
 FROM hr.employees AS e
 INNER JOIN hr.job_history AS h ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
-WHERE MONTH(h.START_DATE) IN (1, 2, 3)
+WHERE MONTH(h.START_DATE) IN (1,2,3)
 ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME), h.START_DATE;
