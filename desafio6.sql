e9599120030ca566bf1a3dc573dab598513c9254
Marcio <marcio@unknown6432a883dae2.localdomain>
2021-03-08 17:37:01 -0300

    refat 6

--- desafio6.sql
+++ desafio6.sql
@@ -2,12 +2,13 @@ select
 concat(e.FIRST_NAME, ' ', e.LAST_NAME) as 'Nome completo',
 j.JOB_TITLE as 'Cargo',
 jh.START_DATE as 'Data de início do cargo',
-d.DEPARTMENT_NAME
+d.DEPARTMENT_NAME as 'Departamento'
 from hr.employees e
-left join
-hr.jobs j on j.JOB_ID = e.JOB_ID
 inner join hr.job_history jh on jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+left join hr.jobs j on j.JOB_ID = jh.JOB_ID
 inner join hr.departments d on d.DEPARTMENT_ID = jh.DEPARTMENT_ID 
 order by
 1 DESC,
 2 ASC;
+
+Error Code: 1064. You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'jh' at line 1
