07fcf2c210bbc8181dc57a0d8bc28471468178b4
Thays Costa <thayscosta3@gmail.com>
2021-03-08 11:24:40 -0300

    Correçoes

--- desafio6.sql
+++ desafio6.sql
@@ -1,10 +1,10 @@
--SELECT CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME) AS `Nome completo`,
+-SELECT CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME) AS 'Nome completo',
 jobs.JOB_TITLE AS 'Cargo',
-history.START_DATE AS `Data de início do cargo`,
+history.START_DATE AS 'Data de início do cargo',
 departments.DEPARTMENT_NAME AS 'Departamento'
 FROM hr.job_history AS history
 INNER JOIN hr.employees AS employees
-ON employees.EMPLOYEE_ID = history.EMPLOYEE_ID
+ON history.EMPLOYEE_ID = employees.EMPLOYEE_ID 
 JOIN hr.jobs AS jobs
 ON jobs.JOB_ID = history.JOB_ID
 JOIN hr.departments AS departments
