a09a7829394030b8b9e62ad574cc08388fad2fc4
Marcio <marcio@unknown6432a883dae2.localdomain>
2021-03-08 17:13:36 -0300

    refat 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,13 +1,14 @@
 select
 concat(e.FIRST_NAME, ' ', e.LAST_NAME) as 'Nome completo',
 j.JOB_TITLE as 'Cargo',
-HIRE_DATE as 'Data de início do cargo',
+jh.START_DATE as 'Data de início do cargo',
 d.DEPARTMENT_NAME as 'Departamento'
 from hr.employees e
 left join
 hr.departments d on d.DEPARTMENT_ID = e.DEPARTMENT_ID
 left join
 hr.jobs j on j.JOB_ID = e.JOB_ID
+left join hr.job_history jh on jh.EMPLOYEE_ID = e.EMPLOYEE_ID
 order by
 1 DESC,
 2 ASC;
