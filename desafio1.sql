c6e39ad0bdaea70b96e4abf2d4a95a1f8c6a3dc1
Thays Costa <thayscosta3@gmail.com>
2021-03-08 11:08:37 -0300

    Tirando aspas

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,1 @@
-SELECT COUNTRY_NAME AS 'País', IF(REGION_ID = 1, 'incluído', 'não incluído') AS 'Status de Inclusão' FROM hr.countries ORDER BY COUNTRY_NAME;
+SELECT COUNTRY_NAME AS País, IF(REGION_ID = 1, 'incluído', 'não incluído') AS 'Status de Inclusão' FROM hr.countries ORDER BY COUNTRY_NAME;
