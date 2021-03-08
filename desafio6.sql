a0845e51035d74c44a995da2b41a35e290677086
Thays Costa <thayscosta3@gmail.com>
2021-03-08 11:49:44 -0300

    Req. 6 substituindo a palavra history por hist

--- desafio6.sql
+++ desafio6.sql
@@ -1,12 +1,12 @@
 SELECT CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME) AS 'Nome completo',
 jobs.JOB_TITLE AS 'Cargo',
-history.START_DATE AS 'Data de início do cargo',
+hist.START_DATE AS 'Data de início do cargo',
 departments.DEPARTMENT_NAME AS 'Departamento'
-FROM hr.job_history AS history
+FROM hr.job_history AS hist
 INNER JOIN hr.employees AS employees
-ON history.EMPLOYEE_ID = employees.EMPLOYEE_ID 
-JOIN hr.jobs AS jobs
-ON jobs.JOB_ID = history.JOB_ID
-JOIN hr.departments AS departments
-ON departments.DEPARTMENT_ID = history.DEPARTMENT_ID
-ORDER BY `Nome completo` DESC, 'Cargo';
+ON hist.EMPLOYEE_ID = employees.EMPLOYEE_ID 
+INNER JOIN hr.jobs AS jobs
+ON jobs.JOB_ID = hist.JOB_ID
+INNER JOIN hr.departments AS departments
+ON departments.DEPARTMENT_ID = hist.DEPARTMENT_ID
+ORDER BY `Nome completo` DESC, `Cargo`;
