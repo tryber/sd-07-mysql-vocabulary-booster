858bef669d464057fe8ceb1d1dd2ba251804725f
Fabio Camargos <fabiocmmcm@gmail.com>
2021-03-09 19:03:24 -0300

    Requisitos 11-14

---
+++ desafio11.sql
@@ -0,0 +1,9 @@
+
+SELECT cus1.ContactName AS Nome,
+cus1.Country AS País,
+COUNT(*) AS `Número de compatriotas`
+FROM customers AS cus1, customers AS cus2
+WHERE cus1.Country = cus2.Country
+AND cus1.CustomerID <> cus2.CustomerID
+GROUP BY cus1.CustomerID
+ORDER BY `Nome`;
