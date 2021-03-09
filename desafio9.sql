b8e14611b86bd1827f3995e8285e4b169643064b
Marcio <marcio@unknown6432a883dae2.localdomain>
2021-03-09 07:54:29 -0300

    refat 9

--- desafio9.sql
+++ desafio9.sql
@@ -2,6 +2,6 @@ select
 concat(e.FirstName, ' ', e.LastName) as 'Nome completo',
 count(o.EmployeeID) as 'Total de pedidos'
 from w3schools.employees e
-left join w3schools.orders o on o.EmployeeID = e.EmployeeID
+inner join w3schools.orders o on o.EmployeeID = e.EmployeeID
 group by 1
 order by 2 ASC;
