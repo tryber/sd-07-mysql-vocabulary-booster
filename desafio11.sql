5eaf44ebfc7e10f81e853bf7b1a05ca3f76eb61b
Marcio <marcio@unknown6432a883dae2.localdomain>
2021-03-09 08:01:14 -0300

    refat 11

--- desafio11.sql
+++ desafio11.sql
@@ -3,4 +3,8 @@ c.ContactName as 'Nome',
 c.Country as 'País',
 (select count(Country) from w3schools.customers where Country = c.Country) - 1 as 'Número de compatriotas'
 from w3schools.customers c
-order by 1;
+where
+((select count(Country) from w3schools.customers where Country = c.Country) - 1) > 0
+order by 1 ASC;
+
+
