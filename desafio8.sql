da6d0d860a4d2de888b52e8576dd45f667662f0c
Marcio <marcio@unknown6432a883dae2.localdomain>
2021-03-09 07:50:07 -0300

    refat 7

--- desafio8.sql
+++ desafio8.sql
@@ -3,8 +3,9 @@ c.ContactName as 'Nome de contato',
 s.ShipperName as 'Empresa que fez o envio',
 o.OrderDate as 'Data do pedido'
 from w3schools.orders o
-left join w3schools.customers c on c.CustomerID = o.CustomerID
-left join w3schools.shippers s on s.ShipperID = o.ShipperID
+inner join w3schools.customers c on c.CustomerID = o.CustomerID
+inner join w3schools.shippers s on s.ShipperID = o.ShipperID
+where s.ShipperName in ('Speedy Express', 'United Package')
 order by
 1 ASC,
 2 ASC,
