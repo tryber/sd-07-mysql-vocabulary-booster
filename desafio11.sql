3c5852e66e84a398bd7932411b64ce375e416ee6
spawnred <bruno.spawnhell@gmail.com>
2021-03-10 06:44:59 -0300

    correção item 11

--- desafio11.sql
+++ desafio11.sql
@@ -5,5 +5,5 @@ COUNT(t2.CustomerID) AS "Número de compatriotas"
 FROM w3schools.customers AS t1, w3schools.customers AS t2
 WHERE t1.Country = t2.Country
 AND t1.CustomerID <> t2.CustomerID
-GROUP by 1
+GROUP by t1.CustomerID
 ORDER By 1 asc;
