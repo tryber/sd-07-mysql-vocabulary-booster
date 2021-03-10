4ee7b1ca5ce6ee3b8ebbfffe579c69b178a7f49c
Marcio <marcio@unknown6432a883dae2.localdomain>
2021-03-10 15:01:05 -0300

    req 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,8 +1,8 @@
 select
 j.JOB_TITLE as 'Cargo',
 (max(j.MAX_SALARY) - min(j.MIN_SALARY)) as 'Variação Salarial',
-round((min(e.SALARY)/ 12), 2) as 'Média mínima mensal',
-round((max(e.SALARY)/ 12), 2) as 'Média máxima mensal'
+round((min(j.MIN_SALARY)/ 12), 2) as 'Média mínima mensal',
+round((max(j.MAX_SALARY)/ 12), 2) as 'Média máxima mensal'
 from hr.employees e
 left join hr.jobs j on e.JOB_ID = j.JOB_ID
 group by 1
