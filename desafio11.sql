343815299914e00d14f1416f3fbd8980d6d85421
spawnred <bruno.spawnhell@gmail.com>
2021-03-10 06:38:01 -0300

    correção item 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,9 +1,9 @@
 SELECT
 t1.ContactName AS "Nome",
 t1.Country AS "País",
-(select COUNT(t2.CustomerID) -1 
-from w3schools.customers AS t2
-where t1.Country = t2.Country
-group by t2.Country) AS "Número de compatriotas"
-FROM w3schools.customers AS t1
-order By 1 asc;
+COUNT(t2.CustomerID) AS "Número de compatriotas"
+FROM w3schools.customers AS t1, w3schools.customers AS t2
+WHERE t1.Country = t2.Country
+AND t1.CustomerID <> t2.CustomerID
+GROUP by 1
+ORDER By 1 asc;
