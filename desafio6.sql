55ac2583c651df37118100faa8de5cb96e30d546
IsabelaCardoso <pcardoso.isabela@gmail.com>
2021-03-11 19:22:36 -0300

    code climate

--- desafio6.sql
+++ desafio6.sql
@@ -6,4 +6,3 @@ INNER JOIN hr.job_history AS h ON h.EMPLOYEE_ID = e.EMPLOYEE_ID
 INNER JOIN hr.jobs AS j ON j.JOB_ID = h.JOB_ID
 INNER JOIN hr.departments AS d ON h.DEPARTMENT_ID = d.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC, 'Cargo';
-
