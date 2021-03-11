8dd5201ac0c5d397ff8cf3e3acdfe2f13de6e911
Marcio <marcio@unknown6432a883dae2.localdomain>
2021-03-11 18:41:18 -0300

    refat 16

--- desafio16.sql
+++ desafio16.sql
@@ -1,6 +1,5 @@
 use hr;
 DELIMITER $$
-
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(300))
 RETURNS INT READS SQL DATA
 BEGIN
@@ -9,6 +8,7 @@ select
 count(*)
 from hr.employees e
 inner join hr.job_history jh on jh.EMPLOYEE_ID = e.EMPLOYEE_ID
-where e.EMAIL like concat('%', email, '%') into jobs_QQ;
+where e.EMAIL like concat('%', email, '%') into jobs_QTT;
+RETURN job_QTT;
 END $$
 DELIMITER ;
