ceef998dee8af17444625f62cbb184381a2e792a
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2021-03-12 16:06:15 -0300

    req 9 correct

--- desafio9.sql
+++ desafio9.sql
@@ -3,4 +3,5 @@ COUNT(*) AS "Total de pedidos"
 FROM w3schools.employees AS e
 INNER JOIN w3schools.orders AS o
 ON e.EmployeeID = o.EmployeeID
-GROUP BY CONCAT(e.FirstName, ' ', e.LastName);
+GROUP BY CONCAT(e.FirstName, ' ', e.LastName)
+ORDER BY COUNT(*);
