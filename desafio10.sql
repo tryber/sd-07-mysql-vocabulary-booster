1c9fb23f9865e0ed599c08db9f6b7bbc2222a45b
IsabelaCardoso <pcardoso.isabela@gmail.com>
2021-03-12 10:40:50 -0300

    desafio 18 e correçao 6 e 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,7 +1,7 @@
 SELECT p.ProductName AS 'Produto',
 MIN(d.Quantity) AS 'Mínima',
 MAX(d.Quantity) AS 'Máxima',
-AVG(d.Quantity) AS 'Média'
+ROUND(AVG(d.Quantity),2) AS 'Média'
 FROM w3schools.products AS p
 INNER JOIN w3schools.order_details AS d ON p.ProductID = d.ProductID
 GROUP BY d.ProductID
