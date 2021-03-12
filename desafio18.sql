cf6dccdc02629e7002a3068d62ef81c53932a27d
Marcio <marcio@unknown6432a883dae2.localdomain>
2021-03-12 18:05:21 -0300

    refat 18

--- desafio18.sql
+++ desafio18.sql
@@ -1,7 +1,7 @@
 select
 concat(e.FIRST_NAME, ' ', e.LAST_NAME) as 'Nome completo',
-concat(day(jh.START_DATE), '/', month(jh.START_DATE), '/', YEAR(jh.START_DATE)) as 'Data de início',
-concat(day(jh.END_DATE), '/', month(jh.END_DATE), '/', YEAR(jh.END_DATE)) as 'Data de rescisão',
+DATE_FORMAT(jh.START_DATE, '%d/%m/%Y') as 'Data de início', 
+DATE_FORMAT(jh.END_DATE, '%d/%m/%Y') as 'Data de rescisão', 
 (select round((DATEDIFF(jh.End_DATE, jh.START_DATE) /365),2)) as 'Anos trabalhados'
 from hr.job_history jh
 inner join hr.employees e on e.EMPLOYEE_ID = jh.EMPLOYEE_ID
