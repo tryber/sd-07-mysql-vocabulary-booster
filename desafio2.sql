930382e2fd7e1aa99dc808bb0aa593a1efc81a15
Erling <erlingjunior@gmail.com>
2021-03-12 12:28:17 -0300

    ta dificil arrumar os climate

--- desafio2.sql
+++ desafio2.sql
@@ -1,8 +1,8 @@
 SELECT 
-	job_title AS "Cargo",
-	max_salary AS "Nível",
+job_title AS "Cargo",
+max_salary AS "Nível",
 CASE 
-		WHEN max_salary <= 10000  THEN "Baixo"
+	WHEN max_salary <= 10000  THEN "Baixo"
     WHEN max_salary BETWEEN 10001 AND 20000 THEN "Medio"
     WHEN max_salary BETWEEN 20001 AND 30000 THEN "Alto"
     WHEN max_salary > 30000 THEN "Altíssimo"
