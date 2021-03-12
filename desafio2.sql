14500aedd6878fd85888c8f44b529ac5035bc38f
Erling <erlingjunior@gmail.com>
2021-03-12 01:02:59 -0300

    fizing

--- desafio2.sql
+++ desafio2.sql
@@ -1,7 +1,7 @@
 SELECT 
 	job_title AS "Cargo",
 	max_salary AS "Nível",
-	CASE 
+CASE 
 		WHEN max_salary <= 10000  THEN "Baixo"
     WHEN max_salary BETWEEN 10001 AND 20000 THEN "Medio"
     WHEN max_salary BETWEEN 20001 AND 30000 THEN "Alto"
