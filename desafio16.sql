836ecb6631dede3a5561b705bb0e5d83f105839b
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2021-03-13 19:59:15 -0300

    Req16

---
+++ desafio16.sql
@@ -0,0 +1,16 @@
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email TEXT)
+RETURNS INT READS SQL DATA
+BEGIN
+	DECLARE email_qnt INT;
+    SELECT COUNT(*) FROM hr.employees AS emp
+    INNER JOIN hr.job_history AS his
+    ON his.EMPLOYEE_ID = emp.EMPLOYEE_ID
+    WHERE email = emp.EMAIL INTO email_qnt;
+    RETURN email_qnt;
+END $$
+
+DELIMITER ;
+
+SELECT buscar_quantidade_de_empregos_por_funcionario("NKOCHHAR");
