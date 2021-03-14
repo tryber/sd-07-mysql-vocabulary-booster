e3061042e3fec21229daa177f26e98f652523694
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2021-03-13 21:17:07 -0300

    req 18, 6 correction

--- desafio6.sql
+++ desafio6.sql
@@ -3,10 +3,10 @@ j.JOB_TITLE AS "Cargo",
 h.START_DATE AS "Data de início do cargo",
 d.DEPARTMENT_NAME AS "Departamento" 
 FROM hr.employees AS e
-INNER JOIN hr.departments AS d
-ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
 INNER JOIN hr.job_history AS h 
 ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
 INNER JOIN hr.jobs AS j
 ON j.JOB_ID = h.JOB_ID
-ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) DESC, j.JOB_TITLE;
+INNER JOIN hr.departments AS d
+ON h.DEPARTMENT_ID = d.DEPARTMENT_ID
+ORDER BY 1 DESC, 2;
