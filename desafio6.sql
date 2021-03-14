ab66b5ff23bbbb08a65c40201fe3e6a572304acb
Erling <erlingjunior@gmail.com>
2021-03-14 16:37:55 -0300

    um dia eu acabo

--- desafio6.sql
+++ desafio6.sql
@@ -7,4 +7,4 @@ FROM hr.employees AS employee
 INNER JOIN hr.jobs AS job ON employee.job_id = job.job_id
 INNER JOIN hr.departments AS department ON employee.department_id = department.department_id
 INNER JOIN hr.job_history AS histo ON employee.employee_id = histo.employee_id
-ORDER BY CONCAT(employee.first_name, ' ', employee.last_name) DESC, job.job_title;
+ORDER BY CONCAT(employee.first_name, ' ', employee.last_name) DESC, job_title;
