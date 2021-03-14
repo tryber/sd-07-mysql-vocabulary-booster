ae4f0b6a4efb495a5136fd3d4d00b1e7fe7e49ef
Erling <erlingjunior@gmail.com>
2021-03-14 16:55:08 -0300

    esqueci de pular a linha

--- desafio10.sql
+++ desafio10.sql
@@ -7,15 +7,3 @@ INNER JOIN w3schools.order_details AS details ON product.ProductID = details.Pro
 GROUP BY product.ProductID
 HAVING Média > 20.00
 ORDER BY Média, Produto;
-
-
-SELECT pro.ProductName AS "Produto",
-MIN(ord.Quantity) AS "Mínimo",
-MAX(ord.Quantity) AS "Máxima",
-ROUND(AVG(ord.Quantity), 2) AS "Média"
-FROM w3schools.products AS pro
-INNER JOIN w3schools.order_details AS ord
-ON ord.ProductID = pro.ProductID
-GROUP BY pro.ProductName
-HAVING Média > 20
-ORDER BY ROUND(AVG(ord.Quantity), 2), pro.ProductName;
