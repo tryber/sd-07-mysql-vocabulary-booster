399d88f52b5f62a02789125e296170e9e7d69d98
Erling <erlingjunior@gmail.com>
2021-03-14 00:34:18 -0300

    ajustando testes

--- desafio2.sql
+++ desafio2.sql
@@ -6,6 +6,6 @@ WHEN max_salary <= 10000  THEN "Baixo"
   WHEN max_salary BETWEEN 10001 AND 20000 THEN "Medio"
   WHEN max_salary BETWEEN 20001 AND 30000 THEN "Alto"
   WHEN max_salary > 30000 THEN "Altíssimo"
-END AS "GranaGranaGrana"
+END AS "Nível"
 FROM hr.jobs
 ORDER BY job_title;
--- desafio4.sql
+++ desafio4.sql
@@ -1,6 +1,6 @@
 SELECT
-jobs.job_title AS Cargo,
-ROUND(AVG(employees.SALARY), 2) AS Média,
+jobs.job_title AS 'Cargo',
+ROUND(AVG(employees.SALARY), 2) AS 'Média salarial',
 CASE 
   WHEN ROUND(AVG(employees.SALARY), 2) > 10500 THEN "CEO"
   WHEN ROUND(AVG(employees.SALARY), 2) > 7501 THEN "Sênior"
