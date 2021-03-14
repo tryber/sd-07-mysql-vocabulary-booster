eaa5c0c38520c08a95f3f3a29ead2360a12e044a
Erling <erlingjunior@gmail.com>
2021-03-14 16:27:10 -0300

    esquci de ordenr o resultado

--- desafio18.sql
+++ desafio18.sql
@@ -5,3 +5,4 @@ DATE_FORMAT(jh.end_date, '%d/%m/%Y') as 'Data de rescisão',
 (SELECT round((DATEDIFF(jh.end_date, jh.start_date) /365),2)) as 'Anos trabalhados'
 from hr.job_history jh
 inner join hr.employees e on e.employee_id = jh.employee_id
+ORDER BY concat(e.first_name, ' ', e.last_name), (SELECT round((DATEDIFF(jh.end_date, jh.start_date) /365),2));
