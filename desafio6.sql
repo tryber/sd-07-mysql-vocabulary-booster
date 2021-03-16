5c142572722daae71482b9eb2c14b4502b9d42fa
vladimirrchagas <vladimirrchagas@gmail.com>
2021-03-16 16:50:34 -0300

    adjustment on challenge 6 and checking challenge 7

--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(he.FIRST_NAME,' ', he.LAST_NAME) AS 'Nome Completo', hj.JOB_TITLE as 'Cargo',
+SELECT CONCAT(he.FIRST_NAME,' ', he.LAST_NAME) AS 'Nome completo', hj.JOB_TITLE as 'Cargo',
 hjh.START_DATE AS 'Data de início do cargo', hd.DEPARTMENT_NAME AS 'Departamento'
 FROM hr.employees he
 INNER JOIN hr.job_history hjh ON he.EMPLOYEE_ID = hjh.EMPLOYEE_ID
