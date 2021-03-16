f02e83b4c2c7d3d4acf59b47d5d4f97e87e0731e
vladimirrchagas <vladimirrchagas@gmail.com>
2021-03-16 19:43:48 -0300

    checking challenge 8

---
+++ desafio8.sql
@@ -0,0 +1,5 @@
+SELECT wcust.CustomerName AS 'Nome de contato', wship.ShipperName AS 'Empresa que fez o envio', word.OrderDate AS 'Data do pedido'
+FROM w3schools.orders word
+INNER JOIN w3schools.customers wcust ON wcust.CustomerID = word.CustomerID
+INNER JOIN w3schools.shippers wship ON wship.ShipperID = word.ShipperID
+ORDER BY 1, 2, 3;
