14f8f1e4f692a90f353a49a8f35a4ba74bd9acdf
vladimirrchagas <vladimirrchagas@gmail.com>
2021-03-16 16:57:55 -0300

    adjustment of challenge 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,5 @@
-SELECT UCASE(CONCAT(he.FIRST_NAME,' ', he.LAST_NAME)) AS 'Nome Completo',
-hjh.START_DATE AS 'Data de início', he.SALARY AS 'Departamento'
+SELECT UCASE(CONCAT(he.FIRST_NAME,' ', he.LAST_NAME)) AS 'Nome completo',
+hjh.START_DATE AS 'Data de início', he.SALARY AS 'Salário'
 FROM hr.employees he
 INNER JOIN hr.job_history hjh ON he.EMPLOYEE_ID = hjh.EMPLOYEE_ID
 WHERE MONTH(hjh.START_DATE) BETWEEN 1 AND 3 
