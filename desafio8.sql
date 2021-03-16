70d7b56bfa49107b8fdb586b92856afa2aa1a738
vladimirrchagas <vladimirrchagas@gmail.com>
2021-03-16 20:10:35 -0300

    adjustment of challenge 8

--- desafio8.sql
+++ desafio8.sql
@@ -2,4 +2,5 @@ SELECT wcust.CustomerName AS 'Nome de contato', wship.ShipperName AS 'Empresa qu
 FROM w3schools.orders word
 INNER JOIN w3schools.customers wcust ON wcust.CustomerID = word.CustomerID
 INNER JOIN w3schools.shippers wship ON wship.ShipperID = word.ShipperID
+WHERE wship.ShipperName = 'Speedy Express' OR wship.ShipperName = 'United Package'
 ORDER BY 1, 2, 3;
