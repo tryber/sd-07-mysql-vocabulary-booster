104305be49742c3e18db999e6ac053014d394679
vladimirrchagas <vladimirrchagas@gmail.com>
2021-03-16 20:38:46 -0300

    adjustmente of challenge 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,4 +1,4 @@
-SELECT wcust.CustomerName AS 'Nome de contato', wship.ShipperName AS 'Empresa que fez o envio', word.OrderDate AS 'Data do pedido'
+SELECT wcust.ContactName AS 'Nome de contato', wship.ShipperName AS 'Empresa que fez o envio', word.OrderDate AS 'Data do pedido'
 FROM w3schools.orders word
 INNER JOIN w3schools.customers wcust ON wcust.CustomerID = word.CustomerID
 INNER JOIN w3schools.shippers wship ON wship.ShipperID = word.ShipperID
