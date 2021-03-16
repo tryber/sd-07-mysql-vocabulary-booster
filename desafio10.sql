38af94ae903e7f8ca351f8d16b5089df6c68bcd7
vladimirrchagas <vladimirrchagas@gmail.com>
2021-03-16 20:50:03 -0300

    checking challenge 10

---
+++ desafio10.sql
@@ -0,0 +1,5 @@
+SELECT wprod.ProductName AS 'Produto', MIN(wodet.Quantity) AS 'Mínima', MAX(wodet.Quantity) AS 'Máxima', ROUND(AVG(wodet.Quantity), 2) AS 'Média'
+FROM w3schools.products wprod
+INNER JOIN w3schools.order_details wodet ON wprod.ProductID = wodet.ProductID
+GROUP BY wprod.ProductID
+ORDER BY 4, 1
