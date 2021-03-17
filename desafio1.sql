b010bab7b0e7f850d8743049943d67e7a87271a1
lucasvr39 <lucasveras1994@gmail.com>
2021-03-17 18:31:23 -0300

    try to fix challenge 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,3 +1,6 @@
-select country_name as "País" ,if(region_id = 1, "incluído", "não incluído") as "Status Inlcusão"
-from hr.countries
+select country_name as "País" ,
+if(region_name = "Europe", "incluído", "não incluído") as "Status Inlcusão"
+from hr.countries as hrc
+inner join hr.regions as hrr
+on hrr.region_id = hrc.region_id
 order by `País` asc;
