a88a925a8a21b5d75f99d195edd68211ad39c0dc
lucasvr39 <lucasveras1994@gmail.com>
2021-03-17 20:19:41 -0300

    fix challenge 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,10 +1,12 @@
 select concat(hre.first_name, " ", hre.last_name) as "Nome completo",
 hrj.job_title as "Cargo",
-hire_date as "Data de início do cargo",
+hrjh.start_date as "Data de início do cargo",
 hrd.department_name as "Departamento"
 from hr.employees as hre
 inner join hr.jobs as hrj
 on hre.job_id = hrj.job_id
 inner join hr.departments as hrd
 on hre.department_id = hrd.department_id
-order by `Nome completo`, `Cargo` asc;
+inner join hr.job_history as hrjh
+on hre.employee_id = hrjh.employee_id
+order by `Nome completo` desc, `Cargo` asc;
