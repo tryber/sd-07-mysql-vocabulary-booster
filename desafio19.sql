31388926f7c2f353337adc7e9673ba50c6f5ca15
Anderson Meira <contatodeanderson@gmail.com>
2021-03-18 10:42:55 -0300

    desafios 4 ao 19

---
+++ desafio19.sql
@@ -0,0 +1,12 @@
+DELIMITER $$
+CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mes INT, ano INT)
+	RETURNS INT READS SQL DATA
+BEGIN
+	DECLARE total INT;
+	SELECT COUNT(*) FROM hr.employees
+    WHERE MONTH(HIRE_DATE) = mes AND YEAR(HIRE_DATE) = ano INTO total;
+	RETURN total;
+END $$
+DELIMITER ;
+
+-- SELECT exibir_quantidade_pessoas_contratadas_por_mes_e_ano(6, 1987);
