19c2bd4b6043a9d5124a8fd1262f215f9b10ed05
lucasvr39 <lucasveras1994@gmail.com>
2021-03-18 15:22:23 -0300

    add month having by to fix challenge 7

--- desafio7.sql
+++ desafio7.sql
@@ -4,4 +4,5 @@ hre.salary as "Salário"
 from hr.employees as hre
 inner join hr.job_history as hrjh
 on hre.employee_id = hrjh.employee_id
+having month(`Data de início`) between 1 and 3 
 order by `Nome completo`, `Data de início` asc;
