38631111381ea8ab66d4453c006a627b1005a634
lucasvr39 <lucasveras1994@gmail.com>
2021-03-18 16:08:02 -0300

    try to fix challenge 8

--- desafio8.sql
+++ desafio8.sql
@@ -6,4 +6,5 @@ inner join w3schools.customers as wcustomers
 on wcustomers.CustomerID = worders.CustomerID
 inner join w3schools.shippers as wshippers
 on wshippers.ShipperID = worders.ShipperID
+having `Empresa que fez o envio` = "Speedy Express" or `Empresa que fez o envio` = "United Package"
 order by `Nome de contato`, `Empresa que fez o envio`, `Data do pedido` asc;
