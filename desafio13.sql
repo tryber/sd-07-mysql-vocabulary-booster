d8f60f05d3d318dd89acba7f7f5f7475ca0802d1
lucasvr39 <lucasveras1994@gmail.com>
2021-03-18 20:10:21 -0300

    fix nonaggregate issues

--- desafio13.sql
+++ desafio13.sql
@@ -2,7 +2,5 @@ select wp.ProductName as "Produto",
 wp.Price as "Preço"
 from w3schools.products as wp
 inner join w3schools.order_details as wod
-on wp.ProductID = wod.ProductID
-group by wod.Quantity
-having wod.Quantity > 80
+on wp.ProductID = wod.ProductID and wod.Quantity > 80
 order by `Produto`;
