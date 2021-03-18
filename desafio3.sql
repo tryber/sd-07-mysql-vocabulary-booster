f6c56a3bbc30b9054c94e591af01cd7a1d0c88c3
lucasvr39 <lucasveras1994@gmail.com>
2021-03-18 13:48:19 -0300

    fix challenges 3 and 6

--- desafio3.sql
+++ desafio3.sql
@@ -1,3 +1,4 @@
 select job_title as "Cargo",
 (max_salary - min_salary) as "Diferença entre salários máximo e mínimo"
-from hr.jobs;
+from hr.jobs
+order by `Diferença entre salários máximo e mínimo` asc;
--- desafio6.sql
+++ desafio6.sql
@@ -3,10 +3,10 @@ hrj.job_title as "Cargo",
 hrjh.start_date as "Data de início do cargo",
 hrd.department_name as "Departamento"
 from hr.employees as hre
-inner join hr.jobs as hrj
-on hre.job_id = hrj.job_id
-inner join hr.departments as hrd
-on hre.department_id = hrd.department_id
-inner join hr.job_history as hrjh
+inner join  hr.job_history as hrjh
 on hre.employee_id = hrjh.employee_id
+inner join hr.jobs as hrj
+on hrj.job_id = hrjh.job_id
+inner join hr.departments as hrd
+on hrd.department_id = hrjh.department_id
 order by `Nome completo` desc, `Cargo` asc;
