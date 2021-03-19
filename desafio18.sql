978b55a722a6ca32f0f86730eb2db4a870892ac5
vladimirrchagas <vladimirrchagas@gmail.com>
2021-03-19 10:13:56 -0300

    adjustment of challenge 18

--- desafio18.sql
+++ desafio18.sql
@@ -1,4 +1,6 @@
-SELECT CONCAT(hemp.FIRST_NAME, ' ', hemp.LAST_NAME) AS 'Nome completo', hjh.START_DATE AS 'Data de início', hjh.END_DATE AS 'Data de recisão',
+SELECT CONCAT(hemp.FIRST_NAME, ' ', hemp.LAST_NAME) AS 'Nome completo',
+CONCAT(DAY(hjh.START_DATE), '/', MONTH(hjh.START_DATE), '/', YEAR(hjh.START_DATE)) AS 'Data de início',
+CONCAT(DAY(hjh.END_DATE), '/', MONTH(hjh.END_DATE), '/', YEAR(hjh.END_DATE)) AS 'Data de recisão',
 ROUND(DATEDIFF(hjh.END_DATE, hjh.START_DATE) / 365, 2) AS 'Anos trabalhados'
 FROM hr.employees hemp
 INNER JOIN hr.job_history hjh ON hemp.EMPLOYEE_ID = hjh.EMPLOYEE_ID
