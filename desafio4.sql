e968d7bb122b9b5c877be211a48711b930e17828
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2021-03-29 20:54:41 -0300

    requisito 4 codeclimate

--- desafio4.sql
+++ desafio4.sql
@@ -1,9 +1,9 @@
 SELECT j.JOB_TITLE AS "Cargo", ROUND(AVG(e.SALARY), 2) AS "Média salarial",
 CASE
-	WHEN ROUND(AVG(e.SALARY), 2) >= 2000 AND ROUND(AVG(e.SALARY), 2) <= 5800 THEN 'Júnior'
-    WHEN ROUND(AVG(e.SALARY), 2) <= 7500 THEN 'Pleno'
-    WHEN ROUND(AVG(e.SALARY), 2) <= 10500 THEN 'Sênior'
-    ELSE 'CEO'
+WHEN ROUND(AVG(e.SALARY), 2) >= 2000 AND ROUND(AVG(e.SALARY), 2) <= 5800 THEN 'Júnior'
+WHEN ROUND(AVG(e.SALARY), 2) <= 7500 THEN 'Pleno'
+WHEN ROUND(AVG(e.SALARY), 2) <= 10500 THEN 'Sênior'
+ELSE 'CEO'
 END AS Senioridade
 FROM hr.jobs j
 INNER JOIN hr.employees e
