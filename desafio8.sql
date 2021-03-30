6bdae4622a978354e94bf217b824032d1b3b6c46
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2021-03-29 23:10:31 -0300

    requisito 8 MUDANDO PARA WHERE

--- desafio8.sql
+++ desafio8.sql
@@ -3,7 +3,8 @@ s.ShipperName AS "Empresa que fez o envio",
 o.OrderDate AS "Data do pedido"
 FROM w3schools.customers c
 INNER JOIN w3schools.orders o
-ON c.CustomerID = o.CustomerID and o.ShipperID IN (1, 2)
+ON c.CustomerID = o.CustomerID
 INNER JOIN w3schools.shippers s
 ON o.ShipperID = s.ShipperID
+WHERE o.ShipperID IN (1, 2)
 ORDER BY 1, 2, 3;
