0642add12cdda1e24c12372d8414122152a27678
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2021-03-29 23:18:51 -0300

    requisito 8 mudando para contactName

--- desafio8.sql
+++ desafio8.sql
@@ -1,10 +1,9 @@
-SELECT c.CustomerName AS "Nome de contato",
+SELECT c.ContactName AS "Nome de contato",
 s.ShipperName AS "Empresa que fez o envio",
 o.OrderDate AS "Data do pedido"
 FROM w3schools.customers c
 INNER JOIN w3schools.orders o
-ON c.CustomerID = o.CustomerID
+ON c.CustomerID = o.CustomerID and o.ShipperID IN (1, 2)
 INNER JOIN w3schools.shippers s
 ON o.ShipperID = s.ShipperID
-WHERE o.ShipperID IN (1, 2)
 ORDER BY 1, 2, 3;
