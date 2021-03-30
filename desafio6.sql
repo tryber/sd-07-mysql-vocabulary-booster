729f3f7a2188c39f48fa8dce3543b8bc1cf558c7
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2021-03-29 22:18:05 -0300

    requisito 6

---
+++ desafio6.sql
@@ -0,0 +1,12 @@
+SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS "Nome completo",
+j.JOB_TITLE AS "Cargo",
+jh.START_DATE AS "Data de início do cargo",
+d.DEPARTMENT_NAME AS  "Departamento" 
+FROM hr.employees e
+INNER JOIN hr.job_history jh
+ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+INNER JOIN hr.jobs j
+ON jh.JOB_ID = j.JOB_ID
+INNER JOIN hr.departments d
+ON d.DEPARTMENT_ID = e.DEPARTMENT_ID
+ORDER BY 1 DESC, Cargo;
