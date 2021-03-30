3f9702e00e6e234915df7c1066bc1830d0736612
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2021-03-29 23:58:44 -0300

    requisito 10 adicionando a condiçao de 20 e colocando o p na linha 1

--- desafio10.sql
+++ desafio10.sql
@@ -1,4 +1,4 @@
-SELECT ProductName AS "Produto",
+SELECT p.ProductName AS "Produto",
 MIN(od.Quantity) AS "Mínima",
 MAX(od.Quantity) AS "Máxima",
 ROUND(AVG(od.Quantity), 2) AS "Média"
@@ -6,4 +6,5 @@ FROM w3schools.products p
 INNER JOIN w3schools.order_details od
 ON p.ProductID = od.ProductID
 GROUP BY p.ProductName
+HAVING Média > 20
 ORDER BY 4, 1;
