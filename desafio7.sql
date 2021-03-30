32ce2497e4dc9939cc60d8668bdec1aa0e77f270
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2021-03-29 22:46:05 -0300

    requisito 7 ADICIONANDO A COLUNA SALARIO

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,6 @@
 SELECT UPPER(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS "Nome completo",
 jh.START_DATE AS "Data de início",
-e.SALARY
+e.SALARY AS "Salário"
 FROM hr.employees e
 INNER JOIN hr.job_history jh
 ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
