1999f52be7fc0b0a70fa542e97b69183a2899e11
Elisa <elisatcamillo@gmail.com>
2021-04-01 17:40:43 -0300

    2 - e tinha esquecido do ponto e vírgula

--- desafio2.sql
+++ desafio2.sql
@@ -1,4 +1,11 @@
-SELECT case
-WHEN condi THEN valor
-ELSE 
-END
+SELECT 
+  job_title AS Cargo,
+  CASE
+    WHEN max_salary > 5000 AND max_salary < 10000 THEN "Baixo"
+    WHEN max_salary > 10001 AND max_salary < 20000 THEN "Médio"
+    WHEN max_salary > 20001 AND max_salary < 30000 THEN "Alto"
+    WHEN max_salary > 30000 THEN "Altíssimo"
+    ELSE "não classificado"
+  END AS Nível
+FROM hr.jobs
+ORDER BY job_title;
