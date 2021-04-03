c7a4b3884d2be94ff1ff51e2c5e8b6fa0ef9c929
André Ferreira <andreferreirap1@gmail.com>
2021-04-02 22:03:52 -0300

    Desafio 14 - Refeito

--- desafio14.sql
+++ desafio14.sql
@@ -1,2 +1,19 @@
-SELECT DISTINCT Country 'País' FROM w3schools.customers
-ORDER BY Country LIMIT 5 ;
+(
+SELECT
+customers.Country AS 'País'
+FROM w3schools.customers
+GROUP BY Country
+ORDER BY Country
+LIMIT 5
+)
+UNION
+(
+SELECT
+suppliers.Country AS 'País'
+FROM w3schools.suppliers
+GROUP BY Country
+ORDER BY Country
+LIMIT 5
+)
+ORDER BY País
+LIMIT 5;
