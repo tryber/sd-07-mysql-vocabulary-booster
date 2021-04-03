6a5868fa3bbab528114a36b778e26079b46fdc3a
André Ferreira <andreferreirap1@gmail.com>
2021-04-03 01:46:57 -0300

    Desafio 18 | farto ordenar

--- desafio18.sql
+++ desafio18.sql
@@ -5,4 +5,5 @@ DATE_FORMAT(job_h.END_DATE, '%d%/%m%/%y') 'Data de recisão',
 ROUND(DATEDIFF(job_h.END_DATE, job_h.START_DATE) / 365, 2) 'Anos trabalhados'
 FROM hr.job_history job_h
 INNER JOIN hr.employees emp
-ON job_h.EMPLOYEE_ID = emp.EMPLOYEE_ID;
+ON job_h.EMPLOYEE_ID = emp.EMPLOYEE_ID
+ORDER BY `Nome completo`, `Anos trabalhados`;
