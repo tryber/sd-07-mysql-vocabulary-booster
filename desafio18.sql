858d35a8a2bd8d4f54ceb3f78fe154a4c7723d98
André Ferreira <andreferreirap1@gmail.com>
2021-04-03 01:59:50 -0300

    Desafio 18 | rescisão é com sc

--- desafio18.sql
+++ desafio18.sql
@@ -1,7 +1,7 @@
 SELECT
 CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) 'Nome completo',
 DATE_FORMAT(job_h.START_DATE, CONCAT('%d%/%m%/', YEAR(job_h.START_DATE))) 'Data de início',
-DATE_FORMAT(job_h.END_DATE, CONCAT('%d%/%m%/', YEAR(job_h.END_DATE))) 'Data de recisão',
+DATE_FORMAT(job_h.END_DATE, CONCAT('%d%/%m%/', YEAR(job_h.END_DATE))) 'Data de rescisão',
 ROUND(DATEDIFF(job_h.END_DATE, job_h.START_DATE) / 365, 2) 'Anos trabalhados'
 FROM hr.job_history job_h
 INNER JOIN hr.employees emp
