4e2bb00a8f61746720e132b6386190544861a14d
André Ferreira <andreferreirap1@gmail.com>
2021-04-02 23:03:42 -0300

    Desafio 15 - Faltou tirar o 3 do nome

--- desafio15.sql
+++ desafio15.sql
@@ -1,7 +1,7 @@
 USE hr;
 DELIMITER $$
 
-CREATE PROCEDURE buscar_media_por_cargo3(IN jobTitle TEXT)
+CREATE PROCEDURE buscar_media_por_cargo(IN jobTitle TEXT)
 BEGIN
 SELECT ROUND(AVG(emp.SALARY), 2) 'Média salarial' FROM employees emp
 INNER JOIN jobs
