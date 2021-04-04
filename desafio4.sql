325c70b282c7beec41152ee235348470b1005b42
Elisa <elisatcamillo@gmail.com>
2021-04-03 21:33:33 -0300

    linter

--- desafio4.sql
+++ desafio4.sql
@@ -14,16 +14,15 @@
 -- Os resultados devem estar ordenados pela média salarial em ordem crescente.
 -- Em caso de empate na média, os resultados devem ser ordenados pelo nome do cargo em ordem alfabética.
 
-
 SELECT 
-	jobs.job_title AS Cargo, -- GROUP BY HAVING
-	ROUND(AVG(employees.salary), 2) as "Média salarial",
-	CASE
-		WHEN ROUND(AVG(employees.salary), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
-		WHEN ROUND(AVG(employees.salary), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
-		WHEN ROUND(AVG(employees.salary)) BETWEEN 7501 AND 10500 THEN 'Sênior'
-		WHEN ROUND(AVG(employees.salary)) > 10500 THEN 'CEO'
-	END AS Senioridade
+jobs.job_title AS Cargo,
+ROUND(AVG(employees.salary), 2) as "Média salarial",
+CASE
+  WHEN ROUND(AVG(employees.salary), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+  WHEN ROUND(AVG(employees.salary), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+  WHEN ROUND(AVG(employees.salary)) BETWEEN 7501 AND 10500 THEN 'Sênior'
+  WHEN ROUND(AVG(employees.salary)) > 10500 THEN 'CEO'
+END AS Senioridade
 FROM hr.employees AS employees
 INNER JOIN hr.jobs as jobs
 ON employees.job_id = jobs.job_id
