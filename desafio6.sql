f3eddd6b0a6756a3ee6873b284b96bd38e935fb0
Elisa <elisatcamillo@gmail.com>
2021-04-09 20:23:23 -0300

    6 ordering results

--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(employees.first_name," " , employees.last_name) AS "Nome completo",
+SELECT CONCAT(employees.first_name, " ", employees.last_name) AS "Nome completo",
 jobs.job_title AS Cargo,
 job_history.start_date AS "Data de início do cargo",
 departments.department_name AS "Departamento"
@@ -8,4 +8,5 @@ ON job_history.employee_id = employees.employee_id
 INNER JOIN hr.jobs AS jobs
 ON job_history.job_id = jobs.job_id
 INNER JOIN hr.departments AS departments
-ON departments.department_id = job_history.department_id;
+ON departments.department_id = job_history.department_id
+ORDER BY "Nome completo" DESC, Cargo;
