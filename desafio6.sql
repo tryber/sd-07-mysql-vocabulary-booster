9b2358d4550c5153251078c4bb56e3d05c8ab120
Elisa <elisatcamillo@gmail.com>
2021-04-09 20:30:04 -0300

    6 - using backticks instead of double quotes works

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
-SELECT CONCAT(employees.first_name, " ", employees.last_name) AS "Nome completo",
+SELECT CONCAT(employees.first_name, ' ', employees.last_name) AS `Nome completo`,
 jobs.job_title AS Cargo,
-job_history.start_date AS "Data de início do cargo",
-departments.department_name AS "Departamento"
+job_history.start_date AS `Data de início do cargo`,
+departments.department_name AS `Departamento`
 FROM hr.job_history AS job_history
 INNER JOIN hr.employees AS employees
 ON job_history.employee_id = employees.employee_id
@@ -9,4 +9,4 @@ INNER JOIN hr.jobs AS jobs
 ON job_history.job_id = jobs.job_id
 INNER JOIN hr.departments AS departments
 ON departments.department_id = job_history.department_id
-ORDER BY "Nome completo" DESC, Cargo;
+ORDER BY `Nome completo` DESC, Cargo;
