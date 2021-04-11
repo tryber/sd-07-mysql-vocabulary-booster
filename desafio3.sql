2d5cfcf3a2ae5a91fab5188e187fb069f21d3a13
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2021-04-11 18:48:11 -0300

    desafio 1, 2 e 3

---
+++ desafio3.sql
@@ -0,0 +1,5 @@
+SELECT * FROM hr.jobs;
+SELECT JOB_TITLE as Cargo, MAX_SALARY - MIN_SALARY as 'Diferença entre salários máximo e mínimo' 
+FROM hr.jobs
+order by MAX_SALARY - MIN_SALARY, cargo;
+-- porque aqui eu não consegui colocar o order by por "'Diferença entre salários máximo e mínimo' "?
