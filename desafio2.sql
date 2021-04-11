b4ea9a1ff34b17093c9c9e412e65465ef15fb733
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2021-04-11 18:13:18 -0300

    desafio 1 e2

---
+++ desafio2.sql
@@ -0,0 +1,9 @@
+select job_title as cargo,  
+case 
+when max_salary between 5000 and 10000 then 'Baixo'
+when max_salary between 10001 and 20000 then 'Médio'
+when max_salary between 20001 and 30000 then 'Alto'
+    else 'Altíssimo'
+    end as Nível
+from hr.jobs
+order by cargo;
