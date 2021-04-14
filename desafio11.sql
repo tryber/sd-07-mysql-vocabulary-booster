bfdc4b9740533c4f40341859ca7bf81e3bdf8347
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2021-04-13 21:07:45 -0300

    Req 1 ao 15

--- desafio11.sql
+++ desafio11.sql
@@ -7,7 +7,7 @@ GROUP BY t1.ContactName, t1.Country
 Having `Número de compatriotas` > 0
 Order by Nome;
 
--- não entendi nada desse, como penser onde colocar o group by a principio, o exemplo da aula não funcionou
+-- não entendi nada desse, como pensar onde colocar o group by a principio, o exemplo da aula não funcionou
 -- porque ele não funciona sem o group by ou porque o group by é assim e nem as outras resoluções das outras pessoas.
 
 -- esse eu não sei se entendi bem o porque nem como está fazendo a subquerry
--- desafio6.sql
+++ desafio6.sql
@@ -4,9 +4,9 @@ h.START_DATE as 'Data de início do cargo',
 d.DEPARTMENT_NAME as Departamento
 from hr.employees as e
 Inner join hr.jobs as j
-on e.job_id = j.job_id
+on h.job_id = j.job_id
 Inner join hr.job_history as h
-on e.EMPLOYEE_ID = h.EMPLOYEE_ID
+on h.EMPLOYEE_ID = h.EMPLOYEE_ID
 Inner join hr.departments as d
 on h.DEPARTMENT_ID = d.DEPARTMENT_ID
 order by concat(e.First_name,' ',e.Last_Name) desc, cargo;
