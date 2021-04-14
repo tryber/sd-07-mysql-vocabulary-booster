66790a0f219d47b6be1f6d347a8fcefc1901f42e
Gustavo Saideles da Silveira <saideles.silveira@gmail.com>
2021-04-14 11:51:14 -0300

    tentando deferente

--- desafio9.sql
+++ desafio9.sql
@@ -1,6 +1,6 @@
-SELECT CONCAT(emplo.FirstName,' ', emplo.LastName) AS 'Nome Completo',
+SELECT CONCAT(employees.FirstName,' ', employees.LastName) AS 'Nome Completo',
 COUNT(orders.OrderID) AS 'Total de pedidos'
-FROM w3schools.employees AS emplo
-INNER JOIN w3schools.orders AS orders ON emplo.EmployeeID = orders.EmployeeID
-GROUP BY emplo.EmployeeID
+FROM w3schools.employees AS employees
+INNER JOIN w3schools.orders AS orders ON employees.EmployeeID = orders.EmployeeID
+GROUP BY employees.EmployeeID
 ORDER BY `Total de pedidos`;
