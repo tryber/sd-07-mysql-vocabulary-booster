f11af3135474766e0ddd02e3a545f06abcd4ca3f
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2021-04-14 02:29:18 -0300

    Requisito 1 ao 18

--- tests/hrChallenges.spec.js
+++ tests/hrChallenges.spec.js
@@ -94,7 +94,7 @@ describe('Desafios iniciais', () => {
     });
   });
 
-  describe.skip('Faça um relatório que lista todas as pessoas funcionárias **que possuem o mesmo cargo**', () => {
+  describe('Faça um relatório que lista todas as pessoas funcionárias **que possuem o mesmo cargo**', () => {
     it('Verifica o desafio 12', async () => {
       const challengeQuery = readFileSync('desafio12.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult12');
@@ -103,7 +103,7 @@ describe('Desafios iniciais', () => {
     });
   });
 
-  describe.skip('Crie uma procedure chamada `buscar_media_por_cargo` que recebe como parâmetro o nome de um cargo e em retorno deve mostrar a média salarial de todas as pessoas que possuem esse cargo', () => {
+  describe('Crie uma procedure chamada `buscar_media_por_cargo` que recebe como parâmetro o nome de um cargo e em retorno deve mostrar a média salarial de todas as pessoas que possuem esse cargo', () => {
     it('Verifica o desafio 15', async () => {
       const challengeQuery = readFileSync('desafio15.sql', 'utf8').trim();
       const createProcedureQuery = /CREATE PROCEDURE.*END/si.exec(challengeQuery)[0];
@@ -117,7 +117,7 @@ describe('Desafios iniciais', () => {
     });
   });
 
-  describe.skip('Crie uma função chamada `buscar_quantidade_de_empregos_por_funcionario` no banco de dados `hr` que, ao receber o **email de uma pessoa funcionária**, retorne a quantidade de empregos **presentes em seu histórico**', () => {
+  describe('Crie uma função chamada `buscar_quantidade_de_empregos_por_funcionario` no banco de dados `hr` que, ao receber o **email de uma pessoa funcionária**, retorne a quantidade de empregos **presentes em seu histórico**', () => {
     it('Verifica o desafio 16', async () => {
       const challengeQuery = readFileSync('desafio16.sql', 'utf8').trim();
       const createFunctionQuery = /CREATE FUNCTION.*END/si.exec(challengeQuery)[0];
@@ -135,7 +135,7 @@ describe('Desafios iniciais', () => {
     });
   });
 
-  describe.skip('Faça um relatório que mostra o **histórico de cargos das pessoas empregadas**, mostrando as datas de início e de saída, assim como os anos que ela ficou nesse cargo', () => {
+  describe('Faça um relatório que mostra o **histórico de cargos das pessoas empregadas**, mostrando as datas de início e de saída, assim como os anos que ela ficou nesse cargo', () => {
     it('Verifica o desafio 18', async () => {
       const challengeQuery = readFileSync('desafio18.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult18');
